b4399503e55a4b7b567f7d23611542a508cb0937
David Ma <davidma415@gmail.com>
2023-01-30 19:20:53 -0700

    chore: update linting

--- packages/portal/document-viewer/src/components/nodes/List.vue
+++ packages/portal/document-viewer/src/components/nodes/List.vue
@@ -18,9 +18,9 @@ const tag = props.isOrdered ? 'ol' : 'ul'
 <style scoped>
 ul, ol {
   font-size: 14px;
+  margin-bottom: var(--spacing-sm, 12px);
   margin-left: 16px;
   padding: 0;
-  margin-bottom: var(--spacing-sm, 12px);
 }
 
 ol {
