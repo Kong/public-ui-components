b6d12ecb713ba71145c29b97e4d1561a702bb93a
Andrew Wylde <andrew.wylde@konghq.com>
2023-03-16 15:28:10 -0500

    refactor(document-viewer): use checked property, add example [TDX-2943]

--- packages/portal/document-viewer/src/components/nodes/TaskCheckbox.vue
+++ packages/portal/document-viewer/src/components/nodes/TaskCheckbox.vue
@@ -1,3 +1,19 @@
 <template>
-  <input type="checkbox">
+  <input
+    :checked="checked"
+    type="checkbox"
+  >
 </template>
+
+<script setup lang="ts">
+import { defineProps, ref } from 'vue'
+
+type TaskCheckboxProps = {
+  checked: boolean;
+};
+
+const props = defineProps<TaskCheckboxProps>()
+
+const checked = ref(props.checked)
+
+</script>
