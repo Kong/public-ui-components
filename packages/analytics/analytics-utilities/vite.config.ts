82fae094c2d85aecc9802bab53b6fdbd6e9b7514
Filip <filgut@protonmail.com>
2023-04-20 12:00:07 -0700

    feat(analytics-utils): add analytics utils package (#323)
    * feat(analytics-utils): add analytics utils package
    
    * chore: build for production
    
    * chore: address comments - export all
    
    * chore: update readme
    
    * chore: rename package to analytics-utilities

---
+++ packages/analytics/analytics-utilities/vite.config.ts
@@ -0,0 +1,28 @@
+import sharedViteConfig from '../../../vite.config.shared'
+import { resolve } from 'path'
+import { defineConfig, mergeConfig } from 'vite'
+
+// Package name MUST always match the kebab-case package name inside the component's package.json file and the name of your `/packages/{package-name}` directory
+const packageName = 'analytics-utilities'
+
+// Merge the shared Vite config with the local one defined below
+const config = mergeConfig(sharedViteConfig, defineConfig({
+  build: {
+    lib: {
+      // The kebab-case name of the exposed global variable. MUST be in the format `kong-ui-public-{package-name}`
+      // Example: name: 'kong-ui-public-demo-component'
+      name: `kong-ui-public-${packageName}`,
+      entry: resolve(__dirname, './src/index.ts'),
+      fileName: (format) => `${packageName}.${format}.js`,
+    },
+  },
+}))
+
+// If we are trying to preview a build of the local `package/analytics-utilities/sandbox` directory,
+// unset the external and lib properties
+if (process.env.PREVIEW_SANDBOX) {
+  config.build.rollupOptions.external = undefined
+  config.build.lib = undefined
+}
+
+export default config
