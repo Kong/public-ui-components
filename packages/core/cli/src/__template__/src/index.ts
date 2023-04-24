91ca8a13cb960c94c310ed6225cec12fd1cc71d3
Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
2023-04-24 13:36:43 -0400

    chore(cli): remove vue plugin export (#342)

--- packages/core/cli/src/__template__/src/index.ts
+++ packages/core/cli/src/__template__/src/index.ts
@@ -1,15 +1,16 @@
-import type { App } from 'vue'
+// import type { App } from 'vue'
 import {%%COMPONENT_NAME%%} from './components/{%%COMPONENT_NAME%%}.vue'
 
-// Export Vue plugin as the default
-export default {
-  // Customize Vue plugin options as desired
-  // Providing a `name` property allows for customizing the registered
-  // name of your component (useful if exporting a single component).
-  install: (app: App, options: { name?: string, [key: string]: any } = {}): void => {
-    app.component(options.name || '{%%COMPONENT_NAME%%}', {%%COMPONENT_NAME%%})
-  },
-}
+// Export Vue plugin
+// We rarely want to export components as a plugin as we prefer to support proper tree-shaking in the host application. Only enable if you're packing a Vue plugin.
+// export default {
+//   // Customize Vue plugin options as desired
+//   // Providing a `name` property allows for customizing the registered
+//   // name of your component (useful if exporting a single component).
+//   install: (app: App, options: { name?: string, [key: string]: any } = {}): void => {
+//     app.component(options.name || '{%%COMPONENT_NAME%%}', {%%COMPONENT_NAME%%})
+//   },
+// }
 
 export { {%%COMPONENT_NAME%%} }
 
