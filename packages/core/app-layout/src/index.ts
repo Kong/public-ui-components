bda8cdeaef9ea3125a452b2479d264b2b9578c85
Adam DeHaven <adam.dehaven@konghq.com>
2023-04-25 15:20:31 -0400

    fix: trigger pr preview

--- packages/core/app-layout/src/index.ts
+++ packages/core/app-layout/src/index.ts
@@ -11,8 +11,7 @@ import AppError from './components/errors/AppError.vue'
 // Export Vue plugin as the default
 export default {
   // Customize Vue plugin options as desired
-  // Providing a `name` property allows for customizing the registered
-  // name of your component (useful if exporting a single component).
+  // Providing a `name` property allows for customizing the registered name of your component (useful if exporting a single component).
   install: (app: App, options: { name?: string, [key: string]: any } = {}): void => {
     app.component(options.name || 'AppLayout', AppLayout)
   },
