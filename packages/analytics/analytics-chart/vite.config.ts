395ca7dff23e8ee29a14d9e57bbbd455d54253cc
Filip Gutica <filgut@protonmail.com>
2023-06-22 15:28:21 -0700

    chore: pr comments

--- packages/analytics/analytics-chart/vite.config.ts
+++ packages/analytics/analytics-chart/vite.config.ts
@@ -12,7 +12,7 @@ const config = mergeConfig(sharedViteConfig, defineConfig({
     lib: {
       // The kebab-case name of the exposed global variable. MUST be in the format `kong-ui-{package-name}`
       // Example: name: 'kong-ui-demo-component'
-      name: `kong-ui-${sanitizedPackageName}`,
+      name: `kong-ui-public${sanitizedPackageName}`,
       entry: resolve(__dirname, './src/index.ts'),
       fileName: (format) => `${sanitizedPackageName}.${format}.js`,
     },
