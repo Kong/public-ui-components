232fe79fab074c4477232dd74dea7bfb5f2b447f
kai.arrowood <kai.arrowood@konghq.com>
2023-08-21 14:43:25 -0400

    fix(*): big drop

--- packages/analytics/analytics-chart/sandbox/App.vue
+++ packages/analytics/analytics-chart/sandbox/App.vue
@@ -181,12 +181,15 @@
       :legend-position="legendPosition"
     />
     <TopNTable
-      v-if="exploreResult"
-      :data="exploreResult"
-      data-key="StatusCode"
+      v-if="exploreResultV3"
+      :data="exploreResultV3"
       description="Last 30-Day Summary"
       title="Top 5 Status Codes"
-    />
+    >
+      <template #name="{ record }">
+        <a href="#">{{ record.name }}</a>
+      </template>
+    </TopNTable>
 
     <!-- Dataset options -->
     <div class="dataset-options">
@@ -474,6 +477,70 @@ const exploreResult = computed<AnalyticsExploreV2Result | null>(() => {
       // @ts-ignore - merely sorting the array for display purposes
       : records.slice(0, 2).sort((a, b) => (a[selectedMetric.value.name] < b[selectedMetric.value.name])),
     meta,
+  }
+})
+const exploreResultV3 = computed(() => {
+  return {
+    meta: {
+      display: {
+        ROUTE: {
+          'b486fb30-e058-4b5f-85c2-495ec26ba522:09ba7bc7-58d6-42d5-b9c0-3ffb28b307e6': 'GetMeAKongDefault (secondaryRuntime)',
+          'd5ac5d88-efed-4e10-9dfe-0b0a6646c219:2a3e9d21-804b-4b3b-ab7e-c6f002dadbf4': 'dp-mock-msg-per-sec-us-dev (default)',
+          'd5ac5d88-efed-4e10-9dfe-0b0a6646c219:8b1db7eb-5c3c-489c-9344-eb0b272019ca': '8b1db (default)',
+          'd5ac5d88-efed-4e10-9dfe-0b0a6646c219:8f3f6808-a723-4793-8444-f2046961226b': 'dp-mock-us-dev (default)',
+          'd5ac5d88-efed-4e10-9dfe-0b0a6646c219:b4cd1c10-d77f-41b0-a84d-31fc0d99f0d9': 'GetMeASongRoute (default)',
+        },
+      },
+      endMs: 1692295253000,
+      granularity: 300000,
+      limit: 50,
+      metricNames: [
+        'REQUEST_COUNT',
+      ],
+      metricUnits: {
+        REQUEST_COUNT: 'count',
+      },
+      queryId: '4cc77ce4-6458-49f0-8a7e-443a4312dacd',
+      startMs: 1692294953000,
+      truncated: false,
+    },
+    records: [
+      {
+        event: {
+          REQUEST_COUNT: 9483,
+          ROUTE: 'b486fb30-e058-4b5f-85c2-495ec26ba522:09ba7bc7-58d6-42d5-b9c0-3ffb28b307e6',
+        },
+        timestamp: '2023-08-17T17:55:53.000Z',
+      },
+      {
+        event: {
+          REQUEST_COUNT: 5587,
+          ROUTE: 'd5ac5d88-efed-4e10-9dfe-0b0a6646c219:b4cd1c10-d77f-41b0-a84d-31fc0d99f0d9',
+        },
+        timestamp: '2023-08-17T17:55:53.000Z',
+      },
+      {
+        event: {
+          REQUEST_COUNT: 5583,
+          ROUTE: 'd5ac5d88-efed-4e10-9dfe-0b0a6646c219:8b1db7eb-5c3c-489c-9344-eb0b272019ca',
+        },
+        timestamp: '2023-08-17T17:55:53.000Z',
+      },
+      {
+        event: {
+          REQUEST_COUNT: 1485,
+          ROUTE: 'd5ac5d88-efed-4e10-9dfe-0b0a6646c219:8f3f6808-a723-4793-8444-f2046961226b',
+        },
+        timestamp: '2023-08-17T17:55:53.000Z',
+      },
+      {
+        event: {
+          REQUEST_COUNT: 309,
+          ROUTE: 'd5ac5d88-efed-4e10-9dfe-0b0a6646c219:2a3e9d21-804b-4b3b-ab7e-c6f002dadbf4',
+        },
+        timestamp: '2023-08-17T17:55:53.000Z',
+      },
+    ],
   }
 })
 
--- packages/analytics/analytics-chart/src/components/TopNTable.vue
+++ packages/analytics/analytics-chart/src/components/TopNTable.vue
@@ -1,6 +1,9 @@
 <template>
   <KCard class="kong-ui-public-top-n-table">
-    <template #title>
+    <template
+      v-if="title"
+      #title
+    >
       {{ title }}
     </template>
     <template
@@ -11,11 +14,22 @@
         {{ description }}
       </div>
     </template>
+
     <template #body>
+      <KEmptyState
+        v-if="errorMessage"
+        cta-is-hidden
+        is-error
+      >
+        <template #title>
+          {{ errorMessage }}
+        </template>
+      </KEmptyState>
       <div class="top-n-table">
         <div class="table-headings">
           <div class="table-row">
             <div class="column-1">
+              <!-- TODO: translate -->
               Name
             </div>
             <div class="column-2">
@@ -23,6 +37,7 @@
             </div>
           </div>
         </div>
+
         <div class="table-body">
           <div
             v-for="(entry, idx) in records"
@@ -30,10 +45,19 @@
             class="table-row"
           >
             <div class="column-1">
-              {{ getName(entry) }}
+              <slot
+                name="name"
+                :record="{
+                  id: getId(entry),
+                  name: getName(entry),
+                  dimension: displayKey,
+                }"
+              >
+                {{ getName(entry) }}
+              </slot>
             </div>
             <div class="column-2">
-              {{ getValue(entry) }}
+              &nbsp;{{ getValue(entry) }}
             </div>
           </div>
         </div>
@@ -43,8 +67,10 @@
 </template>
 
 <script setup lang="ts">
-import { computed, PropType } from 'vue'
-import { AnalyticsExploreResult, AnalyticsExploreV2Result, AnalyticsExploreRecord, RecordEvent, AnalyticsExploreMeta, AnalyticsExploreV2Meta } from '@kong-ui-public/analytics-utilities'
+import { computed } from 'vue'
+import { AnalyticsExploreRecord, RecordEvent } from '@kong-ui-public/analytics-utilities'
+// @ts-ignore - approximate-number no exported module
+import approxNum from 'approximate-number'
 import composables from '../composables'
 
 const props = defineProps({
@@ -57,19 +83,33 @@ const props = defineProps({
     default: '',
   },
   data: {
-    type: Object as PropType<AnalyticsExploreResult | AnalyticsExploreV2Result>,
-    required: true,
-  },
-  dataKey: {
-    type: String,
+    // TODO: type
+  //  type: Object as PropType<AnalyticsExploreResult | AnalyticsExploreV2Result>,
+    type: Object,
     required: true,
   },
 })
 
 const { i18n } = composables.useI18n()
-// TODO: strip table in favor of flex? Alignment of second column
-const meta = computed((): AnalyticsExploreMeta | AnalyticsExploreV2Meta => props.data.meta)
-const records = computed((): AnalyticsExploreRecord[] => props.data.records)
+// TODO: types
+const meta = computed(() => props.data.meta)
+const records = computed(() => props.data.records)
+const displayKey = computed(() => {
+  const key = Object.keys(meta.value?.display)?.[0]
+
+  if (!key) {
+    return
+  }
+
+  return key
+})
+const displayRecords = computed(() => {
+  if (!displayKey.value) {
+    return
+  }
+
+  return meta.value?.display[displayKey.value]
+})
 const columnKey = computed((): string => meta.value?.metricNames?.[0] || '')
 const columnName = computed((): string => {
   if (columnKey.value) {
@@ -78,21 +118,46 @@ const columnName = computed((): string => {
 
   return ''
 })
+const errorMessage = computed((): string => {
+  if (!meta.value) {
+    return 'Error: response must contain `meta`'
+  }
 
-// TODO: error state - missing required fields, or response incorrectly formatted, styling for links, approxnum
+  if (!displayKey.value) {
+    // TODO: translate
+    return 'Error: `display` response incorrectly formatted (must have at least one key)'
+  }
+
+  if (!columnKey.value) {
+    return 'Error: `metricNames` response incorrectly formatted (must have at least one key)'
+  }
+
+  return ''
+})
+
 // TODO: accept only v3, wait for API
-// TODO: remove `dataKey` prop, use `display`, first key in response - look up name from display
-// TODO: slot for column one value, slot param = id, name, and dimension (columnName)
-// TODO: translation
 
 const getEvent = (record: AnalyticsExploreRecord): RecordEvent => {
   return record.event
 }
 
-const getName = (record: AnalyticsExploreRecord): string => {
+const getId = (record: AnalyticsExploreRecord): string => {
+  if (!displayKey.value) {
+    return '-'
+  }
+
   const event = getEvent(record)
 
-  return String(event[props.dataKey]) || '-'
+  return String(event[displayKey.value]) || '-'
+}
+const getName = (record: AnalyticsExploreRecord): string => {
+  if (!displayRecords.value) {
+    return '-'
+  }
+
+  const id = getId(record)
+
+  return String(displayRecords.value[id]) || '-'
 }
 const getValue = (record: AnalyticsExploreRecord): string => {
   if (!columnKey.value) {
@@ -100,21 +165,31 @@ const getValue = (record: AnalyticsExploreRecord): string => {
   }
 
   const event = getEvent(record)
+  const val = event[columnKey.value]
 
-  return String(event[columnKey.value]) || '-'
+  if (!val) {
+    return '-'
+  }
+
+  return approxNum(val, { capital: true, round: true })
 }
 </script>
 
 <style lang="scss" scoped>
 .kong-ui-public-top-n-table {
-  // TODO: use correct tokens
-  /* --KCardTitleColor: #0B172D;
-  --KCardTitleFontSize: 16px; */
+  --kui-border-radius-20: 8px;
+  --kui-font-size-60: 16px;
 
-  .top-n-card-description {
-    color: #6F7787;
-    font-size: 12px;
-    text-align: right;
+  :deep(.k-card-actions) {
+    display: flex;
+    align-self: baseline;
+    line-height: 22px;
+
+    .top-n-card-description {
+      color: #6F7787;
+      font-size: 12px;
+      text-align: right;
+    }
   }
 
   .top-n-table {
@@ -122,15 +197,44 @@ const getValue = (record: AnalyticsExploreRecord): string => {
     flex-direction: column;
     width: 100%;
 
+    .table-headings {
+      font-size: 14px;
+      font-weight: 600;
+      line-height: 24px;
+    }
+
     .table-row {
+      align-self: stretch;
       border-bottom: 1px solid #E7E7EC;
       display: flex;
       justify-content: space-between;
-      padding: 15px;
+    }
+
+    .column-1 {
+      padding: 0 24px 15px 0;
     }
 
     .column-2 {
-      padding-left: 24px;
+      flex: 1;
+      max-width: 110px;
+      padding: 0 0 15px 0;
+    }
+
+    .table-body {
+      .column-1, .column-2 {
+        padding-top: 15px;
+      }
+
+      .column-1 {
+        color: #3C4557;
+        font-size: 13px;
+
+        :deep(a) {
+          color: #1155CB;
+          font-weight: 700;
+          text-decoration: none;
+        }
+      }
     }
   }
 }
