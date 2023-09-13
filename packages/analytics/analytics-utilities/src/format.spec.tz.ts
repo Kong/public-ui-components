01b4b51bf99fdfaddcbf8345513a816bf3b8a0f0
Rocky Fischer <adorack@users.noreply.github.com>
2023-09-13 15:55:50 -0400

    Fix spec typo

--- packages/analytics/analytics-utilities/src/format.spec.tz.ts
+++ packages/analytics/analytics-utilities/src/format.spec.tz.ts
@@ -7,7 +7,7 @@ runNonUtcTest('date formatting, non-UTC', () => {
     const pattern = /20\d{2}-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\d[+-]\d\d?:\d\d/
     const date = new Date('2021-01-01T06:02:03.000Z')
     expect(formatISOTimeWithTZ(date)).toMatch(pattern)
-    expect(formatISOTimeWithTZ(date)).toMatch(pattern)
+    expect(formatISOTimeWithTZ(date.getTime())).toMatch(pattern)
   })
 })
 
@@ -16,6 +16,6 @@ runUtcTest('date formatting in UTC', () => {
     const str = '2020-01-01T01:02:03.000Z'
     const date = new Date(str)
     expect(formatISOTimeWithTZ(date)).toBe(str)
-    expect(formatISOTimeWithTZ(date)).toBe(str)
+    expect(formatISOTimeWithTZ(date.getTime())).toBe(str)
   })
 })
