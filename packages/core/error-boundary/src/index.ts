e7da777c797a93f9d02e34b6ada835932b01c972
Adam DeHaven <adam.dehaven@konghq.com>
2023-10-05 18:18:41 -0400

    chore: remove locales

--- packages/core/error-boundary/src/index.ts
+++ packages/core/error-boundary/src/index.ts
@@ -6,8 +6,6 @@ import type { ErrorBoundaryPluginOptions } from './types'
 // Export Vue plugin
 export default {
   // Customize Vue plugin options as desired
-  // Providing a `name` property allows for customizing the registered
-  // name of your component (useful if exporting a single component).
   install: (app: App, options: ErrorBoundaryPluginOptions = {}): void => {
     app.component(options.name || 'ErrorBoundary', ErrorBoundary)
     app.provide(KONG_UI_ERROR_BOUNDARY_ON_ERROR_INJECTION_KEY, options.onError)
