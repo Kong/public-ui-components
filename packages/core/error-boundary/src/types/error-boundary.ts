70e60ffcaced3ce97a0f62dd1af1ef2bc1b0d3d3
Adam DeHaven <adam.dehaven@konghq.com>
2023-10-18 15:18:17 -0400

    feat(error-boundary): include data-testid [KHCP-9478]

--- packages/core/error-boundary/src/types/error-boundary.ts
+++ packages/core/error-boundary/src/types/error-boundary.ts
@@ -1,5 +1,6 @@
 export interface ErrorBoundaryContext {
   componentName: string
+  dataTestid: string
   info: string
   source: string
   tags: string[]
@@ -12,5 +13,5 @@ export interface ErrorBoundaryCallbackParams {
 
 export interface ErrorBoundaryPluginOptions {
   name?: string
-  onError?: ({ error, context: { componentName, info, source, tags } }: ErrorBoundaryCallbackParams) => void
+  onError?: ({ error, context: { componentName, dataTestid, info, source, tags } }: ErrorBoundaryCallbackParams) => void
 }
