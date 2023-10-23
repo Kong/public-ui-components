5842711b2f308338fe64515b7e65219fe22f266c
Adam DeHaven <adam.dehaven@konghq.com>
2023-10-23 17:44:59 -0400

    docs: add documentation

--- packages/analytics/analytics-chart/sandbox/index.ts
+++ packages/analytics/analytics-chart/sandbox/index.ts
@@ -1,4 +1,4 @@
-import { createApp, provide } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import Kongponents from '@kong/kongponents'
