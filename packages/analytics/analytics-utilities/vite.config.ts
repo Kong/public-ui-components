d6792ecc5484e82c778cd762d1a6ef74e534d1ba
Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
2023-10-23 10:51:17 -0400

    chore(sandbox): build and preview (#858)

--- packages/analytics/analytics-utilities/vite.config.ts
+++ packages/analytics/analytics-utilities/vite.config.ts
@@ -25,10 +25,11 @@ const config = mergeConfig(sharedViteConfig, defineConfig({
 }))
 
 // If we are trying to preview a build of the local `package/analytics-utilities/sandbox` directory,
-// unset the external and lib properties
-if (process.env.PREVIEW_SANDBOX) {
-  config.build.rollupOptions.external = undefined
+// unset the lib, rollupOptions.external and rollupOptions.output.globals properties
+if (process.env.USE_SANDBOX) {
   config.build.lib = undefined
+  config.build.rollupOptions.external = undefined
+  config.build.rollupOptions.output.global = undefined
 }
 
 export default config
