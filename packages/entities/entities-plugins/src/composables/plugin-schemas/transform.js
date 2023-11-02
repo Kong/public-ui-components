56d3f2de2659300b065ac435a44248c8488ca7e1
kai.arrowood <kai.arrowood@konghq.com>
2023-11-01 23:00:04 -0400

    fix(*): remove unused files

--- packages/entities/entities-plugins/src/composables/plugin-schemas/transform.js
+++
@@ -1,9 +0,0 @@
-import { getAutodocField } from './autodoc'
-import { marked } from 'marked'
-
-export const applyAutodocDesc = (o, entity, field) => {
-  const f = getAutodocField(entity, field)
-  if (f && f.desc) {
-    o.help = marked.parse(f.desc)
-  }
-}
