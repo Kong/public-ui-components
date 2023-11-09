84843454eb2b1a620327c5efa8c0923ec86c1295
mihai-peteu <mihai.peteu@konghq.com>
2023-11-09 14:54:52 -0800

    feat(entity-link): move EntityLink over from Konnect [MA-2278]

---
+++ packages/core/entity-link/sandbox/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="sandbox-container">
+    <main>
+      <p>This is the component sandbox.</p>
+      <EntityLink />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { EntityLink } from '../src'
+</script>
