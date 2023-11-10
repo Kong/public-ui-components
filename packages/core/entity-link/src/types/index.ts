7475a11b46d996c69e27ba750bd796e821501caf
mihai-peteu <mihai.peteu@konghq.com>
2023-11-09 18:22:49 -0800

    Revert "feat(entity-link): move EntityLink over from Konnect [MA-2278]"
    This reverts commit 2c9f15f1beeb33b1b0fea299aaff0a2b528cf416.

--- packages/core/entity-link/src/types/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all types and interfaces from this index.ts
-// The actual types and interfaces should be contained in separate files within this folder.
-
-// Example:
-// export * from './component-types'
