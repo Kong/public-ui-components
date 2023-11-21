4aaee1549cc8c76ca5fd763c59d84afb4988256a
Mihai <103061463+mihai-peteu@users.noreply.github.com>
2023-11-21 11:09:00 -0800

    fix(entity-link): more robust url regex (#933)

--- packages/entities/entities-shared/src/composables/useExternalLinkCreator.ts
+++ packages/entities/entities-shared/src/composables/useExternalLinkCreator.ts
@@ -7,11 +7,12 @@ export default function useExternalLinkCreator(routeParams: string[]): string {
     const fullUrl = `${baseUrl}${path}`
     const hasEmptyParams = routeParams.some(item => item.trim() === '')
 
-    /* Test validity of generated URL */
-    const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g // eslint-disable-line no-useless-escape
+    // Test validity of generated URL; only allows `http` protocal if domain is `localhost`
+    /* eslint-disable-next-line no-useless-escape */
+    const validUrlRegex = /^(https:\/\/(www\.)?([a-zA-Z0-9-]+\.){1,}[a-zA-Z]{2,}(:[0-9]+)?(\/[^\/]+)*)$|^(https|http):\/\/localhost(:[0-9]+)?(\/[^\/]+)*$/
 
     /* eslint-disable no-new */
-    if (!urlRegex.test(fullUrl) && !hasEmptyParams && new URL(fullUrl)) {
+    if (validUrlRegex.test(fullUrl) && !hasEmptyParams && new URL(fullUrl)) {
       return `${baseUrl}${path}`
     } else {
       throw new Error('Invalid url')
