758e3541f670e181befcb8eea11824952c6778e6
mihai-peteu <mihai.peteu@konghq.com>
2023-12-15 12:23:12 -0800

    feat: export formatTime and useChartSelectedRange

--- packages/analytics/analytics-chart/src/index.ts
+++ packages/analytics/analytics-chart/src/index.ts
@@ -2,6 +2,7 @@ import AnalyticsChart from './components/AnalyticsChart.vue'
 import SimpleChart from './components/SimpleChart.vue'
 import TopNTable from './components/TopNTable.vue'
 import CsvExportModal from './components/CsvExportModal.vue'
+import useChartSelectedRange from './composables'
 
 export { AnalyticsChart, SimpleChart, TopNTable, CsvExportModal }
 
@@ -9,3 +10,6 @@ export * from './types'
 export * from './enums'
 export * from './utils/colors'
 export * from './utils/customColors'
+export * from './utils/constants'
+
+export { useChartSelectedRange }
