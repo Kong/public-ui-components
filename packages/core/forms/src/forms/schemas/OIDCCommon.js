2203cf08a095e0d4db4b6fafe85f629be5112380
Makito <i@maki.to>
2023-12-25 11:14:56 +0800

    chore(forms): add help text to OIDC form

--- packages/core/forms/src/forms/schemas/OIDCCommon.js
+++ packages/core/forms/src/forms/schemas/OIDCCommon.js
@@ -1,24 +1,30 @@
 export default {
-  fields: [{
-    inputType: 'password',
-    label: 'Client ID',
-    model: 'config-client_id',
-    type: 'input',
-  }, {
-    inputType: 'password',
-    label: 'Client Secret',
-    model: 'config-client_secret',
-    type: 'input',
-  }, {
-    inputType: 'text',
-    label: 'Issuer',
-    model: 'config-issuer',
-    required: true,
-    type: 'input',
-  }, {
-    inputType: 'text',
-    label: 'Auth Methods',
-    model: 'config-auth_methods',
-    type: 'input',
-  }],
+  fields: [
+    {
+      inputType: 'password',
+      label: 'Client ID',
+      model: 'config-client_id',
+      type: 'input',
+    },
+    {
+      inputType: 'password',
+      label: 'Client Secret',
+      model: 'config-client_secret',
+      type: 'input',
+    },
+    {
+      inputType: 'text',
+      label: 'Issuer',
+      model: 'config-issuer',
+      required: true,
+      type: 'input',
+      help: "The identity provider's URL endpoint for obtaining authentication details.",
+    },
+    {
+      inputType: 'text',
+      label: 'Auth Methods',
+      model: 'config-auth_methods',
+      type: 'input',
+    },
+  ],
 }
