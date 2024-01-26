1c2c9a3c86a3f7d1b8489dbba76466a12f213cc7
Filip Gutica <filgut@protonmail.com>
2024-01-26 09:10:05 -0800

    fix: no more bezier curve tension and no more dataset border on line charts

--- packages/analytics/analytics-chart/src/utils/defaultLineOptions.ts
+++ packages/analytics/analytics-chart/src/utils/defaultLineOptions.ts
@@ -1,6 +1,6 @@
 export const defaultLineOptions = {
-  lineTension: 0.4,
-  borderWidth: 2,
+  lineTension: 0,
+  borderWidth: 0,
   pointBorderWidth: 1.2,
   borderJoinStyle: 'round',
   fill: true,
