d5a124c7a729cf6a0f447801fb15d3f8cf167644
Filip Gutica <filgut@protonmail.com>
2024-01-28 16:04:57 -0800

    fix: style improvements + scrollbar updates in tooltip for consistency

--- packages/analytics/analytics-chart/sandbox/pages/BarChartDemo.vue
+++ packages/analytics/analytics-chart/sandbox/pages/BarChartDemo.vue
@@ -288,7 +288,7 @@ const dimensionItems = [{
 
 // Short labels
 const statusCodeLabels = [
-  '200', '300', '400', '123123213asasdfasdfasd', 'asdfasdfasdfasdfadsfasdf',
+  '200', '300', '400', '123123213asa sdfasdfasd', 'asdfasdfasdfasdfadsfasdf',
 ]
 
 const statusCodeDimensionValues = ref(new Set(statusCodeLabels))
