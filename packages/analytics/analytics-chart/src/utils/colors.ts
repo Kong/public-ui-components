48ca7fef8d383c4995cc239c5b06d75483609cd1
mihai-peteu <mihai.peteu@konghq.com>
2024-01-30 11:57:06 -0800

    feat: parseInt ftw [MA-2557]

--- packages/analytics/analytics-chart/src/utils/colors.ts
+++ packages/analytics/analytics-chart/src/utils/colors.ts
@@ -89,7 +89,7 @@ export const apiRequestStatusCodeColors: StatusCodeColors = {
 }
 
 export const statusCodeBadgeBackgroundColor = (statusCode: string) => {
-  const keyIndex = Math.floor(statusCode as any as number / 100) * 100
+  const keyIndex = Math.floor(parseInt(statusCode) / 100) * 100
 
   return apiRequestStatusCodeColors[keyIndex]?.background || '#fafafa'
 }
