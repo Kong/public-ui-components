ea1280d2551b449d3f7a6db9e800ea12d6832ee5
mihai-peteu <mihai.peteu@konghq.com>
2024-01-30 10:37:50 -0800

    feat: move formatTime to analytics-utilities [MA-2557]

--- packages/analytics/analytics-utilities/src/format.ts
+++ packages/analytics/analytics-utilities/src/format.ts
@@ -1,5 +1,47 @@
 import { format } from 'date-fns'
+import { formatInTimeZone } from 'date-fns-tz'
+
+export interface TimeFormatOptions {
+  includeTZ?: boolean
+  tz?: string
+  short?: boolean
+}
 
 export function formatISOTimeWithTZ(ts: number | Date) {
   return format(ts, "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+}
+
+export function formatTime(ts: number, options: TimeFormatOptions = {}) {
+  if (!ts) {
+    return ts
+  }
+
+  const tz = options.tz || Intl.DateTimeFormat().resolvedOptions().timeZone
+
+  try {
+    let timeFormat = 'MMM dd, YYY hh:mm a'
+    if (options.short) {
+      timeFormat = 'MMM dd, YYY'
+    }
+
+    if (options.includeTZ) {
+      timeFormat += ' (z)'
+    }
+
+    const date = new Date(ts)
+
+    // Note: We always need to use `formatInTimeZone` (rather than plain `format`)
+    // in order to achieve consistent results based on the tz of the current computer.
+    // Otherwise, unit tests can fail depending on the tz of the dev's computer (or in CI), etc.
+    // Even if we don't care about timezones, timezones care about us.  :/
+    return formatInTimeZone(date, tz, timeFormat)
+  } catch (exc) {
+    console.error('Invalid value passed to formatTime', ts)
+
+    return '(invalid date)'
+  }
+}
+
+export function formatTimeRange(start: Date, end: Date) {
+  return `${formatTime(start.getTime())} - ${formatTime(end.getTime(), { includeTZ: true })}`
 }
