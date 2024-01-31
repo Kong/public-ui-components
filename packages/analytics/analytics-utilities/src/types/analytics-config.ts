84bc4200fe8da0d9a69f91e179576af2f0f8707b
Rocky Fischer <adorack@users.noreply.github.com>
2024-01-31 16:48:05 -0500

    feat: improve AnalyticsConfig interface

--- packages/analytics/analytics-utilities/src/types/analytics-config.ts
+++ packages/analytics/analytics-utilities/src/types/analytics-config.ts
@@ -1,11 +1,17 @@
-export type AnalyticsRetention = '30d' | '90d' | '180d' | '365d'
+export type AnalyticsRetention = '1d' | '30d' | '90d' | '180d' | '365d'
 export type RequestsRetention = '24h' | '7d' | '14d' | '30d'
 
-export interface AnalyticsConfig {
-  analytics: boolean,
+export interface AnalyticsConfigEnabled {
+  analytics: true,
   percentiles: boolean,
   api_analytics_retention_ms: number,
   api_analytics_retention: AnalyticsRetention,
   api_requests_retention_ms: number,
   api_requests_retention: RequestsRetention,
 }
+
+export interface AnalyticsConfigNoAnalytics {
+  analytics: false
+}
+
+export type AnalyticsConfig = AnalyticsConfigNoAnalytics | AnalyticsConfigEnabled
