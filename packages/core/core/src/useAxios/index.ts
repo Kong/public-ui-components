b23280bfafe8d0bdb77a79299e7d45038662fe57
Adam DeHaven <adam.dehaven@konghq.com>
2024-02-14 09:36:29 -0500

    fix(core): catch an improper inject and provide fallback

--- packages/core/core/src/useAxios/index.ts
+++ packages/core/core/src/useAxios/index.ts
@@ -8,14 +8,24 @@ export default function useAxios() {
    * @param {AxiosRequestConfig} options The axios request config
    * @returns {AxiosInstance} The axios instance
    */
-  const getAxiosInstance = inject<(options?: AxiosRequestConfig) => AxiosInstance>('get-axios-instance', (options: AxiosRequestConfig = {}): AxiosInstance => {
-    // Return a fallback instance
-    return axios.create({
+  const getAxiosInstance = (options?: AxiosRequestConfig) => {
+    const fallbackInstance: AxiosInstance = axios.create({
       withCredentials: true,
       timeout: 30000,
       ...options,
     })
-  })
+
+    try {
+      return inject<(options?: AxiosRequestConfig) => AxiosInstance>('get-axios-instance', (): AxiosInstance => {
+        // Return a fallback instance if the injection key is not provided
+        return fallbackInstance
+      })
+    } catch (err: any) {
+      console.warn('getAxiosInstance:', err.message || err)
+      // inject() can only be used inside setup() or functional components, so provide the fallback instance
+      return fallbackInstance
+    }
+  }
 
   /**
    * Get the `x-datadog-trace-id` header from the provided error, if it exists
