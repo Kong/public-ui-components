262c2ca854d3faaf385867c9423e0ed915821f5d
Rocky <adorack@users.noreply.github.com>
2024-03-06 17:48:27 -0500

    fix(dashboard-renderer): tweak default options [MA-2709] (#1214)
    - Don't stack timeseries charts by default.
    - If a timeseries chart is stacked, it should also be filled.
    - Don't autorefresh charts on the dashboard.

--- packages/analytics/dashboard-renderer/src/types/dashboard-renderer-types.ts
+++ packages/analytics/dashboard-renderer/src/types/dashboard-renderer-types.ts
@@ -85,9 +85,6 @@ export const timeseriesChartSchema = {
     stacked: {
       type: 'boolean',
     },
-    fill: {
-      type: 'boolean',
-    },
     chartDatasetColors: chartDatasetColorsSchema,
     syntheticsDataKey,
     chartTitle,
