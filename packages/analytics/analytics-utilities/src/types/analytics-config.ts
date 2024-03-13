dcd3dfeeb59a22e2d1d133c24375945ec3eeeca1
Alex Grant <alxgenegrant@gmail.com>
2024-03-13 14:48:58 -0400

    feat(analytics): add types for config V2 (#1235)

--- packages/analytics/analytics-utilities/src/types/analytics-config.ts
+++ packages/analytics/analytics-utilities/src/types/analytics-config.ts
@@ -1,6 +1,18 @@
 export type AnalyticsRetention = '1d' | '30d' | '90d' | '180d' | '365d'
 export type RequestsRetention = '1d' | '7d' | '14d' | '30d'
 
+export type AnalyticsRetentionMs =
+  | 86400000 // 1d
+  | 2592000000 // 30d
+  | 7776000000 // 90d
+  | 15552000000 // 180d
+  | 31536000000 // 365d
+export type RequestsRetentionMs =
+  | 86400000 // 1d
+  | 604800000 // 7d
+  | 1209600000 // 14d
+  | 2592000000 // 30d
+
 export interface AnalyticsConfigEnabled {
   analytics: true
   percentiles: boolean
@@ -17,6 +29,18 @@ export interface AnalyticsConfigNoAnalytics {
 export type AnalyticsConfig = AnalyticsConfigNoAnalytics | AnalyticsConfigEnabled
 
 export interface ControlPlaneConfig {
-  id: string
   analytics: boolean
+}
+export interface ApiAnalyticsV2 {
+  percentiles: boolean,
+  retention_ms: AnalyticsRetentionMs
+}
+
+export interface ApiRequestsV2 {
+  retention_ms: RequestsRetentionMs
+}
+
+export interface AnalyticsConfigV2 {
+  analytics: null | ApiAnalyticsV2,
+  requests: null | ApiRequestsV2
 }
