2759ad27dd79cf18ec5bb330d358755daab4d318
alxgrant <alxgenegrant@gmail.com>
2024-03-13 13:54:36 -0400

    feat(analytics): add types for config V2

--- packages/analytics/analytics-utilities/src/types/analytics-config.ts
+++ packages/analytics/analytics-utilities/src/types/analytics-config.ts
@@ -17,6 +17,30 @@ export interface AnalyticsConfigNoAnalytics {
 export type AnalyticsConfig = AnalyticsConfigNoAnalytics | AnalyticsConfigEnabled
 
 export interface ControlPlaneConfig {
-  id: string
   analytics: boolean
 }
+
+export interface AnalyticsConfigNoAnalyticsV2 {
+  analytics: null,
+  requests: null
+}
+
+export interface AnalyticsConfigBasicV2 {
+  analytics: {
+    percentiles: false,
+    retention: 86400000
+  },
+  requests: null
+}
+
+export interface AnalyticsConfigAdvancedV2 {
+  analytics: {
+    percentiles: true,
+    retention: number
+  },
+  requests: {
+    retention: number
+  }
+}
+
+export type AnalyticsConfigV2 = AnalyticsConfigNoAnalyticsV2 | AnalyticsConfigBasicV2 | AnalyticsConfigAdvancedV2
