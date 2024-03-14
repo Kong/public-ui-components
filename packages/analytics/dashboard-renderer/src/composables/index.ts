0aa3a6103ec5478f6a0419c88625ad67f87c0af9
mihai-peteu <mihai.peteu@konghq.com>
2024-03-14 13:16:49 -0500

    feat(dashboard-renderer): stub out support for EntityLink [MA-2716]

--- packages/analytics/dashboard-renderer/src/composables/index.ts
+++ packages/analytics/dashboard-renderer/src/composables/index.ts
@@ -1,6 +1,8 @@
+import useExternalLinkCreator from './useExternalLinkCreator'
 import useI18n from './useI18n'
 
 // All composables must be exported as part of the default object for Cypress test stubs
 export default {
+  useExternalLinkCreator,
   useI18n,
 }
