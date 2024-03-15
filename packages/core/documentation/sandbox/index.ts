bf49bedc5033affc020d3fe49e7ab93ecf9c7ab2
Kai Arrowood <kai.arrowood@konghq.com>
2024-03-15 13:51:20 -0400

    fix(documentation): mobile view [khcp-10984] (#1251)
    Fix for [KHCP-10984](https://konghq.atlassian.net/browse/KHCP-10984).
    Also adds a sandbox and fixes some TS issues.

--- packages/core/documentation/sandbox/index.ts
+++ packages/core/documentation/sandbox/index.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Kongponents from '@kong/kongponents'
+import '@kong/kongponents/dist/style.css'
 
 const app = createApp(App)
+
+app.use(Kongponents)
 
 app.mount('#app')
