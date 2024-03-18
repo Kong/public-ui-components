2e5cce45e2d852cf05223083cb9f5fdffa946bc2
Kai Arrowood <kai.arrowood@konghq.com>
2024-03-18 12:28:20 -0400

    fix(copy-uuid): deprecated warning (#1258)
    * chore(copy-uuid): deprecated warning
    
    * fix: trigger ci and release

--- packages/core/copy-uuid/src/components/CopyUuid.vue
+++ packages/core/copy-uuid/src/components/CopyUuid.vue
@@ -43,7 +43,7 @@
 
 <script setup lang="ts">
 import type { PropType } from 'vue'
-import { computed, ref, inject } from 'vue'
+import { computed, ref, inject, onMounted } from 'vue'
 import { createI18n } from '@kong-ui-public/i18n'
 import english from '../locales/en.json'
 import { COPY_UUID_NOTIFY_KEY } from '../constants'
@@ -175,6 +175,10 @@ const copyIdToClipboard = (executeCopy: (prop: string) => boolean) => {
     emit('success', props.uuid)
   }
 }
+
+onMounted(() => {
+  console.warn('DEPRECATED: @kong-ui-public/copy-uuid package is deprecated in favor of @kong/kongponents KCopy component')
+})
 </script>
 
 <style lang="scss" scoped>
