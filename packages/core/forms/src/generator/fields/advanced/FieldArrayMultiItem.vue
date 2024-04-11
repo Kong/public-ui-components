62de58c94e9578d7c729f87fc237d96d24acd5e6
Kai Arrowood <kai.arrowood@konghq.com>
2024-04-11 11:16:18 -0400

    feat(vfg): field-array-multi-item refactor [khcp-11348] (#1325)
    Convert to setup syntax and use design tokens.
    Address [KHCP-11348](https://konghq.atlassian.net/browse/KHCP-11348).

--- packages/core/forms/src/generator/fields/advanced/FieldArrayMultiItem.vue
+++ packages/core/forms/src/generator/fields/advanced/FieldArrayMultiItem.vue
@@ -4,7 +4,6 @@
     <KButton
       appearance="danger"
       class="delete"
-      type="input"
       @click="$emit('remove-item')"
     >
       - Remove Field
@@ -13,31 +12,14 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'FieldArrayMultiItem',
-  emits: ['remove-item'],
-  data() {
-    return {
-      contentVisible: false,
-    }
-  },
-}
+<script lang="ts" setup>
+defineEmits<{
+  (event: 'remove-item'): void,
+}>()
 </script>
 
 <style lang="scss" scoped>
-.vue-form-generator .field-wrap button.delete {
-  &, &:hover {
-    background: $kui-color-background-danger;
-    border: none;
-  }
-}
 .multi-item-wrapper {
   width: 100%;
-}
-.label-hint {
-  font-weight: 300;
-  padding: 8px;
-  text-align: center;
 }
 </style>
