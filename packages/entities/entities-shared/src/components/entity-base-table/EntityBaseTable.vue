478d3f1873206c0bcd9d17b55ce3cf609d4b10a7
Leopoldthecoder <leopoldthecuber@gmail.com>
2024-04-12 11:43:21 +0800

    fix(entities-shared): table toolbar width

--- packages/entities/entities-shared/src/components/entity-base-table/EntityBaseTable.vue
+++ packages/entities/entities-shared/src/components/entity-base-table/EntityBaseTable.vue
@@ -371,6 +371,7 @@ const handleUpdateTablePreferences = (tablePreferences: UserTablePreferences): v
 .kong-ui-entity-base-table {
   .toolbar-container {
     align-items: center;
+    width: 100%;
   }
 
   .toolbar-button-container {
