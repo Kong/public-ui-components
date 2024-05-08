ea04e129cc54be83eea1865752694062e3404ccd
mihai-peteu <mihai.peteu@konghq.com>
2024-05-08 11:20:24 -0700

    feat: nuke metric-cards yet again [MA-2757]

--- packages/analytics/metric-cards/src/constants.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-export enum MetricCardSize {
-  Small = 'sm',
-  Medium = 'md',
-  Large = 'lg',
-  LargeCompact = 'lgc',
-  ExtraLarge = 'xl'
-}
--- packages/analytics/metric-cards/src/enums/metric-card-type.enum.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-unused-vars */
-export enum MetricCardType {
-  GENERIC_COUNT = 'GenericCount',
-  TRAFFIC = 'Traffic',
-  ERROR_RATE = 'ErrorRate',
-  LATENCY = 'Latency',
-}
