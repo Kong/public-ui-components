9f30f2cdb17d7de1d128fec69b395cd23bea22ae
mihai-peteu <mihai.peteu@konghq.com>
2024-05-07 18:33:38 -0700

    feat: bring back metric-card stuff [MA-2757]

--- packages/analytics/metric-cards/sandbox/App.vue
+++ packages/analytics/metric-cards/sandbox/App.vue
@@ -17,6 +17,14 @@
         />
       </div>
 
+      <h3>Large - compact</h3>
+      <div class="generic-card">
+        <MetricCardContainer
+          v-bind="cardsRegularCompact"
+          hide-title
+        />
+      </div>
+
       <h3>Large - custom CSS theme</h3>
       <div class="generic-card dev-portal">
         <MetricCardContainer v-bind="cardsRegular" />
@@ -149,6 +157,14 @@ const cardsRegular: MetricCardContainerOptions = {
   hasTrendAccess: true,
   fallbackDisplayText: 'Not available',
   cardSize: MetricCardSize.Large,
+}
+
+const cardsRegularCompact: MetricCardContainerOptions = {
+  cards: [...cards].slice(0, 3),
+  loading: false,
+  hasTrendAccess: true,
+  fallbackDisplayText: 'Not available',
+  cardSize: MetricCardSize.LargeCompact,
 }
 
 const cardsXL: MetricCardContainerOptions = {
---
+++ packages/analytics/metric-cards/src/index.ts
@@ -0,0 +1,7 @@
+import MetricCardContainer from './components/MetricCardContainer.vue'
+export { MetricCardContainer }
+
+export * from './types'
+export * from './constants'
+export * from './enums'
+export * from './utilities'
