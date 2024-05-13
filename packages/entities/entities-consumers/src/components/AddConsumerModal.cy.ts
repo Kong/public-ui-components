d97a6a1e955bb70de36aec13c74b15840c0f8d64
portikM <maksym.portianoi@konghq.com>
2024-05-13 13:42:03 -0400

    test(kpop): update selectors

--- packages/entities/entities-consumers/src/components/AddConsumerModal.cy.ts
+++ packages/entities/entities-consumers/src/components/AddConsumerModal.cy.ts
@@ -106,9 +106,9 @@ describe('<AddConsumerModal/>', () => {
       cy.getTestId('multiselect-trigger').should('be.visible')
       cy.getTestId('multiselect-trigger').click()
 
-      cy.get('.k-popover-content .multiselect-list').should('be.visible')
-      cy.get('.k-popover-content .multiselect-list .multiselect-item').should('be.visible')
-      cy.get('.k-popover-content .multiselect-list .multiselect-item').should('have.length', 5)
+      cy.get('.popover-content .multiselect-list').should('be.visible')
+      cy.get('.popover-content .multiselect-list .multiselect-item').should('be.visible')
+      cy.get('.popover-content .multiselect-list .multiselect-item').should('have.length', 5)
 
       cy.get('@vueWrapper').then((wrapper: any) => wrapper.getComponent('.k-multiselect')
         .vm.$emit('update:modelValue', [consumers5[0].id]))
@@ -225,9 +225,9 @@ describe('<AddConsumerModal/>', () => {
       cy.getTestId('multiselect-trigger').should('be.visible')
       cy.getTestId('multiselect-trigger').click()
 
-      cy.get('.k-popover-content .multiselect-list').should('be.visible')
-      cy.get('.k-popover-content .multiselect-list .multiselect-item').should('be.visible')
-      cy.get('.k-popover-content .multiselect-list .multiselect-item').should('have.length', 5)
+      cy.get('.popover-content .multiselect-list').should('be.visible')
+      cy.get('.popover-content .multiselect-list .multiselect-item').should('be.visible')
+      cy.get('.popover-content .multiselect-list .multiselect-item').should('have.length', 5)
 
       cy.get('@vueWrapper').then((wrapper: any) => wrapper.getComponent('.k-multiselect')
         .vm.$emit('update:modelValue', [consumers5[0].id]))
