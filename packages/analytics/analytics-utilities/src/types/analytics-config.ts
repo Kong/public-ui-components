1afa0d3ef78ac133d003a38fd0c6c9d4d24a0606
alxgrant <alxgenegrant@gmail.com>
2024-05-16 10:37:43 -0400

    feat: allow 425 days of retention

--- packages/analytics/analytics-utilities/src/types/analytics-config.ts
+++ packages/analytics/analytics-utilities/src/types/analytics-config.ts
@@ -5,6 +5,7 @@ export type AnalyticsRetentionMs =
   | 7776000000 // 90d
   | 15552000000 // 180d
   | 31536000000 // 365d
+  | 36720000000 // 425d
 export type RequestsRetentionMs =
   | 86400000 // 1d
   | 604800000 // 7d
