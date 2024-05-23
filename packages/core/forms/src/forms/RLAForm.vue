dd83a6a52d63df8df532c75033c1250d1e94ad25
Makito <i@maki.to>
2024-05-24 02:23:15 +0800

    feat(forms): implement use cases for RLA

--- packages/core/forms/src/forms/RLAForm.vue
+++ packages/core/forms/src/forms/RLAForm.vue
@@ -7,15 +7,12 @@
       @model-updated="(value: any, model: string) => onModelUpdated(value, model)"
     />
 
-    <KCard
-      class="rla-form-request-limits"
-    >
+    <KCard class="rla-form-request-limits">
       <template #title>
         <KLabel
           class="rla-form-request-limits-title"
           for="rla-form-request-limits-legend"
           :info="t('rla.request_limits.help')"
-          required
           :tooltip-attributes="{
             maxWidth: '300',
             placement: 'top',
@@ -23,72 +20,120 @@
         >
           {{ t('rla.request_limits.title') }}
         </KLabel>
+
         <div class="rla-form-request-limits-subtitle">
           {{ t('rla.request_limits.subtitle') }}
         </div>
       </template>
 
-      <div class="rla-form-request-limits-items">
-        <div
-          v-for="(requestLimit, index) in requestLimits"
-          :key="index.toString()"
-        >
+      <div class="rla-form-request-limits-content">
+        <div class="rla-form-window-type">
           <KLabel
-            :for="`rla-form-request-limits-item-${index}-legend`"
+            class="rla-form-window-type-label"
+            :tooltip-attributes="{
+              maxWidth: '300',
+              placement: 'top',
+            }"
           >
-            {{ t('rla.request_limits.label', { index: index + 1 }) }}
+            {{ t('rla.window_type.label') }}
+
+            <template #tooltip>
+              <!-- eslint-disable-next-line vue/no-v-html -->
+              <div v-html="t('rla.window_type.help')" />
+            </template>
           </KLabel>
-          <div class="rla-form-request-limits-row">
-            <legend
-              :id="`rla-form-request-limits-item-${index}-legend`"
-              class="rla-form-request-limits-inputs"
+
+          <div class="rla-form-window-type-radios">
+            <KRadio
+              v-for="wt in availableWindowTypes"
+              :key="`window-type-${wt}`"
+              :model-value="windowType"
+              :name="wt"
+              :selected-value="wt"
+              @update:model-value="updateWindowType"
             >
-              <KInput
-                :model-value="requestLimit.limit && requestLimit.limit.toString()"
-                :placeholder="t('rla.request_limits.request_number')"
-                type="number"
-                @update:model-value="(value: string) => updateRequestLimitLimit(index, Number.parseInt(value))"
-              />
-              <div>{{ t('rla.request_limits.interval_determiner') }}</div>
-              <KInput
-                :model-value="requestLimit.windowSize && requestLimit.windowSize.toString()"
-                :placeholder="t('rla.request_limits.time_interval')"
-                type="number"
-                @update:model-value="(value: string) => updateRequestLimitWindowSize(index, Number.parseInt(value))"
-              />
-              <div>{{ t('rla.request_limits.seconds') }}</div>
-            </legend>
+              {{ t(`rla.window_type.options.${wt}` as Parameters<typeof t>[0]) }}
+            </KRadio>
+          </div>
+        </div>
 
-            <div class="rla-form-request-limits-actions">
-              <KButton
-                appearance="tertiary"
-                :disabled="requestLimits.length <= 1"
-                @click="() => removeRequestLimit(index)"
+        <div class="rla-form-request-limits-items">
+          <div
+            v-for="(requestLimit, index) in requestLimits"
+            :key="index.toString()"
+          >
+            <KLabel :for="`rla-form-request-limits-item-${index}-legend`">
+              {{ t('rla.request_limits.label_index', { index: index + 1 }) }}
+            </KLabel>
+            <div class="rla-form-request-limits-row">
+              <legend
+                :id="`rla-form-request-limits-item-${index}-legend`"
+                class="rla-form-request-limits-inputs"
               >
-                <RemoveIcon />
-              </KButton>
+                <KInput
+                  :model-value="requestLimit.limit && requestLimit.limit.toString()"
+                  :placeholder="t('rla.request_limits.request_number')"
+                  type="number"
+                  @update:model-value="(value: string) => updateRequestLimitLimit(index, Number.parseInt(value))"
+                />
+                <div>{{ t('rla.request_limits.interval_determiner') }}</div>
+                <KInput
+                  :model-value="requestLimit.windowSize && requestLimit.windowSize.toString()"
+                  :placeholder="t('rla.request_limits.time_interval')"
+                  type="number"
+                  @update:model-value="(value: string) => updateRequestLimitWindowSize(index, Number.parseInt(value))"
+                />
+                <div>{{ t('rla.request_limits.seconds') }}</div>
+              </legend>
 
-              <KButton
-                appearance="tertiary"
-                @click="() => addRequestLimit(index)"
-              >
-                <AddIcon />
-              </KButton>
+              <div class="rla-form-request-limits-actions">
+                <KButton
+                  appearance="tertiary"
+                  :disabled="requestLimits.length <= 1"
+                  @click="() => removeRequestLimit(index)"
+                >
+                  <RemoveIcon />
+                </KButton>
+
+                <KButton
+                  appearance="tertiary"
+                  @click="() => addRequestLimit(index)"
+                >
+                  <AddIcon />
+                </KButton>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div class="rla-form-request-limits-examples">
-        <div>Examples:</div>
-        <ol>
-          <li
-            v-for="(example, i) in EXAMPLES"
-            :key="`rla-example-${i}`"
-          >
-            {{ example.description }}<br>Request number: <code>{{ example.config.limit }}</code>, Time interval: <code>{{ example.config.window_size }}</code>, Window type: <code>{{ example.config.window_type }}</code>
-          </li>
-        </ol>
+        <div
+          v-if="filteredUseCases.length > 0"
+          class="rla-form-request-limits-examples"
+        >
+          <div>{{ t('rla.try_a_use_case') }}:</div>
+          <div class="rla-form-request-limits-examples-badges">
+            <KTooltip
+              v-for="(useCase, i) in filteredUseCases"
+              :key="`use-case-${i}`"
+              max-width="300"
+            >
+              <KBadge
+                :appearance="selectedUseCase === `${windowType}-${i}` ? 'info' : 'decorative'"
+                class="rla-form-request-limits-examples-badge"
+                @click="() => toggleUseCase(useCase, `${windowType}-${i}`)"
+              >
+                {{ t('rla.use_case_index', { index: i + 1 }) }}
+              </KBadge>
+
+              <template #content>
+                <div>{{ useCase.description }}</div>
+                <br>
+                <div>{{ t('rla.request_limits.label') }}: {{ useCase.config.limit }}</div>
+                <div>{{ t('rla.request_limits.time_interval') }}: {{ useCase.config.window_size }}</div>
+              </template>
+            </KTooltip>
+          </div>
+        </div>
       </div>
     </KCard>
 
@@ -103,7 +148,6 @@
           placement: 'top',
         },
       }"
-      required
       @selected="(item: any) => props.onModelUpdated(item.value, 'config-identifier')"
     >
       <template #label-tooltip>
@@ -124,6 +168,7 @@
       >
         {{ t('rla.error_message.label') }}
       </KLabel>
+
       <legend
         id="rla-form-error-legend"
         class="rla-form-error-inputs"
@@ -163,10 +208,19 @@ import { createI18n } from '@kong-ui-public/i18n'
 import { AddIcon, RemoveIcon } from '@kong/icons'
 import type { SelectItem } from '@kong/kongponents'
 import cloneDeep from 'lodash-es/cloneDeep'
-import { computed, ref } from 'vue'
+import { computed, nextTick, ref } from 'vue'
 import english from '../locales/en.json'
 
-const EXAMPLES = [
+interface UseCase {
+  description: string
+  config: {
+    limit: number
+    window_size:number
+    window_type: string
+  }
+}
+
+const USE_CASES = [
   {
     description: 'A fixed limit of 500 requests per hour resetting sharply on the hour, ensuring no user can exceed this limit.',
     config: {
@@ -266,6 +320,10 @@ const advancedSchema = computed(() => {
   }
 })
 
+const filteredUseCases = computed(() =>
+  USE_CASES.filter((example) => props.formModel['config-window_type'] === example.config.window_type),
+)
+
 const identifierField = computed(() => props.formSchema?.fields?.find((field: any) => field.model === 'config-identifier'))
 
 const identifierSelectItems = computed<SelectItem[]>(() =>
@@ -276,7 +334,23 @@ const identifierSelectItems = computed<SelectItem[]>(() =>
     })),
 )
 
+const selectedUseCase = ref<string | undefined>()
 const advancedCollapsed = ref(true)
+
+const windowType = computed<string>(() =>
+  props.formModel['config-window_type'] ?? props.formSchema?.fields
+    ?.find((field: any) => field.model === 'config-window_type')?.default,
+)
+
+const availableWindowTypes = computed<string[]>(() => {
+  const defaultValue = props.formSchema?.fields?.find((field: any) => field.model === 'config-window_type')?.default
+
+  return (props.formSchema?.fields
+    ?.find((field: any) => field.model === 'config-window_type')?.values ?? [])
+    .sort((a: string, b: string) => {
+      return a === defaultValue ? -1 : b === defaultValue ? 1 : 0
+    })
+})
 
 const requestLimits = computed<RequestLimit[]>(() => {
   const modelValue = props.formModel['config-limit']?.map((limit: number, index: number) => ({
@@ -291,23 +365,65 @@ const requestLimits = computed<RequestLimit[]>(() => {
   return modelValue
 })
 
-const identifier = computed<string | undefined>(() => props.formModel?.['config-identifier'] ?? props.formSchema?.fields?.find((field: any) => field.model === 'config-identifier')?.default)
-const errorCode = computed<number | undefined>(() => props.formModel?.['config-error_code'] ?? props.formSchema?.fields?.find((field: any) => field.model === 'config-error_code')?.default)
-const errorMessage = computed<string | undefined>(() => props.formModel?.['config-error_message'] ?? props.formSchema?.fields?.find((field: any) => field.model === 'config-error_message')?.default)
+const identifier = computed<string | undefined>(() =>
+  props.formModel?.['config-identifier'] ?? props.formSchema?.fields
+    ?.find((field: any) => field.model === 'config-identifier')?.default,
+)
+const errorCode = computed<number | undefined>(() =>
+  props.formModel?.['config-error_code'] ?? props.formSchema?.fields
+    ?.find((field: any) => field.model === 'config-error_code')?.default,
+)
+const errorMessage = computed<string | undefined>(() =>
+  props.formModel?.['config-error_message'] ?? props.formSchema?.fields
+    ?.find((field: any) => field.model === 'config-error_message')?.default,
+)
+
+const toggleUseCase = (useCase: UseCase, useCaseKey: string) => {
+  if (useCaseKey === selectedUseCase.value) {
+    // If selected, clear the list
+    props.onModelUpdated([], 'config-limit')
+    props.onModelUpdated([], 'config-window_size')
+    nextTick(() => {
+      selectedUseCase.value = undefined
+    })
+    return
+  }
+  props.onModelUpdated([useCase.config.limit], 'config-limit')
+  props.onModelUpdated([useCase.config.window_size], 'config-window_size')
+  nextTick(() => {
+    selectedUseCase.value = useCaseKey
+  })
+}
+
+const updateWindowType = (newType: string) => {
+  if (selectedUseCase.value !== undefined) {
+    selectedUseCase.value = undefined
+  }
+  props.onModelUpdated(newType, 'config-window_type')
+}
 
 const updateRequestLimitLimit = (index: number, limit?: number) => {
+  if (selectedUseCase.value !== undefined) {
+    selectedUseCase.value = undefined
+  }
   const limits = cloneDeep(props.formModel?.['config-limit'] ?? [])
   limits[index] = limit
   props.onModelUpdated(limits, 'config-limit')
 }
 
 const updateRequestLimitWindowSize = (index: number, windowSize?: number) => {
+  if (selectedUseCase.value !== undefined) {
+    selectedUseCase.value = undefined
+  }
   const windowSizes = cloneDeep(props.formModel?.['config-window_size'] ?? [])
   windowSizes[index] = windowSize
   props.onModelUpdated(windowSizes, 'config-window_size')
 }
 
 const addRequestLimit = (index: number) => {
+  if (selectedUseCase.value !== undefined) {
+    selectedUseCase.value = undefined
+  }
   const limits = cloneDeep(props.formModel?.['config-limit'] ?? [])
   const windowSizes = cloneDeep(props.formModel?.['config-window_size'] ?? [])
   if (limits.length === 0) {
@@ -324,6 +440,9 @@ const addRequestLimit = (index: number) => {
 
 const removeRequestLimit = (index: number) => {
   if (requestLimits.value.length > 1) {
+    if (selectedUseCase.value !== undefined) {
+      selectedUseCase.value = undefined
+    }
     const limits = cloneDeep(props.formModel?.['config-limit'] ?? [])
     const windowSizes = cloneDeep(props.formModel?.['config-window_size'] ?? [])
     if (limits.length === 0) {
@@ -348,6 +467,25 @@ const removeRequestLimit = (index: number) => {
   margin-bottom: $kui-space-80;
 
   .rla-form-request-limits {
+    .rla-form-request-limits-content {
+      display: flex;
+      flex-direction: column;
+      gap: $kui-space-50;
+    }
+
+    .rla-form-window-type {
+      &-radios {
+        display: flex;
+        flex-direction: row;
+        gap: $kui-space-70;
+
+        :deep(.radio-label) {
+          font-size: $kui-font-size-30;
+          font-weight: $kui-font-weight-regular;
+        }
+      }
+    }
+
     &-title {
       font-size: $kui-font-size-40;
       font-weight: $kui-font-weight-bold;
@@ -388,17 +526,24 @@ const removeRequestLimit = (index: number) => {
     }
 
     &-examples {
-      color: $kui-color-text-neutral;
-      margin-bottom: $kui-space-50;
-      margin-top: $kui-space-50;
+      align-items: center;
+      display: flex;
+      flex-direction: row;
+      gap: $kui-space-40;
 
-      ol {
-        margin-bottom: 0;
-        margin-top: 0;
+      &-badges {
+        display: flex;
+        flex-direction: row;
+        gap: $kui-space-40;
+
+        :deep(.rla-form-request-limits-examples-badge) {
+          cursor: pointer;
+        }
       }
     }
   }
 
+  .rla-form-window-type-label,
   .rla-form-identifiers {
     :deep(.k-tooltip p) {
       margin: 0;
