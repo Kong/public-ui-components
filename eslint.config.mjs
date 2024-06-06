8d87ca6bdb18a62e52c68d89451a670ea55f076c
Adam DeHaven <adam.dehaven@konghq.com>
2024-06-06 09:37:54 -0400

    fix: do not lint json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,8 @@
 import eslintKongUiConfig from '@kong/eslint-config-kong-ui'
-import eslintKongUiConfigJson from '@kong/eslint-config-kong-ui/json'
 import eslintKongUiConfigCypress from '@kong/eslint-config-kong-ui/cypress'
 
 export default [
   ...eslintKongUiConfig,
-  // Only apply the shared JSON config to files that match the given pattern
-  ...eslintKongUiConfigJson.map(config => ({
-    ...config,
-    files: ['tokens/**/*.json'],
-  })),
   ...eslintKongUiConfigCypress.map(config => ({
     ...config,
     files: [
