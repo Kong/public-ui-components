16c122438c48fa4ea820beb14c2bda2e541d61a6
tehkai <kai.arrowood@konghq.com>
2024-08-06 14:33:40 -0400

    feat(vfg): convert FieldCheckbox

--- packages/core/forms/src/components/fields/FieldCheckbox.vue
+++ packages/core/forms/src/components/fields/FieldCheckbox.vue
@@ -1,13 +1,78 @@
-<template lang="pug">
-input(:id="getFieldID(schema)", type="checkbox", v-model="value", :autocomplete="schema.autocomplete", :disabled="disabled || null", :name="schema.inputName", :class="schema.fieldClasses", :required="schema.required", v-attributes="'input'")
+<template>
+  <div class="form-field-wrapper">
+    <KCheckbox
+      v-bind="$attrs"
+      :id="getFieldID(schema)"
+      :autocomplete="schema.autocomplete"
+      :class="schema.fieldClasses"
+      :disabled="disabled || null"
+      :help="hint ? hint : undefined"
+      :model-value="inputValue"
+      :name="schema.inputName"
+      :readonly="schema.readonly"
+      :required="schema.required"
+      :width="schema.width"
+    />
+  </div>
 </template>
 
-<script lang="ts">
-import abstractField from './abstractField'
+<script lang="ts" setup>
+import { toRefs, type PropType } from 'vue'
+import composables from '../../composables'
 
-export default {
-  mixins: [abstractField],
-}
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  formOptions: {
+    type: Object as PropType<Record<string, any>>,
+    default: () => undefined,
+  },
+  model: {
+    type: Object as PropType<Record<string, any>>,
+    default: () => undefined,
+  },
+  schema: {
+    type: Object as PropType<Record<string, any>>,
+    required: true,
+  },
+  vfg: {
+    type: Object,
+    required: true,
+  },
+  /**
+   * TODO: stronger type
+   * TODO: pass this down to KInput error and errorMessage
+   */
+  errors: {
+    type: Array,
+    default: () => [],
+  },
+  hint: {
+    type: String,
+    default: '',
+  },
+})
+
+const emit = defineEmits<{
+  (event: 'modelUpdated', value: any, model: Record<string, any>): void
+}>()
+
+const propsRefs = toRefs(props)
+
+const { getFieldID, value: inputValue, clearValidationErrors } = composables.useAbstractFields({
+  model: propsRefs.model,
+  schema: props.schema,
+  formOptions: props.formOptions,
+  emitModelUpdated: (data: { value: any, model: Record<string, any> }): void => {
+    emit('modelUpdated', data.value, data.model)
+  },
+})
+
+defineExpose({
+  clearValidationErrors,
+})
 </script>
 
 <style lang="scss">
