b1ce143caea3b49062eb79e5e37880a5ce55c55d
Zehao Zhang <zehao93@outlook.com>
2024-09-26 11:04:54 +0800

    fix(entities-plugins): missing of action buttons [KM-547]

---
+++ packages/entities/entities-plugins/src/components/PluginFormActionsWrapper.vue
@@ -0,0 +1,20 @@
+<template>
+  <Teleport
+    v-if="teleportTarget"
+    :to="teleportTarget"
+  >
+    <slot />
+  </Teleport>
+  <div v-else>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  teleportTarget: {
+    type: String,
+    default: '',
+  },
+})
+</script>
