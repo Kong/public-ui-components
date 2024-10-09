b78b0be2da04802a7384461d96f64470f3c5c9e5
Filip Gutica <filgut@protonmail.com>
2024-10-09 12:29:12 -0700

    fix: add unit test for sorting

--- packages/analytics/analytics-chart/src/composables/useExploreResultToDatasets.spec.ts
+++ packages/analytics/analytics-chart/src/composables/useExploreResultToDatasets.spec.ts
@@ -178,214 +178,300 @@ describe('useVitalsExploreDatasets', () => {
     )
   })
 
-})
-
-it('handles no dimension', () => {
-  const exploreResult: ComputedRef<ExploreResultV4> = computed(() => ({
-    data: [
-      {
-        timestamp: '2022-01-01T01:01:02Z',
-        event: {
-          request_count: 1,
+  it('handles no dimension', () => {
+    const exploreResult: ComputedRef<ExploreResultV4> = computed(() => ({
+      data: [
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            request_count: 1,
+          },
         },
-      },
-    ],
-    meta: {
-      start_ms: 1640998862000,
-      end_ms: 1640998870000,
-      granularity_ms: 8000,
-      metric_names: ['request_count'],
-      query_id: '',
-      metric_units: { request_count: 'units' },
-      truncated: false,
-      limit: 15,
-      display: {},
-    },
-  }))
-  const result = useExploreResultToDatasets({ fill: true }, exploreResult)
-
-  expect(result.value).toEqual(
-    {
-      labels: ['Request Count'],
-      datasets: [
-        { label: 'Request Count', backgroundColor: '#a86cd5', data: [1] },
       ],
-    },
-  )
-})
+      meta: {
+        start_ms: 1640998862000,
+        end_ms: 1640998870000,
+        granularity_ms: 8000,
+        metric_names: ['request_count'],
+        query_id: '',
+        metric_units: { request_count: 'units' },
+        truncated: false,
+        limit: 15,
+        display: {},
+      },
+    }))
+    const result = useExploreResultToDatasets({ fill: true }, exploreResult)
 
-it('handles multiple metrics with no dimension', () => {
-  const exploreResult: ComputedRef<ExploreResultV4> = computed(() => ({
-    data: [
+    expect(result.value).toEqual(
       {
-        timestamp: '2022-01-01T01:01:02Z',
-        event: {
-          metric1: 1,
-          metric2: 2,
-        },
-      } as GroupByResult,
-    ],
-    meta: {
-      start_ms: 1640998862000,
-      end_ms: 1640998870000,
-      granularity_ms: 8000,
-      metric_names: [
-        'metric1',
-        'metric2',
-      ] as any as ExploreAggregations[],
-      query_id: '',
-      metric_units: { metric1: 'units', metric2: 'units' },
-      truncated: false,
-      limit: 15,
-      display: {},
-    } as QueryResponseMeta,
-  }))
-  const result = useExploreResultToDatasets({ fill: true }, exploreResult)
+        labels: ['Request Count'],
+        datasets: [
+          { label: 'Request Count', backgroundColor: '#a86cd5', data: [1] },
+        ],
+      },
+    )
+  })
 
-  expect(result.value).toEqual(
-    {
-      labels: ['metric1', 'metric2'],
-      datasets: [
-        { label: 'metric1', backgroundColor: '#a86cd5', data: [1, null] },
-        { label: 'metric2', backgroundColor: '#6a86d2', data: [null, 2] },
+  it('handles multiple metrics with no dimension', () => {
+    const exploreResult: ComputedRef<ExploreResultV4> = computed(() => ({
+      data: [
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            metric1: 1,
+            metric2: 2,
+          },
+        } as GroupByResult,
       ],
-    },
-  )
-})
+      meta: {
+        start_ms: 1640998862000,
+        end_ms: 1640998870000,
+        granularity_ms: 8000,
+        metric_names: [
+          'metric1',
+          'metric2',
+        ] as any as ExploreAggregations[],
+        query_id: '',
+        metric_units: { metric1: 'units', metric2: 'units' },
+        truncated: false,
+        limit: 15,
+        display: {},
+      } as QueryResponseMeta,
+    }))
+    const result = useExploreResultToDatasets({ fill: true }, exploreResult)
 
-it('handles multiple metrics with dimension', () => {
-  const exploreResult: ComputedRef<ExploreResultV4> = computed(() => ({
-    data: [
+    expect(result.value).toEqual(
       {
-        timestamp: '2022-01-01T01:01:02Z',
-        event: {
-          metric1: 1,
-          metric2: 2,
-          gateway_service: 'service1',
-        },
+        labels: ['metric1', 'metric2'],
+        datasets: [
+          { label: 'metric1', backgroundColor: '#a86cd5', data: [1, null] },
+          { label: 'metric2', backgroundColor: '#6a86d2', data: [null, 2] },
+        ],
       },
-      {
-        timestamp: '2022-01-01T01:01:02Z',
-        event: {
-          metric1: 3,
-          metric2: 4,
-          gateway_service: 'service2',
-        },
-      },
-    ],
-    meta: {
-      start_ms: 1640998862000,
-      end_ms: 1640998870000,
-      granularity_ms: 8000,
-      metric_names: [
-        'metric1',
-        'metric2',
-      ] as any as ExploreAggregations[],
-      query_id: '',
-      metric_units: { metric1: 'units', metric2: 'units' },
-      truncated: false,
-      limit: 15,
-      display: {
-        gateway_service: {
-          service1: { name: 'service1' },
-          service2: { name: 'service2' },
-        },
-      },
-    } as QueryResponseMeta,
-  }))
-  const result = useExploreResultToDatasets({ fill: true }, exploreResult)
+    )
+  })
 
-  expect(result.value).toEqual(
-    {
-      labels: ['service2', 'service1'],
-      datasets: [
-        { label: 'metric1', backgroundColor: '#a86cd5', data: [3, 1] },
-        { label: 'metric2', backgroundColor: '#6a86d2', data: [4, 2] },
+  it('handles multiple metrics with dimension', () => {
+    const exploreResult: ComputedRef<ExploreResultV4> = computed(() => ({
+      data: [
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            metric1: 1,
+            metric2: 2,
+            gateway_service: 'service1',
+          },
+        },
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            metric1: 3,
+            metric2: 4,
+            gateway_service: 'service2',
+          },
+        },
       ],
-    },
-  )
-})
+      meta: {
+        start_ms: 1640998862000,
+        end_ms: 1640998870000,
+        granularity_ms: 8000,
+        metric_names: [
+          'metric1',
+          'metric2',
+        ] as any as ExploreAggregations[],
+        query_id: '',
+        metric_units: { metric1: 'units', metric2: 'units' },
+        truncated: false,
+        limit: 15,
+        display: {
+          gateway_service: {
+            service1: { name: 'service1' },
+            service2: { name: 'service2' },
+          },
+        },
+      } as QueryResponseMeta,
+    }))
+    const result = useExploreResultToDatasets({ fill: true }, exploreResult)
 
-it('Correct status code colors', () => {
-  const exploreResult: ComputedRef<ExploreResultV4> = computed(() => ({
-    data: [
+    expect(result.value).toEqual(
       {
-        timestamp: '2022-01-01T01:01:02Z',
-        event: {
-          status_code: 100,
-          metric1: 2,
-        },
-      } as GroupByResult,
-      {
-        timestamp: '2022-01-01T01:01:02Z',
-        event: {
-          status_code: 200,
-          metric1: 2,
-        },
-      } as GroupByResult,
-      {
-        timestamp: '2022-01-01T01:01:02Z',
-        event: {
-          status_code: 300,
-          metric1: 2,
-        },
-      } as GroupByResult,
-      {
-        timestamp: '2022-01-01T01:01:02Z',
-        event: {
-          status_code: 400,
-          metric1: 2,
-        },
-      } as GroupByResult,
-      {
-        timestamp: '2022-01-01T01:01:02Z',
-        event: {
-          status_code: 500,
-          metric1: 2,
-        },
-      } as GroupByResult,
-    ],
-    meta: {
-      start_ms: 1640998862000,
-      end_ms: 1640998870000,
-      granularity_ms: 86400000,
-      metric_names: ['metric1'] as any as ExploreAggregations[],
-      display: {
-        status_code: {
-          100: {
-            name: '100',
-            deleted: false,
-          },
-          200: {
-            name: '200',
-            deleted: false,
-          },
-          300: {
-            name: '300',
-            deleted: false,
-          },
-          400: {
-            name: '400',
-            deleted: false,
-          },
-          500: {
-            name: '500',
-            deleted: false,
-          },
-        },
+        labels: ['service2', 'service1'],
+        datasets: [
+          { label: 'metric1', backgroundColor: '#a86cd5', data: [3, 1] },
+          { label: 'metric2', backgroundColor: '#6a86d2', data: [4, 2] },
+        ],
       },
-      query_id: '',
-      metric_units: { metric: 'units' } as MetricUnit,
-    },
-  }))
-  const result = useExploreResultToDatasets(
-    { fill: false, colorPalette: defaultStatusCodeColors },
-    exploreResult,
-  )
+    )
+  })
 
-  expect(result.value.datasets[0].backgroundColor).toEqual('#80bfff')
-  expect(result.value.datasets[1].backgroundColor).toEqual('#9edca6')
-  expect(result.value.datasets[2].backgroundColor).toEqual('#ffe9b8')
-  expect(result.value.datasets[3].backgroundColor).toEqual('#ffd5b1')
-  expect(result.value.datasets[4].backgroundColor).toEqual('#ffb6b6')
+  it('Correct status code colors', () => {
+    const exploreResult: ComputedRef<ExploreResultV4> = computed(() => ({
+      data: [
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            status_code: 100,
+            metric1: 2,
+          },
+        } as GroupByResult,
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            status_code: 200,
+            metric1: 2,
+          },
+        } as GroupByResult,
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            status_code: 300,
+            metric1: 2,
+          },
+        } as GroupByResult,
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            status_code: 400,
+            metric1: 2,
+          },
+        } as GroupByResult,
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            status_code: 500,
+            metric1: 2,
+          },
+        } as GroupByResult,
+      ],
+      meta: {
+        start_ms: 1640998862000,
+        end_ms: 1640998870000,
+        granularity_ms: 86400000,
+        metric_names: ['metric1'] as any as ExploreAggregations[],
+        display: {
+          status_code: {
+            100: {
+              name: '100',
+              deleted: false,
+            },
+            200: {
+              name: '200',
+              deleted: false,
+            },
+            300: {
+              name: '300',
+              deleted: false,
+            },
+            400: {
+              name: '400',
+              deleted: false,
+            },
+            500: {
+              name: '500',
+              deleted: false,
+            },
+          },
+        },
+        query_id: '',
+        metric_units: { metric: 'units' } as MetricUnit,
+      },
+    }))
+    const result = useExploreResultToDatasets(
+      { fill: false, colorPalette: defaultStatusCodeColors },
+      exploreResult,
+    )
+
+    expect(result.value.datasets[0].backgroundColor).toEqual('#80bfff')
+    expect(result.value.datasets[1].backgroundColor).toEqual('#9edca6')
+    expect(result.value.datasets[2].backgroundColor).toEqual('#ffe9b8')
+    expect(result.value.datasets[3].backgroundColor).toEqual('#ffd5b1')
+    expect(result.value.datasets[4].backgroundColor).toEqual('#ffb6b6')
+  })
+
+
+  it('sorts datasets accordingly', () => {
+    const exploreResult: ComputedRef<ExploreResultV4> = computed(() => ({
+      data: [
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            route: 'route1',
+            status_code: 100,
+            metric1: 1,
+          },
+        } as GroupByResult,
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            route: 'route1',
+            status_code: 200,
+            metric1: 2,
+          },
+        } as GroupByResult,
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            route: 'route2',
+            status_code: 100,
+            metric1: 3,
+          },
+        } as GroupByResult,
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            route: 'route2',
+            status_code: 200,
+            metric1: 4,
+          },
+        } as GroupByResult,
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            route: 'route3',
+            status_code: 100,
+            metric1: 5,
+          },
+        } as GroupByResult,
+        {
+          timestamp: '2022-01-01T01:01:02Z',
+          event: {
+            route: 'route3',
+            status_code: 200,
+            metric1: 6,
+          },
+        } as GroupByResult,
+      ],
+      meta: {
+        start_ms: 1640998862000,
+        end_ms: 1640998870000,
+        granularity_ms: 86400000,
+        metric_names: ['metric1'] as any as ExploreAggregations[],
+        display: {
+          route: {
+            route1: { name: 'route1' },
+            route2: { name: 'route2' },
+            route3: { name: 'route3' },
+          },
+          status_code: {
+            100: {
+              name: '100',
+              deleted: false,
+            },
+            200: {
+              name: '200',
+              deleted: false,
+            },
+          },
+        },
+        query_id: '',
+        metric_units: { metric: 'units' } as MetricUnit,
+      },
+    }))
+    const result = useExploreResultToDatasets(
+      { fill: false, colorPalette: defaultStatusCodeColors },
+      exploreResult,
+    )
+
+    expect(result.value.labels).toEqual(['route3', 'route2', 'route1'])
+  })
 })
