46c5276d08b8b01625f487892669567a8ce973c0
Filip Gutica <filgut@protonmail.com>
2024-10-21 15:05:30 -0700

    feat: add map chart type

--- packages/analytics/analytics-utilities/src/types/chart-types.ts
+++ packages/analytics/analytics-utilities/src/types/chart-types.ts
@@ -2,6 +2,7 @@ export const reportChartTypes = [
   'horizontal_bar',
   'vertical_bar',
   'timeseries_line',
+  'choropleth_map',
 ] as const
 
 export type ReportChartTypes = typeof reportChartTypes[number]
