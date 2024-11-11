f29068933c83b787082e77fdb6f7fbd2bb7a685e
Justineo <justice360@gmail.com>
2024-11-11 15:08:53 +0800

    fix(i18n): unit test

--- packages/core/i18n/src/i18n.spec.ts
+++ packages/core/i18n/src/i18n.spec.ts
@@ -139,7 +139,7 @@ describe('i18n', () => {
       expect(october.substring(0, 7)).toBe('Oct 3, ')
       expect(november.substring(0, 7)).toBe('Nov 14,')
       expect(decimalTimestamp).toEqual(integerTimestamp)
-      expect(timestampInMs).toBe('Nov 13, 2018, 0:08 AM')
+      expect(timestampInMs).toBe('Nov 13, 2018, 2:13 PM')
     })
   })
 
