830c0b765490578e98085a427a7f329c8b71aea6
Leopoldthecoder <leopoldthecuber@gmail.com>
2024-11-25 17:46:02 +0800

    fix(*): address feedback

--- packages/core/forms/src/components/fields/FieldObjectAdvanced.vue
+++ packages/core/forms/src/components/fields/FieldObjectAdvanced.vue
@@ -219,7 +219,7 @@ export default {
         Array.isArray(this.subSchema.fields) &&
         this.subSchema.fields.length === 1 &&
         this.subSchema.fields[0].type === 'array'
-      const defaultValue = this.schema.values?.default ?? ''
+      const defaultValue = this.schema.values?.default ?? (this.valueInputType === 'number' ? 0 : '')
       const initialValue = this.subSchema ? valueIsArray ? [''] : {} : defaultValue
 
       this.value[this.newKeyName] = initialValue
