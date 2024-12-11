993ef771c9873bc974cc842e593e5a7937f5c84f
Rocky Fischer <adorack@users.noreply.github.com>
2024-12-11 16:50:35 -0500

    feat(analytics): specific defaults for granularities [MA-3507]
    Allow specifying default values for fine-grained relative times.

--- packages/analytics/analytics-utilities/src/types/timeframe-options.ts
+++ packages/analytics/analytics-utilities/src/types/timeframe-options.ts
@@ -12,4 +12,5 @@ export interface TimeframeOptions {
   startCustom?: Date
   endCustom?: Date
   allowedGranularitiesOverride?: GranularityValues[]
+  fineGrainedDefaultGranularity?: GranularityValues
 }
