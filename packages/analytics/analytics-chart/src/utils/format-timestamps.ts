255b51ad95a13fd9b39e6e6977bcdd110d18e594
Filip Gutica <filgut@protonmail.com>
2024-12-11 21:41:45 -0800

    fix: unit test

--- packages/analytics/analytics-chart/src/utils/format-timestamps.ts
+++ packages/analytics/analytics-chart/src/utils/format-timestamps.ts
@@ -3,24 +3,29 @@ import { formatInTimeZone } from 'date-fns-tz'
 
 const tz = Intl.DateTimeFormat().resolvedOptions().timeZone
 
-export const formatByGranularity = (tickValue: Date, granularity: GranularityValues, dayBoundaryCrossed: boolean) => {
+export const formatByGranularity = (
+  tickValue: Date,
+  granularity: GranularityValues,
+  dayBoundaryCrossed: boolean,
+  timezone: string = tz,
+) => {
   if (['secondly', 'tenSecondly', 'thirtySecondly'].includes(granularity)) {
-    return formatInTimeZone(tickValue, tz, dayBoundaryCrossed ? 'yyyy-MM-dd h:mm:ss a' : 'h:mm:ss a')
+    return formatInTimeZone(tickValue, timezone, dayBoundaryCrossed ? 'yyyy-MM-dd h:mm:ss a' : 'h:mm:ss a')
   }
   if (['minutely', 'fiveMinutely', 'tenMinutely', 'thirtyMinutely', 'hourly', 'twoHourly'].includes(granularity)) {
-    return formatInTimeZone(tickValue, tz, dayBoundaryCrossed ? 'yyyy-MM-dd h:mm a' : 'h:mm a')
+    return formatInTimeZone(tickValue, timezone, dayBoundaryCrossed ? 'yyyy-MM-dd h:mm a' : 'h:mm a')
   }
   if (granularity === 'twelveHourly') {
-    return formatInTimeZone(tickValue, tz, 'yyyy-MM-dd h:mm a')
+    return formatInTimeZone(tickValue, timezone, 'yyyy-MM-dd h:mm a')
   }
 
   if (granularity === 'daily') {
-    return formatInTimeZone(tickValue, tz, 'yyyy-MM-dd')
+    return formatInTimeZone(tickValue, timezone, 'yyyy-MM-dd')
   }
 
   if (granularity === 'weekly') {
-    return `${formatInTimeZone(tickValue, tz, 'yyyy')}  W${formatInTimeZone(tickValue, tz, 'II')}`
+    return `${formatInTimeZone(tickValue, timezone, 'yyyy')}  W${formatInTimeZone(tickValue, timezone, 'II')}`
   }
 
-  return formatInTimeZone(tickValue, tz, 'yyyy-MM-dd h:mm:ss a')
+  return formatInTimeZone(tickValue, timezone, 'yyyy-MM-dd h:mm:ss a')
 }
