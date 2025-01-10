4e64e2c3ab83eb6f3be08db1ea287d821ee3814b
Zehao Zhang <zehao93@outlook.com>
2025-01-10 14:53:50 +0800

    feat: redis type

--- packages/entities/entities-redis-configurations/src/types/redis-configuration-form.ts
+++ packages/entities/entities-redis-configurations/src/types/redis-configuration-form.ts
@@ -2,11 +2,11 @@ import type { KonnectBaseFormConfig } from '@kong-ui-public/entities-shared'
 
 export interface KonnectRedisConfigurationFormConfig extends KonnectBaseFormConfig { }
 
-export enum Mode {
-  HOST_PORT_OPEN_SOURCE = 'host_port_open_source',
-  HOST_PORT_ENTERPRISE = 'host_port_enterprise',
-  SENTINEL = 'sentinel',
-  CLUSTER = 'cluster',
+export enum RedisType {
+  HOST_PORT_CE,
+  HOST_PORT_EE,
+  SENTINEL,
+  CLUSTER,
 }
 
 export interface SentinelNode {
@@ -21,32 +21,36 @@ export interface ClusterNode {
   port: number
 }
 
+export type PartialType = 'redis-ce' | 'redis-ee'
+
 export interface RedisConfigurationFields {
   name: string
-  mode: Mode
-  port: number
-  host: string
-  database: number
-  username: string
-  password: string
-  ssl: boolean
-  ssl_verify: boolean
-  server_name: string
-  timeout: number
+  type: PartialType
+  config: {
+    port: number
+    host: string
+    database: number
+    username: string
+    password: string
+    ssl: boolean
+    ssl_verify: boolean
+    server_name: string
+    timeout: number
 
-  connect_timeout: number
-  send_timeout: number
-  read_timeout: number
-  sentinel_username: string
-  sentinel_password: string
-  keepalive_pool_size: number
-  keepalive_backlog: number
-  sentinel_master: string
-  sentinel_role?: 'master' | 'slave' | 'any'
-  sentinel_nodes: Identifiable<SentinelNode>[]
-  cluster_nodes: Identifiable<ClusterNode>[]
-  cluster_max_redirections: number
-  connection_is_proxied: boolean
+    connect_timeout: number
+    send_timeout: number
+    read_timeout: number
+    sentinel_username: string
+    sentinel_password: string
+    keepalive_pool_size: number
+    keepalive_backlog: number
+    sentinel_master: string
+    sentinel_role?: 'master' | 'slave' | 'any'
+    sentinel_nodes: Identifiable<SentinelNode>[]
+    cluster_nodes: Identifiable<ClusterNode>[]
+    cluster_max_redirections: number
+    connection_is_proxied: boolean
+  }
 }
 
 export interface RedisConfigurationFormState {
