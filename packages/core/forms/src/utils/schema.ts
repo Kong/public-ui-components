6aaff8eb92136a92c9952c5f7590ecce8e0f5f49
tehkai <kai.arrowood@konghq.com>
2025-01-16 10:07:05 -0500

    fix(vfg): use setup syntax [khcp-11325]

--- packages/core/forms/src/utils/schema.ts
+++ packages/core/forms/src/utils/schema.ts
@@ -6,7 +6,7 @@ import isObject from 'lodash-es/isObject'
 import set from 'lodash-es/set'
 
 // Create a new model by schema default values
-export const createDefaultObject = (schema: any, obj: Record<string, any> = {}) => {
+export const createDefaultObject = (schema: any, obj: Record<string, any> = {}): Record<string, any> => {
   each(schema.fields, (field: any) => {
     if (get(obj, field.model) === undefined && field.default !== undefined) {
       if (isFunction(field.default)) {
@@ -16,22 +16,26 @@ export const createDefaultObject = (schema: any, obj: Record<string, any> = {}) 
       } else set(obj, field.model, field.default)
     }
   })
+
   return obj
 }
 
 // Get a new model which contains only properties of multi-edit fields
-export const getMultipleFields = (schema: any) => {
-  const res: any = []
+export const getMultipleFields = (schema: any): any[] => {
+  const res: any[] = []
+
   each(schema.fields, field => {
-    if (field.multi === true) res.push(field)
+    if (field.multi === true) {
+      res.push(field)
+    }
   })
 
   return res
 }
 
 // Merge many models to one 'work model' by schema
-export const mergeMultiObjectFields = (schema: any, objs: any) => {
-  const model = {}
+export const mergeMultiObjectFields = (schema: any, objs: any): Record<string, any> => {
+  const model: Record<string, any> = {}
 
   const fields = getMultipleFields(schema)
 
@@ -41,11 +45,12 @@ export const mergeMultiObjectFields = (schema: any, objs: any) => {
     const path = field.model
 
     each(objs, (obj: any) => {
-      const v = get(obj, path)
+      const val = get(obj, path)
+
       if (notSet) {
-        mergedValue = v
+        mergedValue = val
         notSet = false
-      } else if (mergedValue !== v) {
+      } else if (mergedValue !== val) {
         mergedValue = undefined
       }
     })
@@ -56,50 +61,50 @@ export const mergeMultiObjectFields = (schema: any, objs: any) => {
   return model
 }
 
-export const slugifyFormID = (schema: any, prefix: any = '') => {
+export const slugifyFormID = (schema: any, prefix: any = ''): string => {
   // Try to get a reasonable default id from the schema,
   // then slugify it.
   if (typeof schema.id !== 'undefined') {
     // If an ID's been explicitly set, use it unchanged
-    return prefix + schema.id
+    return prefix + schema.id + ''
   } else {
     // Return the slugified version of either:
     return (
       prefix +
       (schema.inputName || schema.label || schema.model || '')
-      // NB: This is a very simple, conservative, slugify function,
-      // avoiding extra dependencies.
+        // NB: This is a very simple, conservative, slugify function,
+        // avoiding extra dependencies.
         .toString()
         .trim()
         .toLowerCase()
-      // Spaces & underscores to dashes
+        // Spaces & underscores to dashes
         .replace(/ |_/g, '-')
-      // Multiple dashes to one
+        // Multiple dashes to one
         .replace(/-{2,}/g, '-')
-      // Remove leading & trailing dashes
+        // Remove leading & trailing dashes
         .replace(/^-+|-+$/g, '')
-      // Remove anything that isn't a (English/ASCII) letter, number or dash.
+        // Remove anything that isn't a (English/ASCII) letter, number or dash.
         .replace(/([^a-zA-Z0-9-]+)/g, '')
     )
   }
 }
 
-export const slugify = (name: any = '') => {
+export const slugify = (name: any = ''): string => {
   // Return the slugified version of either:
   return (
     name
-    // NB: This is a very simple, conservative, slugify function,
-    // avoiding extra dependencies.
+      // NB: This is a very simple, conservative, slugify function,
+      // avoiding extra dependencies.
       .toString()
       .trim()
-    // .toLowerCase()
-    // Spaces to dashes
+      // .toLowerCase()
+      // Spaces to dashes
       .replace(/ /g, '-')
-    // Multiple dashes to one
+      // Multiple dashes to one
       .replace(/-{2,}/g, '-')
-    // Remove leading & trailing dashes
+      // Remove leading & trailing dashes
       .replace(/^-+|-+$/g, '')
-    // Remove anything that isn't a (English/ASCII) letter, number or dash.
+      // Remove anything that isn't a (English/ASCII) letter, number or dash.
       .replace(/([^a-zA-Z0-9-_/./:]+)/g, '')
   )
 }
