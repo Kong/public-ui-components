332e14e5b5dd95264817e4dbc5a5fa9eca0761c4
Makito <i@maki.to>
2025-01-16 14:52:20 +0800

    fix(expressions): type imports

--- packages/core/expressions/src/types.ts
+++ packages/core/expressions/src/types.ts
@@ -1,1 +1,3 @@
+import type * as monaco from 'monaco-editor'
+
 export type ProvideRhsValueCompletion = (lhsValue: string, rhsValueValue: string, lhsRange: monaco.Range, rhsValueRange: monaco.Range) => Promise<monaco.languages.CompletionList | undefined>
