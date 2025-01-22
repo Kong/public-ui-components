327437ca535a8e4af120025610a90acd42a37a77
Makito <i@maki.to>
2025-01-22 10:28:42 +0800

    feat(expressions): add func to check if rhs autocompletion is necessary; dispose after use

--- packages/core/expressions/src/types.ts
+++ packages/core/expressions/src/types.ts
@@ -1,4 +1,10 @@
 import type * as monaco from 'monaco-editor'
 
 export type ProvideCompletionItems = monaco.languages.CompletionItemProvider['provideCompletionItems']
+
 export type ProvideRhsValueCompletion = (lhsValue: string, rhsValueValue: string, lhsRange: monaco.Range, rhsValueRange: monaco.Range) => Promise<monaco.languages.CompletionList | undefined>
+
+export interface RhsValueCompletion {
+  provide: ProvideRhsValueCompletion
+  shouldProvide: (lhsIdentValue: string) => boolean
+}
