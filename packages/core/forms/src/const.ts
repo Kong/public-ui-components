ed35303d7e90a9e82a90e7fc1d81bc973977a72d
Tian Tan <tian.tan@konghq.com>
2025-01-22 17:58:43 +0800

    feat: enable custom plugin redis config

--- packages/core/forms/src/const.ts
+++ packages/core/forms/src/const.ts
@@ -2,6 +2,8 @@ export const FORMS_API_KEY = 'kong-ui-forms-api'
 
 export const FORMS_CONFIG = 'kong-ui-forms-config'
 
+export const REDIS_PARTIAL_FETCHER_KEY = 'redis-partial-list-fetcher-key'
+
 /** Used by `template` and `slot` */
 export const AUTOFILL_SLOT_NAME = 'autofill'
 
