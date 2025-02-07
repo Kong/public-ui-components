7aadae31a9d458cdc37681def85855fc70db7b76
afrzhussain <afroz.hussain@konghq.com>
2025-02-07 18:22:18 +0530

    fix: styling issues

--- packages/entities/entities-gateway-services/src/components/GatewayServiceList.vue
+++ packages/entities/entities-gateway-services/src/components/GatewayServiceList.vue
@@ -637,14 +637,14 @@ onBeforeMount(async () => {
 </script>
 
 <style lang="scss" scoped>
+.button-row {
+  align-items: center;
+  display: flex;
+  gap: $kui-space-50;
+}
+
 .kong-ui-entities-gateway-services-list {
   width: 100%;
-
-  .button-row {
-    align-items: center;
-    display: flex;
-    gap: $kui-space-50;
-  }
 
   .kong-ui-entity-filter-input {
     margin-right: $kui-space-50;
