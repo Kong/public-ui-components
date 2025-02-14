ae00fa284d431abdf0ffc6032fa7941a5f83d88b
tehkai <kai.arrowood@konghq.com>
2025-02-14 11:32:44 -0500

    feat(consumers): poc for external create

--- packages/entities/entities-consumers/src/components/ConsumerList.vue
+++ packages/entities/entities-consumers/src/components/ConsumerList.vue
@@ -31,7 +31,10 @@
         />
       </template>
       <!-- Create action -->
-      <template #toolbar-button>
+      <template
+        v-if="!useExternalCreate"
+        #toolbar-button
+      >
         <Teleport
           :disabled="!useActionOutside"
           to="#kong-ui-app-page-header-action-button"
@@ -336,6 +339,13 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  /**
+   * POC: externalize create/LH actions for list view
+   */
+  useExternalCreate: {
+    type: Boolean,
+    default: false,
+  },
 })
 
 const { i18nT, i18n: { t } } = composables.useI18n()
@@ -414,6 +424,12 @@ const { hasRecords, handleStateChange } = useTableState(filterQuery)
 // If new empty states are not enabled, show the learning hub button (for Konnect)
 const showHeaderLHButton = computed((): boolean => hasRecords.value && props.config.app === 'konnect')
 const isLegacyLHButton = computed((): boolean => !props.enableV2EmptyStates && props.config.app === 'konnect')
+
+// Let the host app know if the table has records in order to show/hide action buttons in the header
+defineExpose({
+  hasRecords,
+  handleCreateClick,
+})
 
 const isConsumerGroupPage = computed<boolean>(() => !!props.config.consumerGroupId)
 const preferencesStorageKey = computed<string>(
