68ffb9582a3bedfcb4e36d5c63af031811b69db4
Makito <i@maki.to>
2025-02-14 18:30:41 +0800

    feat(tracing): display more info in the lifecycle view (#1952)
    * feat(tracing): display more info in the lifecycle view
    
    * fix(tracing): bad math
    
    * chore(tracing): apply suggestion
    
    Co-authored-by: Yi Yang <Leopoldthecoder@users.noreply.github.com>
    
    * fix(tracing): remove semicolons
    
    * fix(tracing): fit view
    
    ---------
    
    Co-authored-by: Yi Yang <Leopoldthecoder@users.noreply.github.com>

--- packages/core/tracing/src/types/lifecycle.ts
+++ packages/core/tracing/src/types/lifecycle.ts
@@ -1,13 +1,37 @@
 import type { Edge, Node } from '@vue-flow/core'
+import type { TranslationKey } from '../composables/useI18n'
 import type { LifecycleNodeType } from '../constants'
 import type { SpanNode } from './spans'
 
-export interface LifecycleNodeData {
-  label: string
-  type: LifecycleNodeType
+export interface LifecycleGraphNodeTree {
+  client: {
+    node: LifecycleNode
+    in: LifecycleNode
+    out: LifecycleNode
+  }
+  requests?: {
+    node: LifecycleNode
+    children: LifecycleNode[]
+  }
+  upstream: LifecycleNode
+  responses?: {
+    node: LifecycleNode
+    children: LifecycleNode[]
+  }
+}
+
+export interface LifecycleNodeData<T extends LifecycleNodeType = LifecycleNodeType> {
+  label?: string
+  type: T
   badge?: string
   durationNano?: number
   spans?: SpanNode[]
+  tree?: LifecycleGraphNodeTree
+  labelPlacement?: `${'top' | 'bottom'} ${'left' | 'right'}`
+  labelTooltipKey?: TranslationKey
+  durationTooltipKey?: TranslationKey
+  showTargetHandle?: boolean
+  showSourceHandle?: boolean
 }
 
 export interface LifecycleNode extends Node<LifecycleNodeData, any, LifecycleNodeType> {
@@ -15,7 +39,12 @@ export interface LifecycleNode extends Node<LifecycleNodeData, any, LifecycleNod
   data: LifecycleNodeData
 }
 
+export interface LifecycleEdgeData {
+  tooltip?: string
+}
+
 export interface LifecycleGraph {
+  nodeTree: LifecycleGraphNodeTree
   nodes: LifecycleNode[]
   edges: Edge[]
 }
