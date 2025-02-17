7d42033519e8d6eafb12da97531b0b3082d5930f
afrzhussain <afroz.hussain@konghq.com>
2025-02-17 18:35:27 +0530

    fix: refactor lh logic

--- packages/entities/entities-key-sets/src/components/KeySetList.vue
+++ packages/entities/entities-key-sets/src/components/KeySetList.vue
@@ -32,7 +32,7 @@
         >
           <div class="button-row">
             <KButton
-              v-if="!showEmptyState && config.app === 'konnect'"
+              v-if="showHeaderLHButton"
               appearance="secondary"
               class="open-learning-hub"
               data-testid="keys-set-learn-more-button"
@@ -54,6 +54,27 @@
               </KButton>
             </PermissionsWrapper>
           </div>
+        </Teleport>
+      </template>
+
+      <!-- TODO: remove this slot when empty states M2 is cleaned up -->
+      <template
+        v-if="!hasRecords && isLegacyLHButton"
+        #outside-actions
+      >
+        <Teleport
+          :disabled="!useActionOutside"
+          to="#kong-ui-app-page-header-action-button"
+        >
+          <KButton
+            appearance="secondary"
+            class="open-learning-hub"
+            data-testid="keys-set-learn-more-button"
+            icon
+            @click="$emit('click:learn-more')"
+          >
+            <BookIcon decorative />
+          </KButton>
         </Teleport>
       </template>
 
@@ -262,7 +283,12 @@ const { i18n: { t } } = composables.useI18n()
 const router = useRouter()
 
 const { axiosInstance } = useAxios(props.config?.axiosRequestConfig)
-const { hideTableToolbar: showEmptyState, handleStateChange } = useTableState(() => filterQuery.value)
+const { hasRecords, handleStateChange } = useTableState(() => filterQuery.value)
+// Current empty state logic is only for Konnect, KM will pick up at GA.
+// If new empty states are enabled, show the learning hub button when the empty state is hidden (for Konnect)
+// If new empty states are not enabled, show the learning hub button (for Konnect)
+const showHeaderLHButton = computed((): boolean => hasRecords.value && props.config.app === 'konnect')
+const isLegacyLHButton = computed((): boolean => !props.enableV2EmptyStates && props.config.app === 'konnect')
 
 
 /**
