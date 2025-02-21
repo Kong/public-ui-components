fc2f3d7a10e812b06efe8cdf673c13aad3be84c9
Maksym Portianoi <maksym.portianoi@konghq.com>
2025-02-21 15:08:33 -0500

    fix(dashboard-renderer): enable doughnut charts [MA-3650] (#1972)
    * fix(dashboard-renderer): enable doughnut charts [MA-3650]
    
    * fix: minor fix
    
    * fix(analytics-utilities): add doughnut chart type [MA-3650]
    
    * fix: address pr feedback
    
    * fix: address pr feedback

---
+++ packages/analytics/dashboard-renderer/src/components/DoughnutChartRenderer.vue
@@ -0,0 +1,18 @@
+<template>
+  <BaseAnalyticsChartRenderer
+    :chart-options="chartOptions"
+    :context="context"
+    :height="height"
+    :query="query"
+    :query-ready="queryReady"
+    :refresh-counter="refreshCounter"
+  />
+</template>
+
+<script setup lang="ts">
+import BaseAnalyticsChartRenderer from './BaseAnalyticsChartRenderer.vue'
+import type { RendererProps } from '../types'
+import type { DoughnutChartOptions } from '@kong-ui-public/analytics-utilities'
+
+defineProps<RendererProps<DoughnutChartOptions>>()
+</script>
