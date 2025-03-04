c61d3f38ee0d93e5a3c135884e7a84b9bd4e4002
Val Gorodnichev <val.gorodnichev@konghq.com>
2025-03-04 15:30:24 -0700

    fix: adding test packages

---
+++ packages/core/test-publishing-1/src/index.ts
@@ -0,0 +1,17 @@
+// import type { App } from 'vue'
+import TestPublishing1 from './components/TestPublishing1.vue'
+
+// Export Vue plugin
+// We rarely want to export components as a plugin as we prefer to support proper tree-shaking in the host application. Only enable if you're packing a Vue plugin.
+// export default {
+//   // Customize Vue plugin options as desired
+//   // Providing a `name` property allows for customizing the registered
+//   // name of your component (useful if exporting a single component).
+//   install: (app: App, options: { name?: string, [key: string]: any } = {}): void => {
+//     app.component(options.name || 'TestPublishing1', TestPublishing1)
+//   },
+// }
+
+export { TestPublishing1 }
+
+export * from './types'
