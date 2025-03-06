fffcb9fb956b1d2ce16b0267bff0010303b275d7
Hiroshi Fukada <hiroshi.fukada@konghq.com>
2025-03-06 10:52:26 -0500

    feat(analytics-utilities): add regional_consumer to dimensions

--- packages/analytics/analytics-utilities/src/types/explore/advanced.ts
+++ packages/analytics/analytics-utilities/src/types/explore/advanced.ts
@@ -12,6 +12,7 @@ export const queryableExploreDimensions = [
   'upstream_status_code_grouped',
   'response_source',
   'data_plane_node_version',
+  'regional_consumer',
 ] as const
 
 export type QueryableExploreDimensions = typeof queryableExploreDimensions[number]
