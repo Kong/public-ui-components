43511dd90fb5f6ae583b5f19a0c70493b1fd7dfc
portikM <maksym.portianoi@konghq.com>
2025-03-18 15:02:01 -0400

    fix: minor fix

--- packages/entities/entities-consumer-groups/src/components/ConsumerGroupList.vue
+++ packages/entities/entities-consumer-groups/src/components/ConsumerGroupList.vue
@@ -232,7 +232,7 @@
 
 <script setup lang="ts">
 import type { PropType } from 'vue'
-import { computed, ref, watch, onBeforeMount } from 'vue'
+import { computed, ref, watch, onBeforeMount, toRef } from 'vue'
 import type { AxiosError } from 'axios'
 import { useRouter } from 'vue-router'
 import { AddIcon, BookIcon, TeamIcon } from '@kong/icons'
@@ -435,7 +435,7 @@ const {
   fetcher,
   fetcherState,
   fetcherCacheKey,
-} = useFetcher({ ...props.config, cacheIdentifier: props.cacheIdentifier }, fetcherBaseUrl.value, dataKeyName)
+} = useFetcher(toRef({ ...props.config, cacheIdentifier: props.cacheIdentifier }), fetcherBaseUrl, dataKeyName)
 
 const clearFilter = (): void => {
   filterQuery.value = ''
