6aec5aef0ec32a26c4dbae36dde9512992b3ee09
Afroz Hussain <36099616+afrzhussain@users.noreply.github.com>
2025-03-18 20:13:25 +0530

    feat: gateway service create form revamp (#2001)
    * feat: add ui elements changes
    
    * fix: added error handling changes
    
    * fix: added form validations
    
    * fix: added multiple error handling
    
    * fix: eslint
    
    * fix: refactored code and content
    
    * fix: refactored code and content
    
    * fix: fixed build
    
    * fix: added changes
    
    * fix: added composables
    
    * fix: added fixes and improvements
    
    * fix: added fixes and improvements
    
    * fix: added fixes and improvements
    
    * fix: fixed content
    
    * fix: code nits
    
    * fix: added unit test cases
    
    * fix: updated test cases
    
    * fix: added test cases
    
    * fix: fixed bugs
    
    * fix: added fixes and improvements
    
    * fix: updated test cases
    
    * fix: code nits and improvements

--- packages/entities/entities-gateway-services/src/composables/index.ts
+++ packages/entities/entities-gateway-services/src/composables/index.ts
@@ -1,8 +1,10 @@
 import useI18n from './useI18n'
 import usePortFromProtocol from './getPortFromProtocol'
+import useUrlValidators from './useUrlValidators'
 
 // All composables must be exported as part of the default object for Cypress test stubs
 export default {
   useI18n,
   usePortFromProtocol,
+  useUrlValidators,
 }
