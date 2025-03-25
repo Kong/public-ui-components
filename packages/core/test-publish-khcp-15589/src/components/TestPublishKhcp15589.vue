484260bff6b08c9a9bb4c4dba8a1fbf95a7ad479
ValeryG <val.gorodnichev@konghq.com>
2025-03-25 14:11:49 -0600

    fix: back to pnpm for package publishing [KHCP-15589]

---
+++ packages/core/test-publish-khcp-15589/src/components/TestPublishKhcp15589.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- The <div> tag here is just a placeholder for your component content. -->
+  <!-- We recommend wrapping your component with a unique class when possible, as shown below. -->
+  <div class="kong-ui-public-test-publish-khcp-15589">
+    <p>This is the <b>TestPublishKhcp15589</b> content.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.kong-ui-public-test-publish-khcp-15589 {
+  // Add component styles as needed
+}
+</style>
