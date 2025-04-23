d26befe1dab05eea620330b0dac8cc3173a70c9c
afrzhussain <afroz.hussain@konghq.com>
2025-04-23 16:37:51 +0530

    fix: expose utils

--- packages/entities/entities-gateway-services/src/index.ts
+++ packages/entities/entities-gateway-services/src/index.ts
@@ -6,5 +6,8 @@ import GatewayServiceForm from './components/GatewayServiceForm.vue'
 // Components
 export { GatewayServiceList, GatewayServiceForm, GatewayServiceConfigCard, LegacyGatewayServiceForm }
 
+// Utilities
+export * from './utils'
+
 // Types
 export * from './types'
