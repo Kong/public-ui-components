7e09c06f306c496f500e8a147e2b3dc9904b6dfd
Afroz Hussain <36099616+afrzhussain@users.noreply.github.com>
2025-04-25 21:15:14 +0530

    fix: refactor composable export as util (#2086)
    * fix: refactor composable export as util
    
    * fix: expose utils
    
    * chore: added documentation

--- packages/entities/entities-gateway-services/src/index.ts
+++ packages/entities/entities-gateway-services/src/index.ts
@@ -2,16 +2,12 @@ import GatewayServiceList from './components/GatewayServiceList.vue'
 import LegacyGatewayServiceForm from './components/LegacyGatewayServiceForm.vue'
 import GatewayServiceConfigCard from './components/GatewayServiceConfigCard.vue'
 import GatewayServiceForm from './components/GatewayServiceForm.vue'
-import composables from './composables'
-
-// Extract specific composables to export
-const { useUrlValidators } = composables
 
 // Components
 export { GatewayServiceList, GatewayServiceForm, GatewayServiceConfigCard, LegacyGatewayServiceForm }
 
-// composables
-export { useUrlValidators }
+// Utilities
+export * from './utils'
 
 // Types
 export * from './types'
