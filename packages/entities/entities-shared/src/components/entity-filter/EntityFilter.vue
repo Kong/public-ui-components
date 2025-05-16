90fbf119592ff2dc8f1c4f2d79645d8cc4787543
Justineo <justice360@gmail.com>
2025-05-16 14:52:57 +0800

    fix(entities-shared): fix types

--- packages/entities/entities-shared/src/components/entity-filter/EntityFilter.vue
+++ packages/entities/entities-shared/src/components/entity-filter/EntityFilter.vue
@@ -161,7 +161,7 @@ const emit = defineEmits<{
 }>()
 
 const showMenu = ref(false)
-const searchParams = ref<{ [key: string]: string | number }>({})
+const searchParams = ref<{ [key: string]: string }>({})
 const expandedFields = ref<Set<string>>(new Set())
 
 const filteredFields = computed<string[]>(() => {
@@ -198,7 +198,7 @@ const toggleMenu = () => {
 }
 
 const toggleExpanded = (field: string) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+
   expandedFields.value.has(field) ? expandedFields.value.delete(field) : expandedFields.value.add(field)
 }
 
