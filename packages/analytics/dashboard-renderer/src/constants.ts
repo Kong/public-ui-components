de80d4e2c49bf04c18c9e976794cb92d84654550
Jonathan Walters <jonathan.walters@konghq.com>
2025-05-19 17:24:37 -0400

    feat: use DraggableGridLayout for all dashboard rendering

--- packages/analytics/dashboard-renderer/src/constants.ts
+++ packages/analytics/dashboard-renderer/src/constants.ts
@@ -1,5 +1,6 @@
 // Default tile height and width - approximated from the figma design.
 export const DEFAULT_TILE_HEIGHT = 170
+export const MIN_CELL_HEIGHT = 5
 export const INJECT_QUERY_PROVIDER = 'analytics-query-provider'
 export const ENTITY_ID_TOKEN = '{entity-id}'
 export const CP_ID_TOKEN = '{cp-id}'
