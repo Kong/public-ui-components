38977581d8cd96087d642df9fbb0d9c0a48084a1
Makito <i@maki.to>
2025-07-18 19:31:31 +0900

    feat(entities-plugins): datakit VueFlow setup and node handles impl

--- packages/entities/entities-plugins/src/components/free-form/Datakit/visual-editor/modal/EditorMain.vue
+++ packages/entities/entities-plugins/src/components/free-form/Datakit/visual-editor/modal/EditorMain.vue
@@ -15,29 +15,67 @@
         </KButton>
       </div>
     </header>
+
     <div class="body">
-      <slot />
+      <VueFlow
+        class="flow"
+        :nodes="nodes"
+      >
+        <Background />
+        <Controls />
+
+        <template #node-request>
+          <RequestNode />
+        </template>
+        <template #node-response>
+          <ResponseNode />
+        </template>
+      </VueFlow>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { createI18n } from '@kong-ui-public/i18n'
-import { KButton } from '@kong/kongponents'
 import { ExternalLinkIcon } from '@kong/icons'
+import { KButton } from '@kong/kongponents'
+import { Background } from '@vue-flow/background'
+import { Controls } from '@vue-flow/controls'
+import { VueFlow, type Node } from '@vue-flow/core'
+import { ref } from 'vue'
 import english from '../../../../../locales/en.json'
+import RequestNode from '../node/nodes/RequestNode.vue'
+import ResponseNode from '../node/nodes/ResponseNode.vue'
+
+import '@vue-flow/controls/dist/style.css'
+import '@vue-flow/core/dist/style.css'
+import '@vue-flow/core/dist/theme-default.css'
 
 const { t } = createI18n<typeof english>('en-us', english)
 
 defineSlots<{
   default(): any
 }>()
+
+const nodes = ref<Node[]>([
+  {
+    id: 'implicit:request',
+    type: 'request',
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: 'implicit:service-request',
+    type: 'response',
+    position: { x: 300, y: 0 },
+  },
+])
 </script>
 
 <style lang="scss" scoped>
 .dk-editor-main {
   display: flex;
   flex-direction: column;
+  height: 100%;
 
   .header {
     align-items: center;
@@ -53,6 +91,24 @@ defineSlots<{
   .actions {
     display: flex;
     gap: $kui-space-30;
+  }
+
+  .body {
+    height: 100%;
+    width: 100%;
+  }
+
+  .flow {
+    :deep(.vue-flow__controls) {
+      bottom: 0;
+      left: unset;
+      right: 0;
+    }
+
+    :deep(.vue-flow__controls-button) {
+      // Ensure it works in both the sandbox and host apps
+      box-sizing: content-box;
+    }
   }
 }
 </style>
