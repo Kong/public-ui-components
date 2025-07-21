98f6cb152531b492dfb87b647e82dd2b27a4dde0
Zehao Zhang <zehao93@outlook.com>
2025-07-21 17:17:13 +0800

    fix: vault picker

--- packages/entities/entities-plugins/src/components/free-form/ServiceProtection/ServiceProtectionForm.vue
+++ packages/entities/entities-plugins/src/components/free-form/ServiceProtection/ServiceProtectionForm.vue
@@ -8,10 +8,18 @@
 </template>
 
 <script setup lang="ts">
+import { AUTOFILL_SLOT, AUTOFILL_SLOT_NAME } from '@kong-ui-public/forms'
+import { provide } from 'vue'
 import ConfigForm from './ConfigForm.vue'
 import PluginFormWrapper from '../shared/PluginFormWrapper.vue'
 
 import type { PluginFormWrapperProps } from '../shared/PluginFormWrapper.vue'
 
 const props = defineProps<PluginFormWrapperProps<any>>()
+
+const slots = defineSlots<{
+  [K in typeof AUTOFILL_SLOT_NAME]: () => any
+}>()
+
+provide(AUTOFILL_SLOT, slots?.[AUTOFILL_SLOT_NAME])
 </script>
