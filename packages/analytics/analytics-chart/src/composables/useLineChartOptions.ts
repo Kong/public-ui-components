b84357cfa77dfb51b59454dfe6e4299a50dec9bc
Filip <filgut@protonmail.com>
2025-07-22 16:13:21 -0700

    fix: split the tooltip behaviour functions (#2241)

--- packages/analytics/analytics-chart/src/composables/useLineChartOptions.ts
+++ packages/analytics/analytics-chart/src/composables/useLineChartOptions.ts
@@ -8,7 +8,7 @@ import type {
 import {
   Tooltip,
 } from 'chart.js'
-import { formatByGranularity, horizontalTooltipPositioning, tooltipBehavior, verticalTooltipPositioning } from '../utils'
+import { formatByGranularity, horizontalTooltipPositioning, lineChartTooltipBehavior, verticalTooltipPositioning } from '../utils'
 import { isNullOrUndef } from 'chart.js/helpers'
 import type { ExternalTooltipContext, LineChartOptions } from '../types'
 import { millisecondsToHours } from 'date-fns'
@@ -152,7 +152,7 @@ export default function useLineChartOptions(chartOptions: LineChartOptions) {
         tooltip: {
           ...tooltipOptions,
           external: (context: ExternalTooltipContext) => {
-            tooltipBehavior(chartOptions.tooltipState, context)
+            lineChartTooltipBehavior(chartOptions.tooltipState, context, chartOptions.granularity.value)
           },
         },
       },
