52f94033ab96818bfaa782868649c19de6874af5
Rocky <adorack@users.noreply.github.com>
2025-07-28 16:56:06 -0400

    feat(dashboards): restore dynamic dashboard demo (#2256)

---
+++ packages/analytics/dashboard-renderer/sandbox/pages/DynamicDashboardDemo.vue
@@ -0,0 +1,243 @@
+<template>
+  <SandboxLayout
+    :links="appLinks"
+    title="Dashboard Renderer"
+  >
+    <template #controls>
+      <h2>Definition</h2>
+      <textarea v-model="definitionText" />
+      <KCodeBlock
+        id="data-codeblock"
+        :code="JSON.stringify(definition, null, 2)"
+        language="json"
+        searchable
+      />
+    </template>
+    <div class="sandbox-container">
+      <DashboardRenderer
+        v-if="definition.type === ValidationResultType.Success"
+        v-model="definition.data"
+        :context="context"
+      />
+    </div>
+  </SandboxLayout>
+</template>
+
+<script setup lang="ts">
+import type { DashboardRendererContext } from '../../src'
+import { DashboardRenderer } from '../../src'
+import { computed, ref, inject } from 'vue'
+import Ajv from 'ajv'
+import type { SandboxNavigationItem } from '@kong-ui-public/sandbox-layout'
+import { SandboxLayout } from '@kong-ui-public/sandbox-layout'
+import '@kong-ui-public/sandbox-layout/dist/style.css'
+import { type DashboardConfig, dashboardConfigSchema } from '@kong-ui-public/analytics-utilities'
+
+const appLinks: SandboxNavigationItem[] = inject('app-links', [])
+
+// Consider moving to its own package.
+enum ValidationResultType {
+  JsonParseError,
+  ValidationError,
+  Success,
+}
+
+interface JsonParseError {
+  type: ValidationResultType.JsonParseError
+  message: string
+}
+
+interface ValidationError {
+  type: ValidationResultType.ValidationError
+  errors: any
+}
+
+interface Success {
+  type: ValidationResultType.Success
+  data: DashboardConfig
+}
+
+type ValidationResult = JsonParseError | ValidationError | Success
+
+// Validation boilerplate.
+// See https://github.com/ThomasAribart/json-schema-to-ts#validators
+const ajv = new Ajv()
+
+const validate = ajv.compile(dashboardConfigSchema)
+
+const definitionText = ref(
+  `
+{
+    "tiles": [
+      {
+        "type": "chart",
+        "definition": {
+          "chart": {
+            "type": "horizontal_bar"
+          },
+          "query": {
+            "datasource": "api_usage"
+          }
+        },
+        "layout": {
+          "position": {
+            "col": 0,
+            "row": 0
+          },
+          "size": {
+            "cols": 2,
+            "rows": 2
+          }
+        }
+      },
+      {
+        "type": "chart",
+        "definition": {
+          "chart": {
+            "type": "horizontal_bar"
+          },
+          "query": {
+            "datasource": "api_usage"
+          }
+        },
+        "layout": {
+          "position": {
+            "col": 2,
+            "row": 0
+          },
+          "size": {
+            "cols": 2,
+            "rows": 2
+          }
+        }
+      },
+      {
+        "type": "chart",
+        "definition": {
+          "chart": {
+            "type": "vertical_bar"
+          },
+          "query": {
+            "datasource": "api_usage"
+          }
+        },
+        "layout": {
+          "position": {
+            "col": 0,
+            "row": 2
+          },
+          "size": {
+            "cols": 2,
+            "rows": 2
+          }
+        }
+      },
+      {
+        "type": "chart",
+        "definition": {
+          "chart": {
+            "type": "timeseries_line",
+            "chart_dataset_colors": {
+              "request_count": "#00C8F4"
+            },
+            "chart_title": "Traffic",
+            "synthetics_data_key": "overview-traffic"
+          },
+          "query": {
+            "datasource": "api_usage"
+          }
+        },
+        "layout": {
+          "position": {
+            "col": 2,
+            "row": 2
+          },
+          "size": {
+            "cols": 4,
+            "rows": 2
+          }
+        }
+      }
+    ]
+  }
+  `,
+)
+
+const definition = computed<ValidationResult>(() => {
+  let data
+
+  try {
+    data = JSON.parse(definitionText.value)
+  } catch (e) {
+    return {
+      type: ValidationResultType.JsonParseError,
+      message: `${e}`,
+    }
+  }
+
+  if (!validate(data)) {
+    return {
+      type: ValidationResultType.ValidationError,
+      errors: validate.errors,
+    }
+  }
+
+  return {
+    type: ValidationResultType.Success,
+    data,
+  }
+})
+
+const context: DashboardRendererContext = {
+  filters: [],
+  timeSpec: {
+    type: 'relative',
+    time_range: '24h',
+  },
+  editable: true,
+}
+
+</script>
+
+<style lang="scss" scoped>
+textarea {
+  background-color: $kui-color-background-neutral-weakest;
+  border: 1px solid $kui-color-background-neutral-weak;
+  border-radius: 4px;
+  color: #333333;
+  font-family: 'Fira Code', 'Consolas', 'Monaco', monospace;
+  font-size: $kui-font-size-20;
+  height: 500px;
+  line-height: 1.5;
+  margin: 1rem 0;
+  overflow: auto;
+  padding: 1rem;
+  resize: vertical;
+  scrollbar-color: #bbbbbb #e0e0e0;
+  scrollbar-width: thin;
+  tab-size: 4;
+  -moz-tab-size: 4;
+  width: 90%;
+}
+
+textarea::-webkit-scrollbar {
+  width: 5px;
+}
+
+textarea::-webkit-scrollbar-track {
+  background: #e0e0e0;
+  border-radius: 10px;
+}
+
+textarea::-webkit-scrollbar-thumb {
+  background-color: #bbbbbb;
+  border: 1px solid #aaaaaa;
+  border-radius: 10px;
+}
+
+textarea:focus {
+  border-color: white;
+  box-shadow: 0 0 0 2px $kui-color-background-neutral-weak;
+  outline: none;
+}
+</style>
