a6381e6ca8ee690b765522b11c7ed5fd37d0ac34
Makito <i@maki.to>
2025-07-29 19:26:32 +0900

    feat(entities-plugins): datakit split canvas into two resizable panels

--- packages/entities/entities-plugins/src/components/free-form/Datakit/flow-editor/modal/EditorMain.vue
+++ packages/entities/entities-plugins/src/components/free-form/Datakit/flow-editor/modal/EditorMain.vue
@@ -16,21 +16,49 @@
       </div>
     </header>
 
-    <div class="body">
-      <VueFlow
-        class="flow"
-        :nodes="requestNodes"
-        @click="emit('click:backdrop')"
-        @nodes-initialized="fitView"
-      >
-        <Background @click="emit('click:backdrop')" />
-        <Controls />
+    <div
+      ref="canvasContainer"
+      class="body"
+    >
+      <EditorCanvas>
+        <template #request>
+          <VueFlow
+            id="request-flow"
+            class="flow"
+            fit-view-on-init
+            :nodes="requestNodes"
+            @click="emit('click:backdrop')"
+            @node-click="onNodeClick"
+          >
+            <Background @click="emit('click:backdrop')" />
+            <Controls />
 
-        <!-- To not use the default node style -->
-        <template #node-flow="node">
-          <FlowNode :data="node.data" />
+            <!-- To not use the default node style -->
+            <template #node-flow="node">
+              <FlowNode :data="node.data" />
+            </template>
+          </VueFlow>
         </template>
-      </VueFlow>
+
+        <template #response>
+          <VueFlow
+            id="response-flow"
+            class="flow"
+            fit-view-on-init
+            :nodes="responseNodes"
+            @click="emit('click:backdrop')"
+            @node-click="onNodeClick"
+          >
+            <Background @click="emit('click:backdrop')" />
+            <Controls />
+
+            <!-- To not use the default node style -->
+            <template #node-flow="node">
+              <FlowNode :data="node.data" />
+            </template>
+          </VueFlow>
+        </template>
+      </EditorCanvas>
     </div>
   </div>
 </template>
@@ -41,17 +69,19 @@ import { ExternalLinkIcon } from '@kong/icons'
 import { KButton } from '@kong/kongponents'
 import { Background } from '@vue-flow/background'
 import { Controls } from '@vue-flow/controls'
-import { useVueFlow, VueFlow } from '@vue-flow/core'
+import { VueFlow, type NodeMouseEvent } from '@vue-flow/core'
+
 import english from '../../../../../locales/en.json'
 import FlowNode from '../node/FlowNode.vue'
 import { useEditorStore } from '../store/store'
 
+import EditorCanvas from './EditorCanvas.vue'
+
+import type { NodeInstance } from '../../types'
 
 import '@vue-flow/controls/dist/style.css'
 import '@vue-flow/core/dist/style.css'
 import '@vue-flow/core/dist/theme-default.css'
-
-import type { NodeInstance } from '../../types'
 
 const { t } = createI18n<typeof english>('en-us', english)
 
@@ -70,14 +100,12 @@ if (!editorStore) {
   throw new Error('Editor state is not provided. Ensure you are using provideEditorStore in a parent component.')
 }
 
-const { requestNodes } = editorStore
+const { requestNodes, responseNodes } = editorStore
 
-const { fitView, onNodeClick } = useVueFlow()
-
-onNodeClick(({ event, node }) => {
+const onNodeClick = ({ event, node }: NodeMouseEvent) => {
   event.stopPropagation()
   emit('click:node', node.data)
-})
+}
 </script>
 
 <style lang="scss" scoped>
@@ -103,6 +131,8 @@ onNodeClick(({ event, node }) => {
   }
 
   .body {
+    display: flex;
+    flex-direction: column;
     height: 100%;
     width: 100%;
   }
