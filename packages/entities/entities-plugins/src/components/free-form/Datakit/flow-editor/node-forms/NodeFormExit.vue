5916198352f7ea2182d296195e0b8cb172382d1b
Zehao <zehao93@outlook.com>
2025-08-06 15:43:32 +0800

    feat(entities-plugins): datakit - exit node form [KM-1460] (#2288)

---
+++ packages/entities/entities-plugins/src/components/free-form/Datakit/flow-editor/node-forms/NodeFormExit.vue
@@ -0,0 +1,51 @@
+<template>
+  <Form
+    ref="form"
+    :config="{ updateOnChange: true }"
+    :data="formData"
+    :schema="ExitNodeSchema"
+  >
+    <StringField
+      name="name"
+      @update:model-value="setName"
+    />
+
+    <NumberField
+      name="status"
+      @update:model-value="setConfig()"
+    />
+
+    <BooleanField
+      name="warn_headers_sent"
+      @update:model-value="setConfig()"
+    />
+
+    <InputsField
+      :items="inputOptions"
+      @change:input="setInput"
+      @change:inputs="setInputs"
+    />
+  </Form>
+</template>
+
+<script setup lang="ts">
+import Form from '../../../shared/Form.vue'
+import { ExitNodeSchema } from '../node/mock'
+import { useNodeForm } from './composables'
+import { useTemplateRef } from 'vue'
+import StringField from '../../../shared/StringField.vue'
+import NumberField from '../../../shared/NumberField.vue'
+import BooleanField from '../../../shared/BooleanField.vue'
+import InputsField from './InputsField.vue'
+
+const formRef = useTemplateRef('form')
+
+const {
+  formData,
+  setName,
+  setConfig,
+  inputOptions,
+  setInputs,
+  setInput,
+} = useNodeForm(() => formRef.value!.getInnerData())
+</script>
