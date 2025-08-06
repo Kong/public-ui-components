01286594605c5356cff6253da4f4064b2afef676
Makito <i@maki.to>
2025-08-06 19:57:09 +0900

    feat(entities-plugins): datakit connect/disconnect edges (#2290)
    * feat(entities-plugins): datakit connect/disconnect nodes
    
    * style: space
    
    Co-authored-by: Yi Yang <Leopoldthecoder@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Yi Yang <Leopoldthecoder@users.noreply.github.com>

--- packages/entities/entities-plugins/src/components/free-form/Datakit/flow-editor/constants.ts
+++ packages/entities/entities-plugins/src/components/free-form/Datakit/flow-editor/constants.ts
@@ -2,7 +2,7 @@ import type { AutoLayoutOptions } from './composables/useFlow'
 
 export const AUTO_LAYOUT_DEFAULT_OPTIONS = {
   padding: 80,
-  nodeGap: 80,
+  nodeGap: 40,
   edgeGap: 80,
   rankGap: 80,
 } as const satisfies Pick<AutoLayoutOptions, 'padding' | 'nodeGap' | 'edgeGap' | 'rankGap'>
