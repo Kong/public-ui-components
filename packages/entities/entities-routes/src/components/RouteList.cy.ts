9918a374bda23439bb0346eab6190634447ceb7c
Afroz Hussain <36099616+afrzhussain@users.noreply.github.com>
2025-08-12 17:04:52 +0530

    chore: clean up l2 empty state flag (#2310)

--- packages/entities/entities-routes/src/components/RouteList.cy.ts
+++ packages/entities/entities-routes/src/components/RouteList.cy.ts
@@ -609,8 +609,8 @@ describe('<RouteList />', () => {
 
       cy.wait('@getRoutes')
       cy.get('.kong-ui-entities-routes-list').should('be.visible')
-      cy.get('.table-empty-state').should('be.visible')
-      cy.get('.table-empty-state .empty-state-action .k-button').should('be.visible')
+      cy.getTestId('routes-entity-empty-state').should('be.visible')
+      cy.getTestId('entity-create-button').should('be.visible')
     })
 
     it('should hide empty state and create route cta if user can not create', () => {
@@ -629,8 +629,8 @@ describe('<RouteList />', () => {
 
       cy.wait('@getRoutes')
       cy.get('.kong-ui-entities-routes-list').should('be.visible')
-      cy.get('.table-empty-state').should('be.visible')
-      cy.get('.table-empty-state .empty-state-action .k-button').should('not.exist')
+      cy.getTestId('routes-entity-empty-state').should('be.visible')
+      cy.getTestId('entity-create-button').should('not.exist')
     })
 
     it('should handle error state', () => {
