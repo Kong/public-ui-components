e08ec5d30bb91e430364ac2fd25b59b73665ffaa
GU Yiling <justice360@gmail.com>
2025-08-18 14:38:02 +0800

    feat(entities-plugins): datakit load data, simplify node select (#2332)

--- packages/entities/entities-plugins/src/components/free-form/Datakit/flow-editor/store/history.ts
+++ packages/entities/entities-plugins/src/components/free-form/Datakit/flow-editor/store/history.ts
@@ -1,10 +1,16 @@
 import { useManualRefHistory } from '@vueuse/core'
 import type { Ref } from 'vue'
 
+type HistoryAction = 'commit' | 'undo' | 'redo' | 'clear' | 'reset'
+
 /** Minimal tagged history. Same tag + replace drops previous snapshot. */
 export function useTaggedHistory<T>(
   stateRef: Ref<T>,
-  options?: { capacity?: number, clone?: (v: T) => T },
+  options?: {
+    capacity?: number
+    clone?: (v: T) => T
+    onHistoryChange?: (action: HistoryAction, snapshot: T) => void
+  },
 ) {
   const {
     commit: baseCommit,
@@ -22,6 +28,10 @@ export function useTaggedHistory<T>(
 
   let lastTag: string | undefined
 
+  function notify(action: HistoryAction) {
+    options?.onHistoryChange?.(action, stateRef.value)
+  }
+
   function commit(tag?: string, opts?: { replace?: boolean }) {
     const replace = !!opts?.replace
     if (replace && tag && lastTag === tag && undoStack.value.length > 0) {
@@ -29,23 +39,28 @@ export function useTaggedHistory<T>(
     }
     baseCommit()
     lastTag = tag
+    notify('commit')
   }
 
   function undo() {
     baseUndo()
     lastTag = undefined
+    notify('undo')
   }
   function redo() {
     baseRedo()
     lastTag = undefined
+    notify('redo')
   }
   function clear() {
     baseClear()
     lastTag = undefined
+    notify('clear')
   }
   function reset() {
     baseReset()
     lastTag = undefined
+    notify('reset')
   }
 
   return {
