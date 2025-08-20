5014bfbaa132ad2e6afa2925c43a28adfb97c855
ValeryG <val.gorodnichev@konghq.com>
2025-08-20 13:53:01 -0600

    fix: force publishing

--- packages/core/i18n/src/index.ts
+++ packages/core/i18n/src/index.ts
@@ -1,5 +1,6 @@
 export { default as useI18n, createI18n } from './i18n'
 export { default as Translation, i18nTComponent } from './Translation'
 
+
 export type { IntlMessageFormatOptions } from './i18n'
 export type * from './types'
