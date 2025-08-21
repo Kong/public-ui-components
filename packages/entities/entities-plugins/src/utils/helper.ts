c45a6f475993c7660f091ed00eba9132a01de43b
Zehao <zehao93@outlook.com>
2025-08-21 15:48:05 +0800

    fix(entities-plugins): kafka-* set confluent to null if is default value [KM-1616] (#2365)
    (cherry picked from commit e78c6d0f6313014f5df90fff3086d60b0a116351)

--- packages/entities/entities-plugins/src/utils/helper.ts
+++ packages/entities/entities-plugins/src/utils/helper.ts
@@ -1,3 +1,5 @@
+import { isEqual } from 'lodash-es'
+
 export interface SchemaRegistry {
   confluent?: {
     authentication?: {
@@ -5,11 +7,33 @@ export interface SchemaRegistry {
         username?: string
         password?: string
       }
+      mode?: string
     }
+    ssl_verify?: boolean
+  } | null
+}
+
+const isDefaultConfluent = (confluent: SchemaRegistry['confluent']): boolean => {
+  const keys = Object.keys(confluent!)
+  if (keys.length === 0) return true
+
+  // kafka-upstream, kafka-log, kafka-upstream
+  if (keys.length === 2 && keys.includes('ssl_verify') && keys.includes('authentication')) {
+    return isEqual(confluent!, {
+      ssl_verify: true,
+      authentication: {
+        mode: 'none',
+      },
+    })
   }
+  return false
 }
 
 export const stripEmptyBasicFields = (schemaRegistry: SchemaRegistry) => {
+  if (schemaRegistry.confluent && isDefaultConfluent(schemaRegistry?.confluent)) {
+    schemaRegistry.confluent = null
+  }
+
   const basicFields = schemaRegistry?.confluent?.authentication?.basic
   if (!basicFields) {
     return
