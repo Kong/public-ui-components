2862e8a325011128c6a20266b8d7eb10523d3b2a
Zehao <zehao93@outlook.com>
2025-08-24 00:22:16 +0800

    fix(entities-plugins): datakit - allow static node to save an empty field value (#2371)

--- packages/entities/entities-plugins/src/components/free-form/Datakit/flow-editor/node-forms/NodeFormStatic.vue
+++ packages/entities/entities-plugins/src/components/free-form/Datakit/flow-editor/node-forms/NodeFormStatic.vue
@@ -84,8 +84,12 @@ const formData = computed(() => {
   // gather fieldNames from fields
   const outputsFieldNamesFromFields = currentNode.value!.fields.output.map(field => field.name)
 
+  // gather fieldNames from values
+  const outputsFieldNamesFromValues = Object.keys(values)
+
   // combine fieldNames
   const outputsFieldNames = new Set([
+    ...outputsFieldNamesFromValues,
     ...outputsFieldNamesFromEdges,
     ...outputsFieldNamesFromFields,
   ])
@@ -107,8 +111,9 @@ const formData = computed(() => {
 
 function handleAddField(name: FieldName, value?: string) {
   if (!currentNode.value) throw new Error('No selected node')
-  addField(nodeId, 'output', name, !value)
-  if (value) {
+  const isDefined = value !== undefined
+  addField(nodeId, 'output', name, !isDefined)
+  if (isDefined) {
     const next = {
       values: fieldValueToStoreValue({ ...formData.value.values, [name]: value }),
     }
