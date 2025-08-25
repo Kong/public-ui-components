34b0bad6c3bd4cae70c8dffeb2c857ae79337e75
Jordon Leach <jordonleach@gmail.com>
2025-08-25 15:59:07 -0400

    feat(dashboard-renderer): add empty tile state and card [MA-3949]

---
+++ packages/analytics/dashboard-renderer/sandbox/pages/EmptyDashboardDemo.vue
@@ -0,0 +1,28 @@
+<template>
+  <SandboxLayout
+    :links="appLinks"
+    title="Empty Dashboard"
+  >
+    <DashboardEmptyState
+      :ghost-tiles="ghostTiles"
+      message="This is my custom message"
+      @add-tile="handleAddTile"
+    />
+  </SandboxLayout>
+</template>
+
+<script setup lang="ts">
+import { inject, ref } from 'vue'
+import type { SandboxNavigationItem } from '@kong-ui-public/sandbox-layout'
+import { SandboxLayout } from '@kong-ui-public/sandbox-layout'
+import DashboardEmptyState from '../../src/components/DashboardEmptyState.vue'
+
+const appLinks: SandboxNavigationItem[] = inject('app-links', [])
+
+const ghostTiles = ref(2)
+
+const handleAddTile = () => {
+  console.log('@add-tile')
+  ghostTiles.value += 1
+}
+</script>
---
+++ packages/analytics/dashboard-renderer/src/components/DashboardEmptyTile.vue
@@ -0,0 +1,44 @@
+<template>
+  <KCard>
+    <KEmptyState
+      data-testid="dashboard-empty-state"
+      icon-background
+      :message="message"
+      :title="i18n.t('empty_tile.title')"
+    >
+      <template #icon>
+        <AddChartIcon decorative />
+      </template>
+
+      <template #action>
+        <KButton
+          appearance="secondary"
+          :disabled="!canAddTile"
+          @click.prevent="$emit('add-tile')"
+        >
+          <AddIcon decorative />
+          {{ i18n.t('empty_tile.add') }}
+        </KButton>
+      </template>
+    </KEmptyState>
+  </KCard>
+</template>
+
+<script setup lang="ts">
+import { AddIcon, AddChartIcon } from '@kong/icons'
+import composables from '../composables'
+
+withDefaults(defineProps<{
+  canAddTile?: boolean
+  message?: string
+}>(), {
+  canAddTile: true,
+  message: undefined,
+})
+
+const { i18n } = composables.useI18n()
+
+defineEmits<{
+  (e: 'add-tile'): void
+}>()
+</script>
