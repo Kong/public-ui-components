eb7005f415735aa76cbda4e60cb077cc1718aa77
Filip <filgut@protonmail.com>
2025-08-26 10:34:35 -0700

    feat: add choropleth_map type to dashboard renderer and schema (#2392)

--- packages/analytics/analytics-geo-map/src/types/index.ts
+++ packages/analytics/analytics-geo-map/src/types/index.ts
@@ -1,7 +1,6 @@
 import type { FeatureCollection } from 'geojson'
 
 export * from './map-options'
-export * from './country-codes'
 export * from './metric-units'
 
 export type MapFeatureCollection = FeatureCollection
---
+++ packages/analytics/analytics-geo-map/src/utils/explore-to-country-metrics.ts
@@ -0,0 +1,23 @@
+import type { ExploreResultV4 } from '@kong-ui-public/analytics-utilities'
+
+export const exploreResultToCountryMetrics = (exploreResult: ExploreResultV4): Record<string, number> => {
+  if (!exploreResult || !exploreResult.data) {
+    return {}
+  }
+
+  const metrics = {} as Record<string, number>
+  const data = exploreResult.data
+
+  if (data) {
+    for (const row of data) {
+      const countryKey = row?.event?.iso_code
+      const metricKey = exploreResult.meta.metric_names?.[0]
+
+      if (countryKey) {
+        metrics[countryKey] = metricKey ? row.event[metricKey] as number : 0
+      }
+    }
+  }
+
+  return metrics
+}
