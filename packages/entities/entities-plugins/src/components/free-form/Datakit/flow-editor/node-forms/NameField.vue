b80e5c24ce8c2c550c2acbd82ffe90aadbcfda8f
Zehao Zhang <zehao93@outlook.com>
2025-08-26 11:18:11 +0800

    fix(*): remove the import of defineProps

--- packages/entities/entities-plugins/src/components/free-form/Datakit/flow-editor/node-forms/NameField.vue
+++ packages/entities/entities-plugins/src/components/free-form/Datakit/flow-editor/node-forms/NameField.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineProps, ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 import useI18n from '../../../../../composables/useI18n'
 import type { ValidatorFn } from '../composables/validation'
 
