23637aebe52dd0e6abc7ec537d36f24d02ddd3b6
Jordon Leach <jordonleach@gmail.com>
2025-08-28 14:22:05 -0400

    chore(analytics): remove metric cards package [MA-4105]
    BREAKING CHANGE: The packages/analytics/metric-cards package is being deleted in favor of
    analytics-metric-provider

--- packages/analytics/metric-cards/src/constants.ts
+++
@@ -1,8 +0,0 @@
-
-export enum MetricCardSize {
-  Small = 'sm',
-  Medium = 'md',
-  Large = 'lg',
-  LargeCompact = 'lgc',
-  ExtraLarge = 'xl',
-}
--- packages/analytics/metric-cards/src/types/aproximate-number.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'approximate-number'
