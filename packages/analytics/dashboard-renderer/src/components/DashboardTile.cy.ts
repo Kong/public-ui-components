32d69cf36039d7d8a51a0174dd10dab620436971
Jordon Leach <jordonleach@gmail.com>
2025-09-02 10:56:41 -0400

    fix(analytics): exclude irrelevant filters for view requests [MA-4279] (#2431)

--- packages/analytics/dashboard-renderer/src/components/DashboardTile.cy.ts
+++ packages/analytics/dashboard-renderer/src/components/DashboardTile.cy.ts
@@ -197,6 +197,23 @@ describe('<DashboardTile />', () => {
       .should('not.have.string', 'response_model')
   })
 
+  it('excludes irrelevant context filters from the jump to requests URL', () => {
+    // Passes an llm_usage filter into an api_usage tile
+    const context = {
+      ...mockContext,
+      filters: [{ field: 'response_model', operator: 'in', value: 'my-model' }],
+    }
+
+    mount({ context })
+
+    cy.getTestId('kebab-action-menu-1').click()
+    cy.getTestId('chart-jump-to-requests-1').should('exist')
+
+    cy.getTestId('chart-jump-to-requests-1')
+      .invoke('attr', 'href')
+      .should('not.have.string', 'response_model')
+  })
+
   it('should show aged out warning when query granularity does not match saved granularity', () => {
     mount({
       definition: {
