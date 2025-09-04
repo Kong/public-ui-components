1716f5ed6f3946e469d72d82651f00eec686396b
Zehao <zehao93@outlook.com>
2025-09-04 16:29:52 +0800

    fix(entities-plugins): schema registry data transform [KM-1698] (#2443)

--- packages/entities/entities-plugins/src/utils/helper.ts
+++ packages/entities/entities-plugins/src/utils/helper.ts
@@ -1,45 +1,76 @@
 import { isEqual } from 'lodash-es'
 
 export interface SchemaRegistry {
-  confluent?: {
-    authentication?: {
-      basic?: {
-        username?: string
-        password?: string
-      }
-      mode?: string
-    }
-    ssl_verify?: boolean
-  } | null
+  confluent?: ConfluentConfig | null
 }
 
-const isDefaultConfluent = (confluent: SchemaRegistry['confluent']): boolean => {
-  const keys = Object.keys(confluent!)
-  if (keys.length === 0) return true
-
-  // kafka-upstream, kafka-log, kafka-upstream
-  if (keys.length === 2 && keys.includes('ssl_verify') && keys.includes('authentication')) {
-    return isEqual(confluent!, {
-      ssl_verify: true,
-      authentication: {
-        mode: 'none',
-      },
-    })
-  }
-  return false
+export interface ConfluentConfig {
+  authentication?: AuthenticationConfig
+  ssl_verify?: boolean
 }
+
+export interface AuthenticationConfig {
+  mode?: 'none' | 'basic' | 'oauth2'
+  basic?: BasicAuthConfig
+  oauth2?: OAuth2Config
+  oauth2_client?: OAuth2ClientConfig
+}
+
+export interface BasicAuthConfig {
+  username?: string
+  password?: string
+}
+
+export interface OAuth2Config {
+  token_endpoint?: string
+  token_headers?: Record<string, string>
+  token_post_args?: Record<string, string>
+  grant_type?: 'client_credentials' | 'password'
+  client_id?: string
+  client_secret?: string
+  username?: string
+  password?: string
+  scopes?: string[]
+  audience?: string[]
+}
+
+export interface OAuth2ClientConfig {
+  auth_method?: 'client_secret_post' | 'client_secret_basic' | 'client_secret_jwt' | 'none'
+  client_secret_jwt_alg?: 'HS512' | 'HS256'
+  http_version?: number
+  http_proxy?: string
+  http_proxy_authorization?: string
+  https_proxy?: string
+  https_proxy_authorization?: string
+  no_proxy?: string
+  timeout?: number
+  keep_alive?: boolean
+  ssl_verify?: boolean
+}
+
+const emptyConfluent: ConfluentConfig = { authentication: { mode: 'none' }, ssl_verify: true }
 
 export const stripEmptyBasicFields = (schemaRegistry: SchemaRegistry) => {
-  if (schemaRegistry.confluent && isDefaultConfluent(schemaRegistry?.confluent)) {
+  // Remove the default values if the mode is 'none'
+  if (schemaRegistry.confluent?.authentication?.mode === 'none') {
+    delete schemaRegistry.confluent.authentication.oauth2
+    delete schemaRegistry.confluent.authentication.oauth2_client
+    delete schemaRegistry.confluent.authentication.basic
+  }
+
+  // Remove the default values if the mode is 'basic'
+  if (schemaRegistry.confluent?.authentication?.mode === 'basic') {
+    delete schemaRegistry.confluent.authentication.oauth2
+    delete schemaRegistry.confluent.authentication.oauth2_client
+  }
+
+  // Remove the default values if the mode is 'oauth2'
+  if (schemaRegistry.confluent?.authentication?.mode === 'oauth2') {
+    delete schemaRegistry.confluent.authentication.basic
+  }
+
+  // Remove the entire confluent if all are empty
+  if (isEqual(schemaRegistry.confluent, emptyConfluent)) {
     schemaRegistry.confluent = null
-  }
-
-  const basicFields = schemaRegistry?.confluent?.authentication?.basic
-  if (!basicFields) {
-    return
-  }
-
-  if (!basicFields.password && !basicFields.username) {
-    delete schemaRegistry!.confluent!.authentication!.basic
   }
 }
