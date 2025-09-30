20b1097746ca83bcdf1697decc8dba4b3ec58170
Jordon Leach <jordonleach@gmail.com>
2025-09-30 11:05:50 -0400

    chore(analytics): clean up analytics sandboxes

--- packages/analytics/analytics-chart/sandbox/pages/TimeSeriesChartDemo.vue
+++ packages/analytics/analytics-chart/sandbox/pages/TimeSeriesChartDemo.vue
@@ -143,7 +143,7 @@
               v-if="exportModalVisible"
               :export-state="exportState"
               filename="asdf.csv"
-              @toggle-modal="setModalVisibility"
+              @close-modal="setModalVisibility(false)"
             />
           </div>
         </div>
