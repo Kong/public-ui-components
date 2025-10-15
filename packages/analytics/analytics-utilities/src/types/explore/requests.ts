39302d183725db5db62c28f1ae103cf1da3d9589
Filip Gutica <filgut@protonmail.com>
2025-10-15 11:22:06 -0700

    fix: remove country_code from wildcard dimensions
    this will remove `starts_with` and `ends_with` as avaialable operators when
    filtering on country_code in API Requests

--- packages/analytics/analytics-utilities/src/types/explore/requests.ts
+++ packages/analytics/analytics-utilities/src/types/explore/requests.ts
@@ -47,7 +47,6 @@ export type FilterableRequestDimensions = typeof filterableRequestDimensions[num
 export const queryableRequestWildcardDimensions = [
   'auth_type',
   'client_ip',
-  'country_code',
   'data_plane_node_version',
   'header_host',
   'header_user_agent',
