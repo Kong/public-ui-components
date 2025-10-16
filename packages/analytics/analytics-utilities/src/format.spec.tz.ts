02669132f3e04d1eda0dbf1f29ef1d0f53c60706
Filip Gutica <filgut@protonmail.com>
2025-10-16 15:27:32 -0700

    fix(analytics-utilities): update timestamp formatter [MA-4392]
    update timestamp formatter as per requirements
    
    BREAKING CHANGE: rename/update "formatISOTimeWithTZ" fn exported by analytics-utilities

--- packages/analytics/analytics-utilities/src/format.spec.tz.ts
+++ packages/analytics/analytics-utilities/src/format.spec.tz.ts
@@ -1,21 +1,31 @@
 import { it, expect } from 'vitest'
-import { formatISOTimeWithTZ } from './format'
+import { formatLogTimestamp } from './format'
 import { runNonUtcTest, runUtcTest } from './specUtils'
 
 runNonUtcTest('date formatting, non-UTC', () => {
-  it('formatISOTimeWithTZ should work', () => {
-    const pattern = /20\d{2}-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\d[+-]\d\d?:\d\d/
+  it('formatLogTimestamp should work', () => {
+    const pattern = /^20\d{2}-\d{2}-\d{2}[ T]\d{2}:\d{2}:\d{2}\.\d{3}(?:Z|[+-]\d{2}:?\d{2})?$/
     const date = new Date('2021-01-01T06:02:03.000Z')
-    expect(formatISOTimeWithTZ(date)).toMatch(pattern)
-    expect(formatISOTimeWithTZ(date.getTime())).toMatch(pattern)
+    expect(formatLogTimestamp(date)).toMatch(pattern)
+    expect(formatLogTimestamp(date.getTime())).toMatch(pattern)
+  })
+
+  it('formatLogTimestamp with TZ should work', () => {
+    const date = new Date('2021-01-01T06:02:03.000Z')
+    const formatted = formatLogTimestamp(date, { tz: 'America/New_York', includeTZ: true })
+    expect(formatted).toBe('2021-01-01 01:02:03.000 (EST)')
   })
 })
 
 runUtcTest('date formatting in UTC', () => {
-  it('formatISOTimeWithTZ should work', () => {
+  it('formatLogTimestamp should work', () => {
     const str = '2020-01-01T01:02:03.000Z'
+    const expectedNoTz = '2020-01-01 01:02:03.000'
+    const expectedWithTz = '2020-01-01 01:02:03.000 (UTC)'
     const date = new Date(str)
-    expect(formatISOTimeWithTZ(date)).toBe(str)
-    expect(formatISOTimeWithTZ(date.getTime())).toBe(str)
+    expect(formatLogTimestamp(date)).toBe(expectedNoTz)
+    expect(formatLogTimestamp(date.getTime())).toBe(expectedNoTz)
+
+    expect(formatLogTimestamp(date, { tz: 'UTC', includeTZ: true })).toBe(expectedWithTz)
   })
 })
