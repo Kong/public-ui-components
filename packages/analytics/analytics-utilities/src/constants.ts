f581a05ff91db1cf09b6be53974c86ad502fb7f5
Rocky <adorack@users.noreply.github.com>
2025-10-17 09:53:25 -0400

    feat: increase the CSV export limit to 1000 [MA-4360] (#2537)

--- packages/analytics/analytics-utilities/src/constants.ts
+++ packages/analytics/analytics-utilities/src/constants.ts
@@ -1,5 +1,5 @@
 export const REFRESH_INTERVAL_MS = 30 * 1000
 
-export const EXPORT_RECORD_LIMIT = 250
+export const EXPORT_RECORD_LIMIT = 1000
 
 export const INJECT_QUERY_PROVIDER = 'analytics-query-provider'
