b7f4be1f11af1a663cfaa9d3de2f0addb2af877a
Zehao Zhang <zehao93@outlook.com>
2025-10-22 17:44:43 +0800

    fix: redis tag display

--- packages/entities/entities-redis-configurations/src/composables/useRedisConfigurationSelector.ts
+++ packages/entities/entities-redis-configurations/src/composables/useRedisConfigurationSelector.ts
@@ -6,9 +6,11 @@ import { FORMS_CONFIG } from '@kong-ui-public/forms'
 import { getRedisType } from '../helpers'
 import { useDebouncedFilter, type KongManagerBaseFormConfig, type KonnectBaseFormConfig } from '@kong-ui-public/entities-shared'
 import endpoints from '../partials-endpoints'
+import { RedisType } from '../types'
 
 import type { RedisConfigurationDTO } from '../types'
 import type { SelectItem } from '@kong/kongponents'
+import useI18n from './useI18n'
 
 export function useRedisConfigurationSelector(options: {
   redisType?: 'redis-ce' | 'redis-ee'
@@ -19,6 +21,7 @@ export function useRedisConfigurationSelector(options: {
 
   const pageSize = '1000' // the API returns all partials, so we have to set a high page size to filter them on the frontend
   const formConfig = inject<KonnectBaseFormConfig | KongManagerBaseFormConfig>(FORMS_CONFIG)
+  const { i18n: { t } } = useI18n()
 
   if (!formConfig) {
     throw new Error('FORMS_CONFIG must be provided')
@@ -35,9 +38,22 @@ export function useRedisConfigurationSelector(options: {
     searchKeys: ['id', 'name'],
   })
 
+  const typeToDisplayName = (redisType: RedisType): string => {
+    switch (redisType) {
+      case RedisType.CLUSTER:
+        return t('form.options.type.cluster')
+      case RedisType.HOST_PORT_CE:
+        return t('form.options.type.open_source')
+      case RedisType.SENTINEL:
+        return t('form.options.type.sentinel')
+      case RedisType.HOST_PORT_EE:
+        return t('form.options.type.enterprise')
+    }
+  }
+
   const availableRedisConfigs = computed((): Array<SelectItem<string>> => {
     const configs = (redisConfigsResults.value || [])
-      .map((el) => ({ label: el.id, name: el.name, value: el.id, type: el.type, tag: getRedisType(el as RedisConfigurationDTO) }))
+      .map((el) => ({ label: el.id, name: el.name, value: el.id, type: el.type, tag: typeToDisplayName(getRedisType(el as RedisConfigurationDTO)) }))
       // filter out non-redis configs
       // this is needed because the API returns all partials, not just redis configurations.
       .filter(partial => partial.type === 'redis-ce' || partial.type === 'redis-ee')
