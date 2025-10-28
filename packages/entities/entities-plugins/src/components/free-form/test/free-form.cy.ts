4770dc46ab3dcec5f2b2e040fa82b67ae62be637
Zehao Zhang <zehao93@outlook.com>
2025-10-28 11:47:57 +0800

    test(entities-plugins): add reactivity tests for free form

--- packages/entities/entities-plugins/src/components/free-form/test/free-form.cy.ts
+++ packages/entities/entities-plugins/src/components/free-form/test/free-form.cy.ts
@@ -5,6 +5,7 @@ import type { FormSchema } from 'src/types/plugins/form-schema'
 import StringField from '../shared/StringField.vue'
 import FieldRenderer from '../shared/FieldRenderer.vue'
 import { FIELD_RENDERERS } from '../shared/composables'
+import { ref } from 'vue'
 
 describe('Free Form', () => {
   it('Auto render', () => {
@@ -127,8 +128,143 @@ describe('Free Form', () => {
     })
   })
 
-  describe('Data binding', () => {
-    // todo
+  describe('Reactivity', () => {
+    function getSchema(defaultName?: string) : FormSchema {
+      return {
+        type: 'record',
+        fields: [
+          {
+            name: {
+              type: 'string',
+              default: defaultName,
+            },
+          },
+        ],
+      }
+    }
+
+    it('should call onChange with default value when initialized', () => {
+      const onChangeSpy = cy.spy().as('onChangeSpy')
+
+      cy.mount(Form, {
+        props: {
+          schema: getSchema('Default Name'),
+          onChange: onChangeSpy,
+        },
+      })
+      cy.get('@onChangeSpy')
+        .should('have.been.calledOnce')
+        .should('have.been.calledWith', { name: 'Default Name' })
+    })
+
+    it('should call onChange with initial value when initialized', () => {
+      const onChangeSpy = cy.spy().as('onChangeSpy')
+      const initialData = { name: 'Initial Name' }
+
+      cy.mount(Form, {
+        props: {
+          schema: getSchema(),
+          onChange: onChangeSpy,
+          data: initialData,
+        },
+      })
+      cy.get('@onChangeSpy')
+        .should('have.been.calledOnce')
+        .should('have.been.calledWith', initialData)
+    })
+
+    it('should call onChange when data changes', () => {
+      const onChangeSpy = cy.spy().as('onChangeSpy')
+
+      cy.mount(Form, {
+        props: {
+          schema: getSchema(),
+          onChange: onChangeSpy,
+        },
+      })
+
+      cy.get('@onChangeSpy')
+        .should('have.been.calledOnce')
+        .should('have.been.calledWith', { name: null })
+
+      cy.getTestId('ff-name').type('n')
+      cy.get('@onChangeSpy')
+        .should('have.been.calledTwice')
+        .should('have.been.calledWith', { name: 'n' })
+    })
+
+    it('should update form when data prop changes', () => {
+      const onChangeSpy = cy.spy().as('onChangeSpy')
+
+      const data = ref({ name: 'Initial Name' })
+      const newData = { name: 'New Name' }
+
+      cy.mount(Form, {
+        props: {
+          schema: getSchema(),
+          data,
+          onChange: onChangeSpy,
+        },
+      })
+
+      cy.getTestId('ff-name').should('have.value', 'Initial Name')
+      cy.get('@onChangeSpy')
+        .should('have.been.calledOnce')
+        .should('have.been.calledWith', data.value)
+
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(0).then(() => { // wait for next tick
+        data.value = newData
+        cy.getTestId('ff-name').should('have.value', 'New Name')
+        cy.get('@onChangeSpy')
+          .should('have.been.calledTwice')
+          .should('have.been.calledWith', newData)
+      })
+    })
+
+    it('should call `prepareFormData` on initial data', () => {
+      const prepareFormData = cy.spy(() => {
+        return {
+          name: 'New Name from prepareFormData',
+        }
+      }).as('prepareFormDataSpy')
+
+      cy.mount(Form, {
+        props: {
+          schema: getSchema(),
+          config: { prepareFormData },
+        },
+      })
+
+      cy.getTestId('ff-name').should('have.value', 'New Name from prepareFormData')
+      cy.get('@prepareFormDataSpy').should('have.been.calledOnce')
+    })
+
+    it('should call `hasValue` on initial changes', () => {
+      const hasValue = cy.spy(() => false).as('hasValueSpy')
+
+      cy.mount(Form, {
+        props: {
+          schema: getSchema(),
+          data: { name: 'Some Name' },
+          config: { hasValue },
+        },
+      })
+
+      cy.get('@hasValueSpy')
+        .should('have.been.calledOnce')
+        .should('have.been.calledWith', { name: 'Some Name' })
+    })
+
+    /**
+     * todo:
+     * for every type of field, test that:
+     * - initialize with default value
+     * - initialize with initial value
+     * - empty value
+     * - active update by user operation
+     * - passive update by external data prop change
+     */
   })
 
 })
