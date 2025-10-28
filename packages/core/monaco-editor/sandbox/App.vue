c736ef29f8a7b70bd8865d7490cbf41130625f35
Arash Ari Sheyda <38922203+arashsheyda@users.noreply.github.com>
2025-10-28 10:35:39 -0600

    feat(core): add monaco-editor [KHCP-18422] (#2599)
    * feat(core): add monaco-editor [KHCP-18422]
    
    * chore: update codeowners

---
+++ packages/core/monaco-editor/sandbox/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="sandbox-container">
+    <main>
+      <p>This is the component sandbox.</p>
+      <MonacoEditor />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MonacoEditor } from '../src'
+</script>
