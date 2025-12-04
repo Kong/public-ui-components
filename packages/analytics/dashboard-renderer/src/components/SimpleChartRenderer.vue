f8aad6938119ae273f41fa68f44ff2ebcd706eed
afrzhussain <afroz.hussain@konghq.com>
2025-12-04 08:45:30 +0530

    fix: removed more things

--- packages/analytics/dashboard-renderer/src/components/SimpleChartRenderer.vue
+++ packages/analytics/dashboard-renderer/src/components/SimpleChartRenderer.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>testing</div>
+  <QueryDataProvider
+    :context="context"
+    :query="query"
+    :query-ready="queryReady"
+    :refresh-counter="refreshCounter"
+  >
+    <!-- <div
+      class="analytics-chart"
+      :class="{ 'single-value': isSingleValueChart }"
+    >
+      <SimpleChart
+        :chart-data="data"
+        :chart-options="chartOptions"
+        :synthetics-data-key="isSingleValueChart ? undefined : (chartOptions as GaugeChartOptions).synthetics_data_key"
+      />
+    </div> -->
+  </QueryDataProvider>
 </template>
+
+<script setup lang="ts">
+import type { RendererProps } from '../types'
+import type { GaugeChartOptions, SingleValueOptions } from '@kong-ui-public/analytics-utilities'
+import QueryDataProvider from './QueryDataProvider.vue'
+defineProps<RendererProps<GaugeChartOptions | SingleValueOptions>>()
+
+</script>
