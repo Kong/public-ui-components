9b39c7dac74aa623f5fc58fdb3dac13d2719623e
Justineo <justice360@gmail.com>
2025-12-25 17:10:26 +0800

    fix(entities-plugins): add type annotation for FEATURE_FLAGS

--- packages/entities/entities-plugins/src/constants/index.ts
+++ packages/entities/entities-plugins/src/constants/index.ts
@@ -1,6 +1,8 @@
+import type { InjectionKey } from 'vue'
+
 export const EXPERIMENTAL_FREE_FORM_PROVIDER = Symbol('EXPERIMENTAL_FREE_FORM_PROVIDER')
 
-export const FEATURE_FLAGS = {
+export const FEATURE_FLAGS: Record<string, InjectionKey<boolean>> = {
   DATAKIT_ENABLE_FLOW_EDITOR: Symbol('DATAKIT_ENABLE_FLOW_EDITOR'),
 }
 
