495c12a355f511df7dff7256764f1a071202b565
Makito <i@maki.to>
2026-01-07 11:59:20 +0900

    feat(monaco-editor): add lifecycle utils for disposables (#2769)
    * feat(monaco-editor): add lifecycle utils for disposables
    
    refactor: improve robustness and fix potential bugs
    
    test: add tests
    
    * fix: more readable constant name
    
    * fix: address edge cases from review comments
    
    * fix: address more review comments
    
    * fix: dispose on watcher cleanup and update the condition to set up new editors
    
    * fix: idempotency
    
    * test: update test cases
    
    * docs: add documentation comments
    
    * chore: explicit function return types
    
    * fix: make scope required
    
    * fix: address review comments
    
    * fix: address review comments
    
    * fix: save some curly brackets
    
    * fix: remove redundant imports
    
    * test: update test cases

--- packages/core/monaco-editor/src/composables/useMonacoEditor.ts
+++ packages/core/monaco-editor/src/composables/useMonacoEditor.ts
@@ -1,13 +1,15 @@
-import { onActivated, onBeforeUnmount, onMounted, reactive, toValue, watch, ref } from 'vue'
+import { onActivated, onBeforeUnmount, onMounted, onWatcherCleanup, reactive, ref, shallowRef, toValue, watch } from 'vue'
 import { DEFAULT_MONACO_OPTIONS } from '../constants'
 import { unrefElement, useDebounceFn } from '@vueuse/core'
 
 import * as monaco from 'monaco-editor'
 import { shikiToMonaco } from '@shikijs/monaco'
 import { getSingletonHighlighter, bundledLanguages, bundledThemes } from 'shiki'
+import * as lifecycle from '../singletons/lifecycle'
 
 import type { MaybeComputedElementRef, MaybeElement } from '@vueuse/core'
 import type { MonacoEditorStates, UseMonacoEditorOptions } from '../types'
+import type { editor as Editor } from 'monaco-editor'
 
 // Flag if monaco loaded
 const isMonacoLoaded = ref(false)
@@ -59,10 +61,9 @@ export function useMonacoEditor<T extends MaybeElement>(
 ) {
   /**
    * The Monaco editor instance.
-   * @type {monaco.editor.IStandaloneCodeEditor | undefined}
    * @default undefined
   */
-  let editor: monaco.editor.IStandaloneCodeEditor | undefined
+  const editor = shallowRef<Editor.IStandaloneCodeEditor>()
 
   // Internal flag to prevent multiple setups
   let _isSetup = false
@@ -77,16 +78,16 @@ export function useMonacoEditor<T extends MaybeElement>(
 
   /** Replace the editor content. */
   const setContent = (content: string): void => {
-    if (!_isSetup || !editor) return
+    if (!_isSetup || !editor.value) return
     // TODO: update this so we can preserve undo/redo stack
-    editor.setValue(content)
+    editor.value.setValue(content)
   }
 
   /** Toggle read-only mode. */
-  const setReadOnly = (readOnly: boolean): void => editor?.updateOptions({ readOnly })
+  const setReadOnly = (readOnly: boolean): void => editor.value?.updateOptions({ readOnly })
 
   /** Focus the editor programmatically. */
-  const focus = (): void => editor?.focus()
+  const focus = (): void => editor.value?.focus()
 
   /**
    * Triggers a keyboard command in the Monaco editor.
@@ -95,9 +96,9 @@ export function useMonacoEditor<T extends MaybeElement>(
    */
   const triggerKeyboardCommand = (id: string): void => {
     try {
-      if (!editor || !id) return
-      editor.focus()
-      editor.trigger('keyboard', id, null)
+      if (!editor.value || !id) return
+      editor.value.focus()
+      editor.value.trigger('keyboard', id, null)
     } catch (error) {
       console.error(`useMonacoEditor: Failed to trigger command: ${id}`, error)
     }
@@ -106,12 +107,12 @@ export function useMonacoEditor<T extends MaybeElement>(
   /** Toggle the status of findController widget */
   const toggleSearchWidget = (): void => {
     try {
-      if (!editor) return
+      if (!editor.value) return
 
       // close the widget
       if (editorStates.searchBoxIsRevealed) {
         // @ts-ignore - property exists
-        return editor!.getContribution('editor.contrib.findController')?.closeFindWidget()
+        return editor.value.getContribution('editor.contrib.findController')?.closeFindWidget()
       }
 
       triggerKeyboardCommand('actions.find')
@@ -130,17 +131,18 @@ export function useMonacoEditor<T extends MaybeElement>(
     let model: monaco.editor.ITextModel | undefined
 
     // `toValue()` safely unwraps refs, getters, or plain elements
-    watch([isMonacoLoaded, () => toValue(target)], ([_isLoaded, _target]) => {
+    watch([isMonacoLoaded, () => toValue(target)], ([_isLoaded, _target], [, previousTarget]) => {
 
       // This ensures we skip setup if it's null, undefined, or an SVG element (as unrefElement can return SVGElement)
       const el = unrefElement(_target)
+      const previousEl = unrefElement(previousTarget)
       if (!(el instanceof HTMLElement) || !_isLoaded) {
         _isSetup = false
         return
       }
 
-      // prevent multiple setups
-      if (_isSetup) return
+      // Only set up when not already set up or target element changed
+      if (_isSetup && previousEl === el) return
 
       if (!model) {
         // we want to create our model before creating the editor so we don't end up with multiple models for the same editor (v-if toggles, etc.)
@@ -148,7 +150,7 @@ export function useMonacoEditor<T extends MaybeElement>(
         model = monaco.editor.createModel(options.code.value, options.language, uri)
       }
 
-      editor = monaco.editor.create(el, {
+      editor.value = monaco.editor.create(el, {
         ...DEFAULT_MONACO_OPTIONS,
         readOnly: options.readOnly || false,
         language: options.language,
@@ -163,11 +165,13 @@ export function useMonacoEditor<T extends MaybeElement>(
       editorStates.hasContent = !!options.code.value
 
       // Watch content changes and trigger callbacks efficiently
-      editor.onDidChangeModelContent(() => {
-        const content = editor!.getValue()
-        editorStates.hasContent = !!content.length
-        options.onChanged?.(content)
-      })
+      lifecycle.trackForEditor(editor.value,
+        editor.value.onDidChangeModelContent(() => {
+          const content = editor.value!.getValue()
+          editorStates.hasContent = !!content.length
+          options.onChanged?.(content)
+        }),
+      )
 
       // TODO: register editor actions
 
@@ -179,7 +183,7 @@ export function useMonacoEditor<T extends MaybeElement>(
 
       try {
         // Access the internal "FindController" contribution
-        const findController = editor.getContribution('editor.contrib.findController')
+        const findController = editor.value.getContribution('editor.contrib.findController')
 
         if (findController) {
           // Get the state object from the FindController
@@ -187,14 +191,20 @@ export function useMonacoEditor<T extends MaybeElement>(
           const findState = findController.getState()
 
           // Listen for changes to the state of the "find" panel
-          findState.onFindReplaceStateChange(() => {
-            editorStates.searchBoxIsRevealed = findState.isRevealed
-          })
+          lifecycle.trackForEditor(editor.value,
+            findState.onFindReplaceStateChange(() => {
+              editorStates.searchBoxIsRevealed = findState.isRevealed
+            }), // This returns a disposable
+          )
         }
       } catch (error) {
         console.error('useMonacoEditor: Failed to get the state of findController', error)
       }
 
+      // Dispose the editor if any
+      onWatcherCleanup(() => {
+        editor.value?.dispose()
+      })
     }, {
       immediate: true,
       flush: 'post',
@@ -210,10 +220,10 @@ export function useMonacoEditor<T extends MaybeElement>(
   onActivated(remeasureFonts)
 
   onBeforeUnmount(() => {
-    if (!editor) return
+    if (!editor.value) return
 
-    const model = editor.getModel()
-    editor.dispose()
+    const model = editor.value.getModel()
+    editor.value.dispose()
     if (model) {
       model.dispose()
     }
---
+++ packages/core/monaco-editor/src/singletons/lifecycle.ts
@@ -0,0 +1,280 @@
+/**
+ * Lifecycle tracker for {@link IDisposable disposables} in Monaco Editor.
+ *
+ * In Monaco Editor, when attaching listeners in the global scope (e.g.,
+ * {@link Editor.onDidCreateModel onDidCreateModel}) or in the per-resource scope
+ * (e.g.,{@link Editor.ITextModel.onDidChangeContent onDidChangeContent}), an
+ * {@link IDisposable disposable} is always returned to allow detaching the listener.
+ *
+ * However, per-resource-scoped disposables are not automatically disposed when
+ * the resource is disposed. This can lead to memory leaks if the disposables are
+ * not properly tracked and disposed.
+ *
+ * This tracker is designed to be a singleton to track disposables in all the scopes
+ * mentioned above.
+ */
+
+import { type editor as Editor, type IDisposable } from 'monaco-editor'
+
+const MSG_PREFIX = '[monaco-editor] [lifecycle]'
+
+/**
+ * A type friendly representation of a scope.
+ */
+type Scope
+  = { type: 'editor', source: Editor.ICodeEditor }
+  | { type: 'model', source: Editor.ITextModel }
+
+/**
+ * The source (i.e., resource) instance of a scope.
+ */
+type ScopeSource = Editor.ICodeEditor | Editor.ITextModel
+
+/**
+ * Data for a scope that is being tracked.
+ */
+interface ScopeData {
+  /**
+   * Disposables being tracked for a scope.
+   */
+  disposables: Set<IDisposable>
+
+  /**
+   * The disposable of the listener who signals the disposal of the scope.
+   * This allows the listener itself to also be safely disposed.
+   */
+  scopeListener: IDisposable
+}
+
+/**
+ * A representation of a disposable being tracked.
+ */
+interface Tracked {
+  /**
+   * The decorated (returned by {@link track} and other track functions) disposable
+   * to dispose the original disposable and untrack the decorated disposable itself.
+   */
+  decorated: IDisposable
+
+  /**
+   * The scope of the original disposable being tracked, if any.
+   */
+  scope?: Scope
+}
+
+const scopedDisposables = new WeakMap<ScopeSource, ScopeData>()
+const allDisposables = new Map<IDisposable, Tracked>() // [Original: Tracked]
+
+/**
+ * A map to find the original disposable from a decorated disposable.
+ *
+ * This is helpful when checking if a decorated disposable has been provided to
+ * {@link track}.
+ */
+const reversedLookup = new WeakMap<IDisposable, IDisposable>()
+
+function ensureScopeData(scope: Scope): ScopeData {
+  let scopeData = scopedDisposables.get(scope.source)
+
+  if (!scopeData) {
+    let listener: IDisposable
+    switch (scope.type) {
+      case 'editor':
+        listener = scope.source.onDidDispose(() => disposeScoped(scope.source))
+        break
+      case 'model':
+        listener = scope.source.onWillDispose(() => disposeScoped(scope.source))
+        break
+      default:
+        throw new Error(`${MSG_PREFIX} Unknown scope: ${scope}`)
+    }
+
+    scopeData = {
+      disposables: new Set(),
+      scopeListener: listener,
+    }
+    scopedDisposables.set(scope.source, scopeData)
+  }
+
+  return scopeData
+}
+
+function cleanupScope(source: ScopeSource): void {
+  const data = scopedDisposables.get(source)
+  if (data) {
+    data.scopeListener.dispose()
+    scopedDisposables.delete(source)
+  }
+}
+
+function attachToScope(disposable: IDisposable, scope: Scope): void {
+  const scopeData = ensureScopeData(scope)
+  scopeData.disposables.add(disposable)
+}
+
+function detachFromScope(disposable: IDisposable, scope: Scope): void {
+  const scopeData = scopedDisposables.get(scope.source)
+  if (!scopeData) return
+
+  scopeData.disposables.delete(disposable)
+  if (scopeData.disposables.size === 0) {
+    cleanupScope(scope.source)
+  }
+}
+
+/**
+ * Dispose a list of {@link IDisposable disposables}.
+ *
+ * @throws {@link AggregateError}
+ * Thrown if one or more disposables throw errors while being disposed.
+ */
+function disposeMany(disposables: IDisposable[]): void {
+  const errors: unknown[] = []
+
+  disposables.forEach((disposable) => {
+    try {
+      disposable.dispose()
+    } catch (err) {
+      errors.push(err)
+    }
+  })
+
+  if (errors.length) {
+    throw new AggregateError(
+      errors, `${MSG_PREFIX} One or more disposables threw while being disposed.`,
+    )
+  }
+}
+
+/**
+ * Track a disposable in an optional scope.
+ *
+ * **WARNING**: Should not call {@link IDisposable.dispose dispose} on the
+ *              disposable being passed in. Otherwise, the tracking will be broken.
+ *
+ * @param disposable - The disposable to track. When passing a decorated disposable,
+ *                     the **SAME** decorated disposable will be **UPDATED** (if scope changes)
+ *                     and returned. The previous scope will be no longer tracked.
+ * @param scope - The scope of the disposable. When omitted, the disposable is
+ *                tracked globally.
+ *
+ * @returns A decorated disposable to dispose the disposable and untrack it.
+ */
+export function track(disposable: IDisposable, scope?: Scope): IDisposable {
+  let original = disposable // Assume `disposable` is not a decorated one (previously returned by this function)
+  let existing = allDisposables.get(original) // Check if already being tracked
+
+  if (!existing) {
+    // `disposable` is not being tracked.
+    // Check if `disposable` is a decorated one.
+    const existingOriginal = reversedLookup.get(disposable)
+    if (existingOriginal) {
+      // If so, reuse the existing tracked entry without creating a new one.
+      original = existingOriginal
+      existing = allDisposables.get(existingOriginal)
+    }
+  }
+
+  if (existing) {
+    // Check if the scope is the same
+    if (existing.scope?.type === scope?.type && existing.scope?.source === scope?.source)
+      return existing.decorated
+
+    if (existing.scope) {
+      detachFromScope(existing.decorated, existing.scope)
+    }
+
+    if (scope) {
+      attachToScope(existing.decorated, scope)
+      existing.scope = scope
+    } else {
+      existing.scope = undefined
+    }
+
+    return existing.decorated
+  }
+
+  const originalDispose = original.dispose
+  const decoratedDisposable = {
+    dispose: () => {
+      const tracked = allDisposables.get(original)
+      if (!tracked) return // Idempotency
+
+      if (tracked.scope) {
+        detachFromScope(decoratedDisposable, tracked.scope)
+      }
+      allDisposables.delete(original)
+      reversedLookup.delete(decoratedDisposable)
+
+      return originalDispose.call(original)
+    },
+  }
+
+  allDisposables.set(original, { decorated: decoratedDisposable, scope })
+  reversedLookup.set(decoratedDisposable, original)
+
+  if (scope) {
+    attachToScope(decoratedDisposable, scope)
+  }
+
+  return decoratedDisposable
+}
+
+/**
+ * Track a disposable for an {@link Editor.ICodeEditor editor}.
+ *
+ * The tracker uses {@link Editor.ICodeEditor.onDidDispose onDidDispose} to dispose
+ * the tracked disposable.
+ *
+ * @param editor - The editor to track
+ * @param disposable - The disposable to track
+ * @returns A decorated disposable to dispose the disposable and untrack it.
+ */
+export function trackForEditor(editor: Editor.ICodeEditor, disposable: IDisposable): IDisposable {
+  return track(disposable, { type: 'editor', source: editor })
+}
+
+/**
+ * Track a disposable for a {@link Editor.ITextModel text model}.
+ *
+ * The tracker uses {@link Editor.ITextModel.onWillDispose onWillDispose} to dispose
+ * the tracked disposable.
+ *
+ * @param model - The text model to track
+ * @param disposable - The disposable to track
+ * @returns A decorated disposable to dispose the disposable and untrack it.
+ */
+export function trackForModel(model: Editor.ITextModel, disposable: IDisposable): IDisposable {
+  return track(disposable, { type: 'model', source: model })
+}
+
+/**
+ * Dispose all tracked disposables in a scope.
+ *
+ * @param source - The scope source whose tracked disposables to dispose.
+ */
+export function disposeScoped(source: ScopeSource): void {
+  const data = scopedDisposables.get(source)
+  if (!data) return
+
+  const toDispose = Array.from(data.disposables)
+  try {
+    disposeMany(toDispose)
+  } catch (e) {
+    console.warn(`${MSG_PREFIX} Encountered errors while disposing scoped disposables:`, e, ', Source:', source)
+  }
+  cleanupScope(source)
+}
+
+/**
+ * Dispose all tracked disposables.
+ */
+export function disposeAll(): void {
+  const toDispose = Array.from(allDisposables.values())
+  try {
+    disposeMany(toDispose.map(t => t.decorated))
+  } catch (e) {
+    console.warn(`${MSG_PREFIX} Encountered errors while disposing all disposables:`, e)
+  }
+  allDisposables.clear()
+}
