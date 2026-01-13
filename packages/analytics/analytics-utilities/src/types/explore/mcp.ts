466479fd46b21d4b418f02cb91596e4bc0278495
Hiroshi Fukada <hiroshi.fukada@konghq.com>
2026-01-13 12:18:26 -0500

    feat: mcp info types on explore

---
+++ packages/analytics/analytics-utilities/src/types/explore/mcp.ts
@@ -0,0 +1,42 @@
+import { makeFilterable } from './util'
+import type { BasicExploreEmptyFilterV2, BasicExploreInFilterV2, BasicExploreQuery } from './basic'
+import { exploreAggregations, queryableExploreDimensions } from './advanced'
+
+export const queryableMcpExploreDimensions = [
+  ...queryableExploreDimensions,
+  'mcp_info',
+  'mcp_session_id',
+  'mcp_method',
+  'mcp_tool_name',
+  'mcp_error',
+] as const
+
+export type QueryableMcpExploreDimensions = typeof queryableMcpExploreDimensions[number]
+
+export const filterableMcpExploreDimensions = makeFilterable(queryableMcpExploreDimensions)
+
+export type FilterableMcpExploreDimensions = typeof queryableMcpExploreDimensions[number]
+
+export interface McpExploreInFilterV2 extends Omit<BasicExploreInFilterV2, 'field'> {
+  field: FilterableMcpExploreDimensions
+}
+
+export interface McpExploreEmptyFilterV2 extends Omit<BasicExploreEmptyFilterV2, 'field'> {
+  field: FilterableMcpExploreDimensions
+}
+
+export const mcpExploreAggregations = [
+  ...exploreAggregations,
+  'mcp_response_body_size_sum',
+] as const
+
+export type McpExploreAggregations = typeof mcpExploreAggregations[number]
+
+export type McpExploreFilterAll = McpExploreInFilterV2 | McpExploreEmptyFilterV2
+
+export interface McpExploreQuery extends Omit<BasicExploreQuery, 'metrics' | 'dimensions' | 'filters'> {
+  metrics?: McpExploreAggregations[]
+  dimensions?: QueryableMcpExploreDimensions[]
+  filters?: McpExploreFilterAll[]
+}
+
