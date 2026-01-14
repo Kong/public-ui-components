98c1af7e4766b21b00709e90b45ba3f1a1448121
Jordon Leach <jordonleach@gmail.com>
2026-01-14 16:18:18 -0500

    fix(analytics): remove inline style for single-value charts [MA-4712] (#2827)

--- packages/analytics/dashboard-renderer/src/components/SimpleChartRenderer.vue
+++ packages/analytics/dashboard-renderer/src/components/SimpleChartRenderer.vue
@@ -9,7 +9,7 @@
     <div
       class="analytics-chart"
       :class="{ 'single-value': isSingleValueChart }"
-      :style="{ height: `${height}px` }"
+      :style="isSingleValueChart ? undefined : { height: `${height}px` }"
     >
       <SimpleChart
         :chart-data="data"
