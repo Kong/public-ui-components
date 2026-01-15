8ed924ce333f62f8af6b36f0483228fdccae704b
Arash Ari Sheyda <ari.sheyda@konghq.com>
2026-01-15 12:11:57 -0700

    feat: update sandbox

---
+++ packages/core/split-pane/sandbox/pages/FullPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <SplitPane
+    :navigation-items="navItems"
+    :pane-center="props.paneCenter"
+    :pane-left="props.paneLeft"
+    :pane-right="props.paneRight"
+  >
+    <template #pane-left>
+      left
+    </template>
+    <template #pane-center>
+      centre
+    </template>
+    <template #pane-right>
+      right
+    </template>
+  </SplitPane>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { SplitPane, type SplitPaneProps, type VerticalNavigationItem } from '../../src'
+import { ArrowLeftIcon } from '@kong/icons'
+
+const props = computed((): SplitPaneProps => ({
+  paneLeft: { visible: true },
+  paneCenter: { visible: true },
+  paneRight: { visible: true },
+}))
+
+const navItems = computed<VerticalNavigationItem[]>(() => [
+  {
+    active: false,
+    icon: ArrowLeftIcon,
+    testid: 'return',
+    tooltip: 'Return to Home',
+    to: '/',
+  },
+])
+</script>
---
+++ packages/core/split-pane/sandbox/pages/HomePage.vue
@@ -0,0 +1,39 @@
+<template>
+  <main class="custom-container">
+    <KCard>
+      <SplitPane
+        :pane-center="props.paneCenter"
+        :pane-left="props.paneLeft"
+        :pane-right="props.paneRight"
+        :show-navigation="false"
+      >
+        <template #pane-left>
+          left
+        </template>
+        <template #pane-center>
+          centre
+        </template>
+        <template #pane-right>
+          right
+        </template>
+      </SplitPane>
+    </KCard>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { SplitPane, type SplitPaneProps } from '../../src'
+
+const props = computed((): SplitPaneProps => ({
+  paneLeft: { visible: true },
+  paneCenter: { visible: true },
+  paneRight: { visible: true },
+}))
+</script>
+
+<style lang="scss" scoped>
+.custom-container {
+  padding: $kui-space-100;
+}
+</style>
