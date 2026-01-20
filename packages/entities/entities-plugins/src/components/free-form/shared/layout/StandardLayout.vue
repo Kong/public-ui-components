789ad8f848a25815dcfdec4431a3d025ad46c576
GU Yiling <justice360@gmail.com>
2026-01-20 18:11:08 +0800

    feat(entities-plugins): use EntityFormBlock for plugin code editor (#2839)

--- packages/entities/entities-plugins/src/components/free-form/shared/layout/StandardLayout.vue
+++ packages/entities/entities-plugins/src/components/free-form/shared/layout/StandardLayout.vue
@@ -157,8 +157,13 @@
     </template>
 
     <template v-else>
-      <!-- TODO: Implement default code editor -->
-      <slot name="code-editor" />
+      <EntityFormBlock
+        :description="t('plugins.form.sections.code_mode.description')"
+        :title="t('plugins.form.sections.code_mode.title')"
+      >
+        <!-- TODO: Implement default code editor -->
+        <slot name="code-editor" />
+      </EntityFormBlock>
     </template>
   </Form>
 </template>
