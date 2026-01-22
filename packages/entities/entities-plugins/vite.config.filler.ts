9cbe29e4a13d1cd8996880a511a18b2ef5a7490b
Zehao Zhang <zehao93@outlook.com>
2026-01-22 11:17:39 +0800

    fix(entities-plugins): update import paths in package.json and add vite configuration for filler

---
+++ packages/entities/entities-plugins/vite.config.filler.ts
@@ -0,0 +1,25 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: 'dist/filler',
+    emptyOutDir: false,
+    lib: {
+      entry: {
+        'cypress/index': resolve(__dirname, './src/components/free-form/filler/cypress/index.ts'),
+        'playwright/index': resolve(__dirname, './src/components/free-form/filler/playwright/index.ts'),
+      },
+      formats: ['es'],
+      fileName: (_format, entryName) => `${entryName}.js`,
+    },
+    rollupOptions: {
+      external: [
+        // Cypress is external
+        'cypress',
+        // Playwright is external
+        '@playwright/test',
+      ],
+    },
+  },
+})
