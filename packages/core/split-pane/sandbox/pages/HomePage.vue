50ea9ca485b0f3dd42cba678f7784652fbb6e0a4
Arash Ari Sheyda <38922203+arashsheyda@users.noreply.github.com>
2026-01-21 22:52:48 -0700

    feat(split-pane): add splitpane component [KHCP-19160] (#2823)
    * feat(split-pane): add splitpane component [KHCP-19160]
    
    * fix: show navigation on condition
    
    * chore: style update
    
    * feat: add VerticalNavigation
    
    * feat: add tests
    
    * fix: address pr suggestions
    
    * fix: remove toRef
    
    * chore: update class names
    
    * chore: update readme
    
    * fix: update scss
    
    * fix: adjust toolbar height calculation
    
    * chore: remove unnecessary import
    
    * fix: address pr suggestions 2
    
    * fix(vertical-nav): add timeout for disabling toggle animation
    
    * feat: update sandbox
    
    * fix: layout styling in differnt container sizes
    
    * chore: rename classes
    
    * fix: remove remove user select style from composable to rely on css
    
    * fix: update tests
    
    * fix: finish tests
    
    * fix: address pr suggestions
    
    * chore: update imports

---
+++ packages/core/split-pane/sandbox/pages/HomePage.vue
@@ -0,0 +1,44 @@
+<template>
+  <main class="custom-container">
+    <KCard class="my-card">
+      <SplitPane
+        :pane-center="props.paneCenter"
+        :pane-left="props.paneLeft"
+        :pane-right="props.paneRight"
+        :show-navigation="false"
+      >
+        <template #pane-left>
+          left
+        </template>
+        <template #pane-center>
+          centre
+        </template>
+        <template #pane-right>
+          right
+        </template>
+      </SplitPane>
+    </KCard>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { SplitPane, type SplitPaneProps } from '../../src'
+
+const props = computed((): SplitPaneProps => ({
+  paneLeft: { visible: true },
+  paneCenter: { visible: true },
+  paneRight: { visible: true },
+}))
+</script>
+
+<style lang="scss" scoped>
+.custom-container {
+  padding: $kui-space-100;
+
+  .my-card {
+    height: 500px;
+    padding: $kui-space-0;
+  }
+}
+</style>
---
+++ packages/core/split-pane/src/constants/split-pane.ts
@@ -0,0 +1,8 @@
+/** Default minimum width for the left pane */
+export const PANE_LEFT_MIN_WIDTH = '260px'
+/** Default maximum width for the left pane */
+export const PANE_LEFT_MAX_WIDTH = '500px'
+/** Default maximum width for the center pane */
+export const PANE_CENTER_DEFAULT_MAX_WIDTH: string = '50%'
+/** Default maximum width for the right pane */
+export const PANE_RIGHT_DEFAULT_MAX_WIDTH: string = '50%'
