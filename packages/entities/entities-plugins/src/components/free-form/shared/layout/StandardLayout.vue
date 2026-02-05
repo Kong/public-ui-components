e864f14b5f63d40140ba5f9cfb88642454d53921
Zehao <zehao93@outlook.com>
2026-02-05 11:14:24 +0800

    feat(entities-plugins): expose freeform schema to window (#2907)

--- packages/entities/entities-plugins/src/components/free-form/shared/layout/StandardLayout.vue
+++ packages/entities/entities-plugins/src/components/free-form/shared/layout/StandardLayout.vue
@@ -4,6 +4,9 @@
     class="ff-standard-layout"
     :config="realFormConfig"
     :data="(prunedData as T)"
+    :data-instance-id="instanceId"
+    :data-plugin-name="pluginName"
+    data-testid="ff-standard-layout-form"
     :render-rules="renderRules"
     :schema="freeFormSchema"
     tag="div"
@@ -197,7 +200,7 @@ export type Props<T extends FreeFormPluginData = any> = {
 </script>
 
 <script setup lang="ts" generic="T extends FreeFormPluginData">
-import { computed, inject, nextTick, ref, useTemplateRef } from 'vue'
+import { computed, inject, nextTick, ref, useTemplateRef, toRaw, useId, watch, onUnmounted } from 'vue'
 import { EntityFormBlock } from '@kong-ui-public/entities-shared'
 import { has, pick } from 'lodash-es'
 import { KRadio, KTooltip } from '@kong/kongponents'
@@ -217,6 +220,7 @@ import IdentityRealmsField from '../../../fields/key-auth-identity-realms/FreeFo
 import Field from '../Field.vue'
 import StringArrayField from '../StringArrayField.vue'
 import StringField from '../StringField.vue'
+import { FREE_FORM_SCHEMA_MAP_KEY } from '../../../../constants'
 
 const FREE_FORM_CONTROLLED_FIELDS: Array<keyof FreeFormPluginData> = [
   // plugin specific config
@@ -236,6 +240,8 @@ const FREE_FORM_CONTROLLED_FIELDS: Array<keyof FreeFormPluginData> = [
   'route',
   'service',
 ]
+
+const instanceId = useId()
 
 const { t } = createI18n<typeof english>('en-us', english)
 
@@ -530,6 +536,24 @@ function getScopesFromFormModel(): Partial<T> {
   })
   return data
 }
+
+/**
+ * This watch is used to expose the plugin schema for auto-filler.
+ * The auto-filler script requires the **final** schema to be used in the freeform.
+ */
+watch(freeFormSchema, (newSchema) => {
+  if (!(window as any)[FREE_FORM_SCHEMA_MAP_KEY]) {
+    (window as any)[FREE_FORM_SCHEMA_MAP_KEY] = {}
+  }
+  ;(window as any)[FREE_FORM_SCHEMA_MAP_KEY][instanceId] = toRaw(newSchema)
+}, { immediate: true, deep: true })
+
+// Clean up the schema on unmount
+onUnmounted(() => {
+  if ((window as any)[FREE_FORM_SCHEMA_MAP_KEY]) {
+    delete (window as any)[FREE_FORM_SCHEMA_MAP_KEY][instanceId]
+  }
+})
 </script>
 
 <style lang="scss" scoped>
