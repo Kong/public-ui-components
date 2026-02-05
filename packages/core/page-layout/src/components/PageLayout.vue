66a1d2894bbd8428023597cc11c73e0e0201a62f
Maksym Portianoi <maksym.portianoi@konghq.com>
2026-02-05 17:14:03 -0500

    feat(page-layout): create package [KHCP-19462] (#2911)

---
+++ packages/core/page-layout/src/components/PageLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- The <div> tag here is just a placeholder for your component content. -->
+  <!-- We recommend wrapping your component with a unique class when possible, as shown below. -->
+  <div class="kong-ui-public-page-layout">
+    <p>This is the <b>PageLayout</b> content.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.kong-ui-public-page-layout {
+  // Add component styles as needed
+}
+</style>
