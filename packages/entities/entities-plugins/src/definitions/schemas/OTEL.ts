b9ca4e32399074bd2a4651e3b7a6a5af184bcfc7
Zehao Zhang <zehao93@outlook.com>
2026-03-10 14:38:52 +0800

    fix(entities-plugins): otel data transformation

--- packages/entities/entities-plugins/src/definitions/schemas/OTEL.ts
+++ packages/entities/entities-plugins/src/definitions/schemas/OTEL.ts
@@ -12,10 +12,10 @@ export const otelSchema: OTELSchema = {
     // Clean up empty `custom_attributes_by_lua`
     if (isEqual(params.payload.config?.access_logs?.custom_attributes_by_lua, {})) {
       const accessLogs = params.payload.config?.access_logs as
-        | { custom_attributes_by_lua?: Record<string, unknown> }
+        | { custom_attributes_by_lua?: Record<string, unknown> | null }
         | undefined
       if (accessLogs) {
-        delete accessLogs.custom_attributes_by_lua
+        accessLogs.custom_attributes_by_lua = null
       }
     }
 
