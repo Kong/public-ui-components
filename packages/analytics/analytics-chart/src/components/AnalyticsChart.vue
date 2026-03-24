fbae9edc76bfc8833ae0be2f5f4d7fc98b5e5ea2
Shifan <1589110+shifan-w@users.noreply.github.com>
2026-03-24 16:59:15 -0700

    Update packages/analytics/analytics-chart/src/components/AnalyticsChart.vue
    Co-authored-by: Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>

--- packages/analytics/analytics-chart/src/components/AnalyticsChart.vue
+++ packages/analytics/analytics-chart/src/components/AnalyticsChart.vue
@@ -49,7 +49,7 @@
         :chart-data="computedChartData"
         :chart-legend-sort-fn="chartLegendSortFn"
         :chart-tooltip-sort-fn="chartTooltipSortFn"
-        :dimension-axes-title="props.chartOptions?.dimensionAxesTitle ?? timestampAxisTitle"
+        :dimension-axes-title="chartOptions?.dimensionAxesTitle ?? timestampAxisTitle"
         :fill="chartOptions.stacked"
         :granularity="timeSeriesGranularity"
         :legend-values="legendValues"
