679f4a8dc2ac77aa2137a43a0a846b0571e58798
Justineo <justice360@gmail.com>
2026-03-27 15:01:03 +0800

    feat(entities-plugins): sync datakit examples with docs

--- packages/entities/entities-plugins/src/components/free-form/Datakit/examples/index.ts
+++ packages/entities/entities-plugins/src/components/free-form/Datakit/examples/index.ts
@@ -1,13 +1,27 @@
-import { default as authenticate } from './authenticate.yml?raw'
-import { default as vault } from './vault.yml?raw'
-import { default as combine } from './combine.yml?raw'
-import { default as cache } from './cache.yml?raw'
-import { default as manipulate } from './manipulate.yml?raw'
+import { camelCase } from 'lodash-es'
 
-export default {
-  authenticate,
-  vault,
-  combine,
-  cache,
-  manipulate,
+export type DatakitExample = {
+  id: string
+  i18nKey: string
+  code: string
 }
+
+const exampleModules = import.meta.glob('./*.yaml', {
+  eager: true,
+  import: 'default',
+  query: '?raw',
+}) as Record<string, string>
+
+const examples: DatakitExample[] = Object.entries(exampleModules)
+  .sort(([a], [b]) => a.localeCompare(b))
+  .map(([path, code]) => {
+    const id = path.replace(/^\.\//, '').replace(/\.yaml$/, '')
+
+    return {
+      id,
+      i18nKey: camelCase(id),
+      code,
+    }
+  })
+
+export default examples
