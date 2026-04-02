23844e95777c46cc23c2b107f6da4ed10351efe1
Zehao Zhang <zehao93@outlook.com>
2026-04-02 18:05:24 +0800

    feat: update definePluginConfig to include default component

--- packages/entities/entities-plugins/src/components/free-form/shared/define-plugin-config.ts
+++ packages/entities/entities-plugins/src/components/free-form/shared/define-plugin-config.ts
@@ -1,7 +1,11 @@
+import CommonForm from '../Common/CommonForm.vue'
 import type { PluginFormConfig } from './types'
 
 export function definePluginConfig(config: Partial<PluginFormConfig>): PluginFormConfig {
-  return config
+  return {
+    ...config,
+    component: config.component ?? CommonForm,
+  }
 }
 
 export type { FieldRenderer, PluginFormConfig } from './types'
