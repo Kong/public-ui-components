b1611f0f7747eb2fedccaa3dcc7ee8c0f7da85ce
Zehao <zehao93@outlook.com>
2026-04-14 16:57:58 +0800

    feat(entities-plugins): add custom plugin form (#3129)

--- packages/entities/entities-plugins/src/external-links.ts
+++ packages/entities/entities-plugins/src/external-links.ts
@@ -5,4 +5,6 @@ export default {
    * TODO: wait for Doc team provide a new link.
    */
   condition: 'https://developer.konghq.com/gateway/routing/expressions/',
+  customPluginSandboxing: 'https://developer.konghq.com/gateway/sandboxing/',
+  pluginPriority: 'https://developer.konghq.com/gateway/entities/plugin/#plugin-priority',
 }
