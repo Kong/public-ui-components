c4f6e70bb71a6135db3526769efd05091c0d0c99
portikM <maksym.portianoi@konghq.com>
2026-05-05 09:46:17 -0400

    fix: minor fix

--- packages/core/app-layout/src/components/pageHeader/AppPageHeader.vue
+++ packages/core/app-layout/src/components/pageHeader/AppPageHeader.vue
@@ -161,7 +161,7 @@ const breadcrumbIconSlots = computed((): string[] => {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    margin-bottom: var(--kui-space-0, $kui-space-0);
+    margin-bottom: var(--kui-space-50, $kui-space-50);
     min-height: 32px;
 
     .page-header-title-section {
