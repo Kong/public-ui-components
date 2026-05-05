1cce0f81b9fa14feac5897a63d28f7af1cc300e5
John Cowen <john.cowen@konghq.com>
2026-05-05 17:59:06 +0100

    chore(linting): add missing linter and lint

--- packages/portal/swagger-ui-web-component/webpack.config.js
+++ packages/portal/swagger-ui-web-component/webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const path = require('path')
 const TerserPlugin = require('terser-webpack-plugin')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
