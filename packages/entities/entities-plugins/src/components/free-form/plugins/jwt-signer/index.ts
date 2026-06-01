d1022798b2364e8cac764a64764a77e6fc15504d
Makito <i@maki.to>
2026-06-01 15:07:39 +0900

    feat(entities-plugins): add warnings for keyset fields in jwt-signer (#3359)
    * feat(entities-plugins): add warnings for keyset fields in jwt-signer
    
    * feat(jwt-signer): move render-rules to StandardLayout

---
+++ packages/entities/entities-plugins/src/components/free-form/plugins/jwt-signer/index.ts
@@ -0,0 +1,6 @@
+import { definePluginConfig } from '../../shared/define-plugin-config'
+import JwtSignerForm from './JwtSignerForm.vue'
+
+export default definePluginConfig({
+  component: JwtSignerForm,
+})
