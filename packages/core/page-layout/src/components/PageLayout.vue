31f0bad830fc4d1b15d1da690fb9128fbee1fac5
portikM <maksym.portianoi@konghq.com>
2026-06-08 13:08:07 -0600

    fix: minor fix

--- packages/core/page-layout/src/components/PageLayout.vue
+++ packages/core/page-layout/src/components/PageLayout.vue
@@ -157,7 +157,7 @@ const isFavorite = computed((): boolean =>
   pageShortcutsContext.isFavorite === true) ||
   ('isPageFavorite' in pageShortcutsContext &&
   typeof pageShortcutsContext.isPageFavorite === 'function' &&
-  pageShortcutsContext.isPageFavorite())))
+  pageShortcutsContext.isPageFavorite(pageShortcutData))))
 
 /** Handle navigation back via the backTo prop */
 const navigateBack = async () => {
