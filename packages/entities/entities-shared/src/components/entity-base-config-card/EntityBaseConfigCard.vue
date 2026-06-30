199f193c6a78353561078e49a80f32fd48320912
Yi Yang <Leopoldthecoder@users.noreply.github.com>
2026-06-30 17:01:25 +0800

    fix(entities-shared): labels display (#3511)

--- packages/entities/entities-shared/src/components/entity-base-config-card/EntityBaseConfigCard.vue
+++ packages/entities/entities-shared/src/components/entity-base-config-card/EntityBaseConfigCard.vue
@@ -455,6 +455,11 @@ const DEFAULT_BASIC_FIELDS_CONFIGURATION: DefaultCommonFieldsConfigurationSchema
     order: -1, // the last property displayed
     section: ConfigurationSchemaSection.Basic,
   },
+  labels: {
+    type: ConfigurationSchemaType.BadgeTag,
+    order: -1, // the last property displayed
+    section: ConfigurationSchemaSection.Basic,
+  },
   partials: {
     type: ConfigurationSchemaType.LinkInternal,
     label: t('baseConfigCard.commonFields.partial_label'),
