f815311298a93d948991c2ea688449f748236883
Zehao <zehao93@outlook.com>
2026-07-13 17:01:18 +0800

    feat: update migration report and remove deprecated GraphQL rate limiting components

--- packages/entities/entities-plugins/src/components/free-form/plugins/graphql-rate-limiting-advanced/index.ts
+++
@@ -1,7 +0,0 @@
-import GraphQLRateLimitingAdvancedForm from './GraphQLRateLimitingAdvancedForm.vue'
-import { definePluginConfig } from '../../shared/define-plugin-config'
-
-export default definePluginConfig({
-  experimental: true,
-  component: GraphQLRateLimitingAdvancedForm,
-})
--- packages/entities/entities-plugins/src/components/free-form/plugins/request-termination.ts
+++ packages/entities/entities-plugins/src/components/free-form/plugins/request-termination.ts
@@ -1,16 +1,5 @@
-import StringField from '../shared/StringField.vue'
 import { definePluginConfig } from '../shared/define-plugin-config'
 
 export default definePluginConfig({
   experimental: true,
-  fieldRenderers: [
-    {
-      match: 'config.body',
-      component: StringField,
-      propsOverrides: {
-        multiline: true,
-        rows: 4,
-      },
-    },
-  ],
 })
