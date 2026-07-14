871678ef2009dc2c6414768da153e71b91577687
Zehao <zehao93@outlook.com>
2026-07-14 11:35:47 +0800

    fix: configs

--- packages/entities/entities-plugins/src/components/free-form/plugins/datadog.ts
+++ packages/entities/entities-plugins/src/components/free-form/plugins/datadog.ts
@@ -1,5 +1,16 @@
 import { definePluginConfig } from '../shared/define-plugin-config'
+import ArrayField from '../shared/ArrayField.vue'
 
 export default definePluginConfig({
   experimental: true,
+  fieldRenderers: [
+    {
+      match: 'config.metrics',
+      component: ArrayField as any,
+      propsOverrides: {
+        appearance: 'tabs',
+        itemLabel: (_: unknown, index: number) => `#${index + 1} Metric`,
+      },
+    },
+  ],
 })
--- packages/entities/entities-plugins/src/components/free-form/plugins/zipkin.ts
+++ packages/entities/entities-plugins/src/components/free-form/plugins/zipkin.ts
@@ -1,5 +1,16 @@
 import { definePluginConfig } from '../shared/define-plugin-config'
+import ArrayField from '../shared/ArrayField.vue'
 
 export default definePluginConfig({
   experimental: true,
+  fieldRenderers: [
+    {
+      match: 'config.static_tags',
+      component: ArrayField as any,
+      propsOverrides: {
+        appearance: 'tabs',
+        itemLabel: (_: unknown, index: number) => `#${index + 1} Tag`,
+      },
+    },
+  ],
 })
