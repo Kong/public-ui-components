0f21c67188c9fb33ebbee46b12d8d69ea61a84c6
Zehao <zehao93@outlook.com>
2026-07-14 17:05:13 +0800

    feat(entities-plugins): add ArrayField component for custom injections

--- packages/entities/entities-plugins/src/components/free-form/plugins/injection-protection.ts
+++ packages/entities/entities-plugins/src/components/free-form/plugins/injection-protection.ts
@@ -1,5 +1,6 @@
 import { definePluginConfig } from '../shared/define-plugin-config'
 import EnumField from '../shared/EnumField.vue'
+import ArrayField from '../shared/ArrayField.vue'
 
 export default definePluginConfig({
   experimental: true,
@@ -13,6 +14,14 @@ export default definePluginConfig({
       match: 'config.locations',
       component: EnumField,
       propsOverrides: { multiple: true },
+    },
+    {
+      match: 'config.custom_injections',
+      component: ArrayField as any,
+      propsOverrides: {
+        appearance: 'tabs',
+        itemLabel: (_: unknown, index: number) => `#${index + 1} Injection`,
+      },
     },
   ],
 })
