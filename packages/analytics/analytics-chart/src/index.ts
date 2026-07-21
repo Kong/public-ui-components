25233d0ce9304bca323682b19bd6d4004b256c89
Filip Gutica <filgut@protonmail.com>
2026-07-21 13:58:34 -0700

    refactor(analytics-chart): deprecate CSV export modal [MA-5029]

--- packages/analytics/analytics-chart/src/index.ts
+++ packages/analytics/analytics-chart/src/index.ts
@@ -1,10 +1,14 @@
 import AnalyticsChart from './components/AnalyticsChart.vue'
 import SimpleChart from './components/SimpleChart.vue'
 import TopNTable from './components/TopNTable.vue'
-import CsvExportModal from './components/CsvExportModal.vue'
 import SparklineChart from './components/SparklineChart.vue'
 
-export { AnalyticsChart, SimpleChart, TopNTable, CsvExportModal, SparklineChart }
+export { AnalyticsChart, SimpleChart, TopNTable, SparklineChart }
+
+/**
+ * @deprecated Import CsvExportModal from '@kong-ui-public/dashboard-renderer' instead.
+ */
+export { default as CsvExportModal } from './components/CsvExportModal.vue'
 
 export * from './types'
 export * from './enums'
