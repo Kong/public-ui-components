0bb9ff6384d2c264f8a356d3ffd595fbf874292b
Zehao <zehao93@outlook.com>
2026-07-28 10:54:56 +0800

    fix(entities-plugins): avoid string field emits empty string

--- packages/entities/entities-plugins/src/components/free-form/shared/StringField.vue
+++ packages/entities/entities-plugins/src/components/free-form/shared/StringField.vue
@@ -130,16 +130,12 @@ const emit = defineEmits<{
 
 const { value: fieldValue, hide, ...field } = useField<string | null>(toRef(() => name))
 const fieldAttrs = useFieldAttrs(field.path!, toRef({ ...props, ...attrs }))
-const initialValue = fieldValue?.value
 
 function handleUpdate(value: string) {
-  if (initialValue !== undefined && value === '' && value !== initialValue) {
-    fieldValue!.value = null
-    emit('update:modelValue', null)
-  } else {
-    fieldValue!.value = value.trim()
-    emit('update:modelValue', value.trim())
-  }
+  const trimmedValue = value.trim()
+
+  fieldValue!.value = trimmedValue === '' ? null : trimmedValue
+  emit('update:modelValue', fieldValue!.value)
 }
 
 const encrypted = computed(() => {
