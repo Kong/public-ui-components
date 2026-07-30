06cf0fa7c345bed2104aa8dfb6a3b02edff6244f
Filip <filgut@protonmail.com>
2026-07-30 15:15:31 -0700

    feat(dashboard-renderer): own CSV export flow [MA-5029] (#3596)
    * feat(dashboard-renderer): own CSV export modal [MA-5029]
    
    * refactor(dashboard-renderer): use owned CSV export modal [MA-5029]
    
    * refactor(analytics-chart): deprecate CSV export modal [MA-5029]
    
    * docs(analytics-chart): mark CSV cleanup scope [MA-5236]
    
    * fix(dashboard-renderer): secure CSV field selection [MA-5029]
    
    * test(dashboard-renderer): remove redundant export modal test
    
    * Update packages/analytics/dashboard-renderer/src/components/CsvExportModal.vue
    
    Co-authored-by: Maksym Portianoi <maksym.portianoi@konghq.com>
    
    ---------
    
    Co-authored-by: Maksym Portianoi <maksym.portianoi@konghq.com>

--- packages/analytics/dashboard-renderer/src/index.ts
+++ packages/analytics/dashboard-renderer/src/index.ts
@@ -1,8 +1,9 @@
 import DashboardRenderer from './components/DashboardRenderer.vue'
 import GridLayout from './components/layout/GridLayout.vue'
 import DashboardTilePreview from './components/DashboardTilePreview.vue'
+import CsvExportModal from './components/CsvExportModal.vue'
 
-export { DashboardRenderer, GridLayout, DashboardTilePreview }
+export { DashboardRenderer, GridLayout, DashboardTilePreview, CsvExportModal }
 
 export * from './types'
 export * from './constants'
