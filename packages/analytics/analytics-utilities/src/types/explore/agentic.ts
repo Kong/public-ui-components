fbee1fb9d4c181ec0f2baec0b494f0f9eabde130
Alex Grant <alxgenegrant@gmail.com>
2026-09-10 12:54:45 -0400

    feat: add cost_per_request and active_agents (#3823)

--- packages/analytics/analytics-utilities/src/types/explore/agentic.ts
+++ packages/analytics/analytics-utilities/src/types/explore/agentic.ts
@@ -34,6 +34,7 @@ export const agenticExploreAggregations = [
   ...exploreAggregations,
   'a2a_latency_average',
   'a2a_response_size_sum',
+  'active_agents',
   'mcp_latency_average',
   'mcp_latency_p99',
   'mcp_latency_p95',
