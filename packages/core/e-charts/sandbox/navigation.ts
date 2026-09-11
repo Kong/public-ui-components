812f3b269c9b7a7abffdbf6bde2d547124b326d9
Vaibhav <vaibhav.rajsingh@konghq.com>
2026-09-11 20:29:25 +0530

    feat(e-charts): add sandbox example for heatmap

---
+++ packages/core/e-charts/sandbox/navigation.ts
@@ -0,0 +1,8 @@
+import type { SandboxNavigationItem } from '@kong-ui-public/sandbox-layout'
+
+export const appLinks: SandboxNavigationItem[] = [
+  {
+    name: 'Heatmap',
+    to: { name: 'home' },
+  },
+]
