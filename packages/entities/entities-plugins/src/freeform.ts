9edfc19ab2108263a29323c73776ee2800acf271
Zehao <zehao93@outlook.com>
2026-09-14 11:01:27 +0800

    fix: exports

--- packages/entities/entities-plugins/src/freeform.ts
+++ packages/entities/entities-plugins/src/freeform.ts
@@ -1,30 +1,3 @@
 import '@kong-ui-public/freeform/style.css'
 
-export {
-  Form,
-  Field,
-  FieldRenderer,
-  ExpressionField,
-  ExpressionEditor,
-  StringField,
-  SwitchField,
-  BooleanField,
-  ArrayField,
-  ObjectField,
-  NumberField,
-  EnumField,
-  StringArrayField,
-  JsonField,
-  ForeignField,
-  MapField,
-  renderRuleExactMatch,
-  USE_SECRET_INPUT_KEY,
-} from '@kong-ui-public/freeform'
-export type {
-  FormSchema,
-  UnionFieldSchema,
-  NamedFieldSchema,
-  ExpressionFieldSchema,
-  RenderRules,
-  FormConfig,
-} from '@kong-ui-public/freeform'
+export * from '@kong-ui-public/freeform'
