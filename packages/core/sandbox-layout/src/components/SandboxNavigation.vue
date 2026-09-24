4b72821549683a75a17069604d8bba09243f785e
Adam DeHaven <adam.dehaven@konghq.com>
2026-09-24 17:36:07 -0400

    fix: text color

--- packages/core/sandbox-layout/src/components/SandboxNavigation.vue
+++ packages/core/sandbox-layout/src/components/SandboxNavigation.vue
@@ -83,7 +83,7 @@ const navLinks: SandboxNavigationItem[] = inject(KONG_UI_SANDBOX_LAYOUT_LINKS_IN
 // Unscoped to control page elements
 html, body {
   background-color: var(--kui-color-background, $kui-color-background);
-  color: var(--kui-color-text-primary, $kui-color-text-primary);
+  color: var(--kui-color-text, $kui-color-text);
   margin: 0;
   min-height: 100%;
   padding: 0;
